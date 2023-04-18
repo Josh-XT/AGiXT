733a0760e43f6f29d06022ee0cd7d900a19ce569
Josh XT <josh@devxt.com>
2023-04-18 15:06:25 -0400

    Update readme, Flask runs from main.py now

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python3
-import argparse
-from babyagi import babyagi
+from app import app
 
-def main(primary_objective):
-    tms = babyagi(primary_objective=primary_objective, initial_task="Develop a task list")
-    tms.run()
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Task Management System")
-    parser.add_argument("primary_objective", help="Specify the primary objective for the Task Management System")
-
-    args = parser.parse_args()
-    main(args.primary_objective)
+if __name__ == '__main__':
+    app.run(debug=True)
