deb7f6b3f4b2c40315bb6fb973492519c06da44f
Josh XT <josh@devxt.com>
2023-04-18 16:48:40 -0400

    Front end QOL

--- frontend/src/AppHeader.js
+++ frontend/src/AppHeader.js
@@ -1,13 +1,35 @@
 import React from "react";
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  FormGroup,
+  FormControlLabel,
+  Switch,
+} from "@mui/material";
 
-const AppHeader = () => {
+const AppHeader = ({ darkMode, handleToggleDarkMode }) => {
+  const handleClick = (e) => {
+    e.stopPropagation();
+  };
+
   return (
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" component="div">
           Agent-LLM
         </Typography>
+        <div style={{ marginLeft: "auto" }}>
+          <FormGroup>
+            <FormControlLabel
+              control={
+                <Switch checked={darkMode} onChange={handleToggleDarkMode} />
+              }
+              label="Toggle Dark Mode"
+              onClick={handleClick}
+            />
+          </FormGroup>
+        </div>
       </Toolbar>
     </AppBar>
   );
