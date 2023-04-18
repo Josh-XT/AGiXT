4210110137d38d251229ba07dbbf6c621eae6d07
Josh XT <josh@devxt.com>
2023-04-18 12:52:58 -0400

    Added llama path selection support, updated prompt

--- provider/llamacpp.py
+++ provider/llamacpp.py
@@ -1,7 +1,11 @@
 import subprocess
+from Config import Config
+
+CFG = Config()
 
 class AIProvider:
     def instruct(self, prompt):
-        cmd = ["llama/main", "-p", prompt]
+        llama_path = CFG.LLAMACPP_PATH if CFG.LLAMACPP_PATH else "llama/main"
+        cmd = [llama_path, "-p", prompt]
         result = subprocess.run(cmd, shell=True, stderr=subprocess.DEVNULL, stdout=subprocess.PIPE, text=True)
         return result.stdout.strip()
