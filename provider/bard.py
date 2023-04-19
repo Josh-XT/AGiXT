a65bd7d674d9ec3877501bf58c7e082ca69ef7cf
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2023-04-18 20:47:32 -0400

    Add Google Bard provider
    Signed-off-by: Josh XT <102809327+Josh-XT@users.noreply.github.com>

---
+++ provider/bard.py
@@ -0,0 +1,27 @@
+import os
+import random
+from selenium import webdriver
+from Bard import Chatbot
+from Config import Config
+
+CFG = Config()
+
+class AIProvider:
+    def __init__(self):
+        if CFG.AI_PROVIDER.lower() == "bard":
+            driver = webdriver.Chrome()
+            
+            BARD_URL = "https://bard.google.com/"
+            driver.get(BARD_URL)
+            driver.execute_script("return document.cookie")
+            psid_cookie = driver.get_cookie("__Secure-1PSID")
+            self.psid = psid_cookie["value"] if psid_cookie else None
+            driver.quit()
+
+            if not self.psid:
+                raise Exception("Unable to get __Secure-1PSID cookie.")
+            self.chatbot = Chatbot(self.psid)
+
+    def instruct(self, prompt, seed=None):
+        response = self.chatbot.ask(prompt)
+        return response.replace("\n", "\n")
