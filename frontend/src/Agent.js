f67270f33931b15d157162e0715138f3d9b5faf9
James Grieve <jamesonrgrieve@gmail.com>
2023-04-20 14:18:59 -0600

    Logs;

--- frontend/src/Agent.js
+++ frontend/src/Agent.js
@@ -7,18 +7,23 @@ import {
 } from "@mui/material";
 import { LoadingContext } from "./App";
 const Agent = (props) => {
-    const [refresh, setRefresh] = useState();
+    const [refresh, setRefresh] = useState(null);
     const [agentData, setAgentData] = useState(null);
     const [objective, setObjective] = useState("");
     const baseURI = useContext(URIContext);
     const [loading, setLoading] = useState(true);
     function toggleRunning() {
+        console.log(refresh);
+        console.log("Toggling Agent [Agent.js]");
         if (refresh) {
             try
             {
+                console.log("Stopping Agent [Agent.js]");
                 fetch(`${baseURI}/api/task/stop/${props.agent}`, { method: "POST" }).then(async () => {
+                    console.log("Fetched [Agent.js]");
                     if (!await(await fetch(`${baseURI}/api/task/status/${props.agent}`)).json().status)
                     {
+                        console.log("Stopped [Agent.js]");
                         clearInterval(refresh);
                         setRefresh(null);
                     }
@@ -31,6 +36,7 @@ const Agent = (props) => {
 
         } else {
             try {
+                console.log("Starting Agent [Agent.js]");
                 fetch(`${baseURI}/api/task/start/${props.agent}`, {
                     method: "POST",
                     headers: {
@@ -38,10 +44,13 @@ const Agent = (props) => {
                     },
                     body: JSON.stringify({ objective: objective }),
                 }).then(async () => {
+                    console.log("Fetched [Agent.js]");
                     if (await(await fetch(`${baseURI}/api/task/status/${props.agent}`)).json().status)
                     {
+                        console.log("Started [Agent.js]");
                         setRefresh(
                             setInterval(async () => {
+                                console.log("Updating (Interval) [Agent.js]");
                                 const response = await ((await fetch(`${baseURI}/api/task/output/${props.agent}`)).json());
                                 console.log(response);
                                 if (response.output && response.output.length > 0) setAgentData(response.output);
