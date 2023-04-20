c01152946e688a84da01e53c565f46cae7d532e0
Josh XT <josh@devxt.com>
2023-04-20 00:09:50 -0400

    Back end updates

--- app.py
+++ app.py
@@ -15,8 +15,6 @@ app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
-babyagi_instance = babyagi()
-
 SWAGGER_URL = '/api/docs'
 API_URL = '/static/swagger.json'
 
@@ -60,7 +58,6 @@ class DeleteAgent(Resource):
         agent_folder = f"agents/{agent_name}/"
         agent_file = os.path.abspath(agent_file)
         agent_folder = os.path.abspath(agent_folder)
-
         try:
             os.remove(agent_file)
         except FileNotFoundError:
@@ -95,49 +92,7 @@ class Instruct(Resource):
         agent = AgentLLM(agent_name)
         response = agent.run(objective, max_context_tokens=500, long_term_access=False)
         return {"response": str(response)}, 200
-
-class SetObjective(Resource):
-    def post(self):
-        objective = request.json.get("objective")
-        babyagi_instance.set_objective(objective)
-        return {"message": "Objective updated"}, 200
-
-class AddInitialTask(Resource):
-    def post(self):
-        babyagi_instance.add_initial_task()
-        return {"message": "Initial task added"}, 200
-
-class ExecuteNextTask(Resource):
-    def get(self):
-        task = babyagi_instance.execute_next_task()
-        task_list = list(babyagi_instance.task_list)
-        if task:
-            return {"task": task, "result": babyagi_instance.response, "task_list": task_list}, 200
-        else:
-            return {"message": "All tasks complete"}, 200
-
-class CreateTask(Resource):
-    def post(self):
-        objective = request.json.get("objective")
-        result = request.json.get("result")
-        task_description = request.json.get("task_description")
-        task_list = request.json.get("task_list")
-        new_tasks = babyagi_instance.task_creation_agent(objective, result, task_description, task_list)
-        return {"new_tasks": new_tasks}, 200
-
-class PrioritizeTasks(Resource):
-    def post(self):
-        task_id = request.json.get("task_id")
-        babyagi_instance.prioritization_agent(task_id)
-        return {"task_list": babyagi_instance.task_list}, 200
-
-class ExecuteTask(Resource):
-    def post(self):
-        objective = request.json.get("objective")
-        task = request.json.get("task")
-        result = babyagi_instance.execution_agent(objective, task)
-        return {"result": result}, 200
-
+    
 class GetCommands(Resource):
     def get(self):
         commands = Commands()
@@ -189,17 +144,42 @@ class DisableAllCommands(Resource):
         except Exception as e:
             return {"message": f"Error enabling all commands for agent '{agent_name}': {str(e)}"}, 500
 
+class StartTaskAgent(Resource):
+    def post(self, agent_name):
+        objective = request.json.get("objective")
+        babyagi_instance = babyagi(agent_name)
+        babyagi_instance.set_objective(objective)
+        babyagi_instance.run()
+        return {"message": "Task agent started"}, 200
+
+class StopTaskAgent(Resource):
+    def post(self, agent_name):
+        babyagi_instance = babyagi(agent_name)
+        babyagi_instance.stop_running()
+        return {"message": "Task agent stopped"}, 200
+
+class GetTaskOutput(Resource):
+    def get(self, agent_name):
+        babyagi_instance = babyagi(agent_name)
+        output = babyagi_instance.get_output()
+        print(f"Output: {output}")
+        return {"output": output}, 200
+
+class GetTaskStatus(Resource):
+    def get(self, agent_name):
+        babyagi_instance = babyagi(agent_name)
+        status = babyagi_instance.get_status()
+        return {"status": status}, 200
+
+api.add_resource(StartTaskAgent, '/api/task/start/<string:agent_name>')
+api.add_resource(StopTaskAgent, '/api/task/stop/<string:agent_name>')
+api.add_resource(GetTaskOutput, '/api/task/output/<string:agent_name>')
+api.add_resource(GetTaskStatus, '/api/task/status/<string:agent_name>')
 api.add_resource(AddAgent, '/api/add_agent/<string:agent_name>')
 api.add_resource(DeleteAgent, '/api/delete_agent/<string:agent_name>')
 api.add_resource(GetAgents, '/api/get_agents')
 api.add_resource(GetChatHistory, '/api/get_chat_history/<string:agent_name>')
 api.add_resource(Instruct, '/api/instruct')
-api.add_resource(SetObjective, '/api/set_objective')
-api.add_resource(AddInitialTask, '/api/add_initial_task')
-api.add_resource(ExecuteNextTask, '/api/execute_next_task')
-api.add_resource(CreateTask, '/api/create_task')
-api.add_resource(PrioritizeTasks, '/api/prioritize_tasks')
-api.add_resource(ExecuteTask, '/api/execute_task')
 api.add_resource(GetCommands, '/api/get_commands')
 api.add_resource(GetAvailableCommands, '/api/get_available_commands/<string:agent_name>')
 api.add_resource(EnableCommand, '/api/enable_command/<string:agent_name>/<string:command_name>')
