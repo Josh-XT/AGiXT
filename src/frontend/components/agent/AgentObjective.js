127f607632921eca12ac5106648ee6aefab256b2
James Grieve <jamesonrgrieve@gmail.com>
2023-04-21 17:50:53 -0600

    Fixed import;

--- src/frontend/components/agent/AgentObjective.js
+++ src/frontend/components/agent/AgentObjective.js
@@ -2,6 +2,7 @@ import { useState, useEffect, useCallback } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
 import useSWR from "swr";
+import { mutate } from "swr";
 import {
     Typography,
     Paper,
