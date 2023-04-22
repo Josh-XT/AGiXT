4b45449df3206d5f274733054b101d45fb74e8c7
James Grieve <jamesonrgrieve@gmail.com>
2023-04-21 18:11:44 -0600

    For the love of...

--- src/frontend/components/agent/AgentObjective.js
+++ src/frontend/components/agent/AgentObjective.js
@@ -1,6 +1,7 @@
 import { useState, useEffect, useCallback } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
+import { mutate } from "swr";
 import useSWR from "swr";
 import {
     Typography,
