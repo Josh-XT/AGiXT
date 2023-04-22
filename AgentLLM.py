9900e15eb7f47e31c552f287ac2ac2680a40f9b0
Josh XT <josh@devxt.com>
2023-04-21 20:31:59 -0400

    Check status at start of loop

--- AgentLLM.py
+++ AgentLLM.py
@@ -253,9 +253,9 @@ class AgentLLM:
         self.add_initial_task()
         self.running = True
         while self.running:
+            self.running = self.get_status()
             task = self.execute_next_task()
             self.display_result(task)
-            self.running = self.get_status()
             if not self.task_list:
                 self.output_list.append(f"\n\nAll tasks complete.")
                 print("\033[91m\033[1m" + "\n*****ALL TASKS COMPLETE*****\n" + "\033[0m\033[0m")
