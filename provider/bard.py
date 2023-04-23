ad316b2fb372319d7f033a6dbfc2cb8d10232e2a
Josh XT <josh@devxt.com>
2023-04-23 08:29:40 -0400

    Add BARD_TOKEN and fix #29

--- provider/bard.py
+++ provider/bard.py
@@ -7,19 +7,8 @@ CFG = Config()
 class AIProvider:
     def __init__(self):
         if CFG.AI_PROVIDER.lower() == "bard":
-            driver = webdriver.Chrome()
-            
-            BARD_URL = "https://bard.google.com/"
-            driver.get(BARD_URL)
-            driver.execute_script("return document.cookie")
-            psid_cookie = driver.get_cookie("__Secure-1PSID")
-            self.psid = psid_cookie["value"] if psid_cookie else None
-            driver.quit()
-
-            if not self.psid:
-                raise Exception("Unable to get __Secure-1PSID cookie.")
-            self.chatbot = Chatbot(self.psid)
+            self.chatbot = Chatbot(CFG.BARD_TOKEN)
 
     def instruct(self, prompt, seed=None):
         response = self.chatbot.ask(prompt)
-        return response.replace("\n", "\n")
+        return response['content'].replace("\n", "\n")
