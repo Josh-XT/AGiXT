21473652b2187bed69fc787506de00fe7e98f611
Josh XT <josh@devxt.com>
2023-04-23 12:09:16 -0400

    Add custom prompt endpoints

--- Config.py
+++ Config.py
@@ -397,3 +397,30 @@ class Config():
     def log_interaction(self, role: str, message: str):
         self.memory["interactions"].append({"role": role, "message": message})
         self.save_memory()
+    
+    def add_prompt(self, prompt_name, prompt):
+        # if prompts folder does not exist, create it
+        if not os.path.exists("prompts"):
+            os.mkdir("prompts")
+        # if prompt file does not exist, create it
+        if not os.path.exists(os.path.join("prompts", f"{prompt_name}.txt")):
+            with open(os.path.join("prompts", f"{prompt_name}.txt"), "w") as f:
+                f.write(prompt)
+        else:
+            raise Exception("Prompt already exists")
+        
+    def get_prompt(self, prompt_name):
+        with open(os.path.join("prompts", f"{prompt_name}.txt"), "r") as f:
+            prompt = f.read()
+        return prompt
+    
+    def get_prompts(self):
+        prompts = os.listdir("prompts")
+        return prompts
+    
+    def delete_prompt(self, prompt_name):
+        os.remove(os.path.join("prompts", f"{prompt_name}.txt"))
+
+    def update_prompt(self, prompt_name, prompt):
+        with open(os.path.join("prompts", f"{prompt_name}.txt"), "w") as f:
+            f.write(prompt)
