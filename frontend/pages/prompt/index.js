4b1d8997cc3309bf5a4f2f3746d751ee8c6ed854
James Grieve <jamesonrgrieve@gmail.com>
2023-04-24 18:47:07 -0600

    Fixed markdown file refs;

--- frontend/pages/prompt/index.js
+++ frontend/pages/prompt/index.js
@@ -8,7 +8,7 @@ import PromptList from '@/components/prompt/PromptList';
 import {useRouter} from 'next/router';
 export default function Home() {
   const router = useRouter();
-  const docs = useSWR('docs/prompt', async () => (await axios.get("https://raw.githubusercontent.com/Josh-XT/Agent-LLM/main/docs/nextjs-2/PROMPT.md")).data);
+  const docs = useSWR('docs/prompt', async () => (await axios.get("https://raw.githubusercontent.com/Josh-XT/Agent-LLM/main/docs/nextjs-2/concepts/PROMPT.md")).data);
   const prompts = useSWR('prompt', async () => (await axios.get(`${process.env.NEXT_PUBLIC_API_URI ?? 'http://localhost:5000'}/api/prompt`)).data.prompts);
   return <DoubleSidedMenu title={"Prompt Homepage"} leftHeading={"Prompts"} leftSWR={prompts} leftMenu={PromptList} rightHeading={null} rightSWR={null} rightMenu={null} content={() => <Container>
     <ContentSWR swr={docs} content={({ data }) => (
