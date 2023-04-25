1e130936261db251f74e886b37b105a804c4dbe8
James Grieve <jamesonrgrieve@gmail.com>
2023-04-24 22:42:22 -0600

    Cleaned up prints;

--- provider/llamacpp.py
+++ provider/llamacpp.py
@@ -15,9 +15,5 @@ class AIProvider:
             self.model = Model(ggml_model=CFG.MODEL_PATH, n_ctx=self.max_tokens)
             # TODO: Need to reseach to add temperature, no obvious flag.
 
-    def new_text_callback(self, text: str):
-        print(text, end="", flush=True)
-
     def instruct(self, prompt):
-        output = self.model.generate(f"Q: {prompt}", n_predict=55, new_text_callback=self.new_text_callback, n_threads=8)
-        return output
+        return self.model.generate(prompt, n_predict=55, n_threads=8)
