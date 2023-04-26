3e89be37c54de5021254c5f597fc00f72d2324c8
Gene Ruebsamen <gururise@gmail.com>
2023-04-26 05:27:29 -0700

    fix oobabooga api access (#101)

--- provider/oobabooga.py
+++ provider/oobabooga.py
@@ -10,16 +10,32 @@ class AIProvider:
     def instruct(self, prompt, seed=None):
         if seed is None:
             seed = random.randint(1, 1000000000)
+        
         params = {
-            'max_new_tokens': int(CFG.MAX_TOKENS), 'do_sample': True, 'temperature': float(CFG.AI_TEMPERATURE), 'top_p': 0.73, 'typical_p': 1,
-            'repetition_penalty': 1.1, 'encoder_repetition_penalty': 1.0, 'top_k': 0, 'min_length': 0,
-            'no_repeat_ngram_size': 0, 'num_beams': 1, 'penalty_alpha': 0, 'length_penalty': 1,
-            'early_stopping': False, 'seed': seed, 'add_bos_token': True, 'custom_stopping_strings': [],
-            'truncation_length': 4096, 'ban_eos_token': False
+            'prompt': prompt,
+            'max_new_tokens': int(CFG.MAX_TOKENS),
+            'do_sample': True,
+            'temperature': float(CFG.AI_TEMPERATURE),
+            'top_p': 0.73,
+            'typical_p': 1,
+            'repetition_penalty': 1.1,
+            'top_k': 0,
+            'min_length': 0,
+            'no_repeat_ngram_size': 0,
+            'num_beams': 1,
+            'penalty_alpha': 0,
+            'length_penalty': 1,
+            'early_stopping': False,
+            'seed': seed,
+            'add_bos_token': True,
+            'truncation_length': 4096,
+            'ban_eos_token': False,
+            'skip_special_tokens': True,
+            'stopping_strings': []
         }
-        response = requests.post(f"{CFG.AI_PROVIDER_URI}/api/v1/generate", json={"data": [json.dumps([prompt, params])]})
-        data = response.json()
-        if "data" in data:
-            data = data["data"]
-            data = re.sub(r"(?<!\\)\\(?!n)", "", data[0])
+        response = requests.post(f"{CFG.AI_PROVIDER_URI}/api/v1/generate", json=params)
+        data = None
+        if response.status_code == 200:
+            data = response.json()['results'][0]['text']
+            data = re.sub(r"(?<!\\)\\(?!n)", "", data)
         return data
