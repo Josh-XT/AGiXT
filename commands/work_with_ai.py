233990ef038dc055b3316ad7ac15a5eded2eb67a
Josh XT <josh@devxt.com>
2023-04-25 20:46:20 -0400

    Maybe fixed!

--- commands/work_with_ai.py
+++ commands/work_with_ai.py
@@ -7,13 +7,12 @@ CFG = Config()
 class work_with_ai(Commands):
     def __init__(self):
         agents = CFG.get_agents()
-        if agents == None:
-            agents = []
         self.commands = {}
-        for agent in agents:
-            if "name" in agent:
-                name = f" AI Agent {agent.name}"
-                self.commands.update({f"Ask{name}": self.ask, f"Instruct{name}": self.instruct})
+        if agents != None:
+            for agent in agents:
+                if "name" in agent:
+                    name = f" AI Agent {agent['name']}"
+                    self.commands.update({f"Ask{name}": self.ask, f"Instruct{name}": self.instruct})
 
     def ask(self, prompt: str) -> str:
         response = AgentLLM().run(prompt, commands_enabled=False)
