e8181ff03d5d800e43198cced28b9ac8c3ef5815
Josh XT <josh@devxt.com>
2023-04-26 20:46:36 -0600

    Fixed SWR endpoint names so the section homepages work properly;

--- frontend/pages/agent/index.js
+++ frontend/pages/agent/index.js
@@ -6,7 +6,7 @@ import ContentSWR from '@/components/content/ContentSWR';
 import DoubleSidedMenu from '@/components/content/DoubleSidedMenu';
 import MenuAgentList from '@/components/agent/AgentList';
 export default function Home() {
-  const docs = useSWR('docs/prompt', async () => (await axios.get("https://raw.githubusercontent.com/Josh-XT/Agent-LLM/main/docs/concepts/AGENT.md")).data);
+  const docs = useSWR('docs/agent', async () => (await axios.get("https://raw.githubusercontent.com/Josh-XT/Agent-LLM/main/docs/concepts/AGENT.md")).data);
   const agents = useSWR('agent', async () => (await axios.get(`${process.env.NEXT_PUBLIC_API_URI ?? 'http://localhost:7437'}/api/agent`)).data.agents);
   return <DoubleSidedMenu title={"Agent Homepage"} leftHeading={"Agents"} leftSWR={agents} leftMenu={MenuAgentList} rightHeading={null} rightSWR={null} rightMenu={null} content={() => <Container>
     <ContentSWR swr={docs} content={({ data }) => (
