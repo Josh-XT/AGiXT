58444712e571b09674a92c752d6802bed3800d0e
James Grieve <jamesonrgrieve@gmail.com>
2023-04-28 12:03:00 -0600

    Fixed providers;

--- frontend/pages/provider/index.js
+++ frontend/pages/provider/index.js
@@ -2,17 +2,16 @@ import axios from 'axios';
 import useSWR from 'swr';
 import ReactMarkdown from 'react-markdown';
 import { Container } from '@mui/material';
-import ContentSWR from '@/components/data/ContentSWR';
-import DoubleSidedMenu from '@/components/content/PopoutDrawerWrapper';
-import ProviderList from '@/components/systems/provider/ProviderList';
+import ContentSWR from '../../components/data/ContentSWR';
+import PopoutDrawerWrapper from '../../components/menu/PopoutDrawerWrapper';
+import ProviderList from '../../components/systems/provider/ProviderList';
 export default function Home() {
   const docs = useSWR('docs/provider', async () => (await axios.get("https://raw.githubusercontent.com/Josh-XT/Agent-LLM/main/docs/concepts/PROVIDER.md")).data);
   const providers = useSWR('provider', async () => (await axios.get(`${process.env.NEXT_PUBLIC_API_URI ?? 'http://localhost:7437'}/api/provider`)).data.providers);
-  return <DoubleSidedMenu title={"Provider Homepage"} leftHeading={"Providers"} leftSWR={providers} leftMenu={ProviderList} rightHeading={null} rightSWR={null} rightMenu={null} content={() => <Container>
+  return <PopoutDrawerWrapper title={"Provider Homepage"} leftHeading={"Providers"} leftSWR={providers} leftMenu={ProviderList} rightHeading={null} rightSWR={null} rightMenu={null}>
+    <Container>
     <ContentSWR swr={docs} content={({ data }) => (
       <ReactMarkdown>{data}</ReactMarkdown>
     )} />;
-  </Container>} />;
-
-;
+  </Container> </PopoutDrawerWrapper>
 }
