d1d91cc8dfb7a9ae6f10ea53d8b06cad4ba73c9d
James Grieve <jamesonrgrieve@gmail.com>
2023-04-27 18:09:25 -0600

    Started building chain files;

--- frontend/components/chain/ChainControl.js
+++ frontend/components/chain/ChainControl.js
@@ -1,3 +1,13 @@
-export default function ChainControl({ data }) {
-    return <p>Chain Control</p>;
+import { useRouter } from 'next/router';
+import axios from 'axios';
+import useSWR from 'swr';
+import ChainPanel from './AgentPanel';
+import DoubleSidedMenu from '@/components/content/DoubleSidedMenu';
+import ChainList from './ChainList';
+export default function AgentControl({ data }) {
+    const chainName = useRouter().query.agent;
+    const chains = useSWR('chain', async () => (await axios.get(`${process.env.NEXT_PUBLIC_API_URI ?? 'http://localhost:7437'}/api/chain`)).data.chains);
+    return <DoubleSidedMenu title={"Manage Chain \""+chainName+"\""} leftHeading={"Agents"} leftSWR={chains} leftMenu={ChainList} rightHeading={null} rightSWR={null} rightMenu={null} content={ChainPanel} />;
 }
+
+
