62dff7049a994cb00436e888d7387012fe5038ce
Josh XT <josh@devxt.com>
2023-04-28 06:56:48 -0400

    Add Huggingchat Provider

---
+++ provider/huggingchat.py
@@ -0,0 +1,55 @@
+from Config import Config
+from requests.sessions import Session
+from json import loads
+
+CFG = Config()
+
+
+class AIProvider:
+    def __init__(self) -> None:
+        if CFG.AI_PROVIDER_URI == "huggingchat":
+            self.url = None
+            session = Session()
+            session.get(url="https://huggingface.co/chat/")
+            self.session = session
+            res = self.session.post(url="https://huggingface.co/chat/conversation")
+            assert res.status_code == 200, "Failed to create new conversation"
+            self.conversation_id = res.json()["conversationId"]
+            self.url = (
+                f"https://huggingface.co/chat/conversation/{self.conversation_id}"
+            )
+
+    def instruct(self, prompt: str) -> str:
+        res = self.session.post(
+            url=self.url,
+            json={
+                "inputs": prompt,
+                "parameters": {
+                    "temperature": float(CFG.AI_TEMPERATURE),
+                    "top_p": 0.95,
+                    "repetition_penalty": 1.2,
+                    "top_k": 50,
+                    "truncate": 2048,
+                    "watermark": False,
+                    "max_new_tokens": int(CFG.MAX_TOKENS),
+                    "stop": ["<|endoftext|>"],
+                    "return_full_text": False,
+                },
+                "stream": True,
+                "options": {"use_cache": False},
+            },
+            stream=True,
+        )
+
+        assert res.status_code == 200, "Failed to send message"
+
+        last_response = None
+        for chunk in res.iter_content(chunk_size=None):
+            if chunk:
+                data = loads(chunk.decode("utf-8")[5:])
+                if "error" not in data:
+                    last_response = data
+                else:
+                    print("error: ", data["error"])
+                    break
+        return last_response
