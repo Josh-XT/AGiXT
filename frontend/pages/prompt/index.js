f30c07db90722986659cac2ea19c0d88a5ac161d
James Grieve <jamesonrgrieve@gmail.com>
2023-04-28 12:28:20 -0600

    Fixed prompts docs;

--- frontend/pages/prompt/index.js
+++ frontend/pages/prompt/index.js
@@ -8,7 +8,7 @@ import PromptList from '@/components/systems/prompt/PromptList';
 import {useRouter} from 'next/router';
 export default function Home() {
   const router = useRouter();
-  const docs = useSWR('docs/prompt', async () => (await axios.get("https://raw.githubusercontent.com/Josh-XT/Agent-LLM/main/docs/main/concepts/PROMPT.md")).data);
+  const docs = useSWR('docs/prompt', async () => (await axios.get("https://raw.githubusercontent.com/Josh-XT/Agent-LLM/main/docs/concepts/PROMPT.md")).data);
   const prompts = useSWR('prompt', async () => (await axios.get(`${process.env.NEXT_PUBLIC_API_URI ?? 'http://localhost:7437'}/api/prompt`)).data.prompts);
   return <PopoutDrawerWrapper title={"Prompt Homepage"} leftHeading={"Prompts"} leftSWR={prompts} leftMenu={PromptList} rightHeading={null} rightSWR={null} rightMenu={null}> 
   <Container>
--- frontend/pages/provider/[provider].js
+++ frontend/pages/provider/[provider].js
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router';
 import axios from 'axios';
 import useSWR from 'swr';
-import ContentSWR from '@/components/data/ContentSWR';
+import ContentSWR from '../../components/data/ContentSWR';
 import PopoutDrawerWrapper from '../../components/menu/PopoutDrawerWrapper';
 import ProviderList from '../../components/systems/provider/ProviderList';
 import { Container } from '@mui/material';
@@ -11,12 +11,9 @@ export default function Provider() {
   const docs = useSWR('docs/provider/' + providerName, async () => (await axios.get("https://raw.githubusercontent.com/Josh-XT/Agent-LLM/main/docs/providers/" + providerName.toUpperCase() + ".md")).data);
   const providers = useSWR('provider', async () => (await axios.get(`${process.env.NEXT_PUBLIC_API_URI ?? 'http://localhost:7437'}/api/provider`)).data.providers);
   return <PopoutDrawerWrapper title={"Provider Homepage"} leftHeading={"Providers"} leftSWR={providers} leftMenu={ProviderList} rightHeading={null} rightSWR={null} rightMenu={null} >
-
     <Container>
       <ContentSWR swr={docs} content={({ data }) => (
         <ReactMarkdown>{data}</ReactMarkdown>
       )} />
     </Container></PopoutDrawerWrapper>;
-
-
 }
