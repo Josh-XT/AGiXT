828aa7f3b30e0415131d5a97d4be3a6da38188c6
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2023-05-03 05:26:10 -0400

    Remove if statement breaking bard (#202)

--- provider/bard.py
+++ provider/bard.py
@@ -7,8 +7,7 @@ CFG = Config()
 class BardProvider:
     def __init__(self, BARD_TOKEN: str = "", **kwargs):
         self.requirements = ["GoogleBard"]
-        if CFG.AI_PROVIDER.lower() == "bard":
-            self.chatbot = Chatbot(BARD_TOKEN)
+        self.chatbot = Chatbot(BARD_TOKEN)
 
     def instruct(self, prompt):
         response = self.chatbot.ask(prompt)
