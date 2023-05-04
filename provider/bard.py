89221971886a58f9daa49b2ebdf62b88d4f7d8ad
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2023-05-03 20:54:32 -0400

    Rework bard provider (#221)

--- provider/bard.py
+++ provider/bard.py
@@ -7,8 +7,12 @@ CFG = Config()
 class BardProvider:
     def __init__(self, BARD_TOKEN: str = "", **kwargs):
         self.requirements = ["GoogleBard"]
-        self.chatbot = Chatbot(BARD_TOKEN)
+        self.BARD_TOKEN = BARD_TOKEN
 
     def instruct(self, prompt):
-        response = self.chatbot.ask(prompt)
-        return response["content"].replace("\n", "\n")
+        try:
+            chatbot = Chatbot(self.BARD_TOKEN)
+            response = chatbot.ask(prompt)
+            return response["content"].replace("\n", "\n")
+        except Exception as e:
+            return f"Bard Error: {e}"
