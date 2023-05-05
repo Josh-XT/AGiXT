d8d3b1ae2d95b98b7f9e854d43214dbd148cf905
Luis Gustavo Couto Wacker <Lgcwacker@gmail.com>
2023-05-05 17:37:43 -0300

    Fix huggingchat provider (#230)
    * Add new POST that allows to create conversation
    
    Signed-off-by: Luis Gustavo Couto Wacker <Lgcwacker@gmail.com>
    
    * lint
    
    Signed-off-by: Luis Gustavo Couto Wacker <Lgcwacker@gmail.com>
    
    ---------
    
    Signed-off-by: Luis Gustavo Couto Wacker <Lgcwacker@gmail.com>

--- provider/huggingchat.py
+++ provider/huggingchat.py
@@ -12,11 +12,18 @@ class HuggingchatProvider:
         session = Session()
         session.get(url="https://huggingface.co/chat/")
         res = session.post(
+            url="https://huggingface.co/chat/settings",
+            data={"ethicsModalAccepted": True},
+        )
+        assert res.status_code == 200, "Failed to accept ethics modal"
+
+        res = session.post(
             url="https://huggingface.co/chat/conversation",
             json={"model": "OpenAssistant/oasst-sft-6-llama-30b-xor"},
             headers={"Content-Type": "application/json"},
         )
         assert res.status_code == 200, "Failed to create new conversation"
+
         conversation_id = res.json()["conversationId"]
         url = f"https://huggingface.co/chat/conversation/{conversation_id}"
         max_tokens = int(self.MAX_TOKENS) - len(prompt)
