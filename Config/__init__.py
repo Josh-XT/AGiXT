9cb25dd68830e777672de60ed6e9d4f596516965
Luis Gustavo Wacker <lgcwacker@gmail.com>
2023-05-07 16:24:39 -0300

    Fix envar default value

--- Config/__init__.py
+++ Config/__init__.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 class Config:
     def __init__(self):
-        self.WORKING_DIRECTORY = os.getenv("WORKING_DIRECTORY", "WORKSPACE")
+        self.WORKING_DIRECTORY = os.getenv("WORKING_DIRECTORY", "/WORKSPACE")
         self._create_directory_if_not_exists(self.WORKING_DIRECTORY)
         self.WORKING_DIRECTORY_RESTRICTED = os.getenv(
             "WORKING_DIRECTORY_RESTRICTED", True
