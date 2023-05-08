89e847424ddea44a057b4f0d950fc091d9553a17
Josh XT <josh@devxt.com>
2023-05-07 21:36:15 -0400

    Increment version

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ CFG = Config()
 app = FastAPI(
     title="Agent-LLM",
     description="Agent-LLM is an Artificial Intelligence Automation platform for creating and managing AI agents.",
-    version="1.1.5-alpha",
+    version="1.1.8-alpha",
 )
 agent_threads = {}
 agent_stop_events = {}
