eb1744ac64c11bd12b7c7841f548363209d8c850
Josh XT <josh@devxt.com>
2023-05-08 23:57:07 -0400

    Add Bing Provider

---
+++ provider/bing.py
@@ -0,0 +1,19 @@
+from EdgeGPT import Chatbot, ConversationStyle
+
+
+class BingProvider:
+    def __init__(self, **kwargs):
+        self.requirements = ["EdgeGPT"]
+
+    def instruct(self, prompt, tokens: int = 0):
+        try:
+            bot = Chatbot.create(cookies_path="./cookies.json")
+            response = bot.ask(
+                prompt,
+                ConversationStyle.creative,
+                wss_link="wss://sydney.bing.com/sydney/ChatHub",
+            )
+            bot.close()
+            return response
+        except Exception as e:
+            return f"EdgeGPT Error: {e}"
