04048d1709c721927c69f3dafbbe7b2005228e44
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2023-05-09 20:51:17 -0400

    Add gpt4all from #227 (#283)

---
+++ provider/gpt4all.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+from Config import Config
+
+try:
+    from nomic.gpt4all import GPT4All
+except ImportError:
+    print("Failed to import gpt4all.")
+
+CFG = Config()
+
+
+class AIProvider:
+    def __init__(self):
+        if not CFG.MODEL_PATH and not Path(CFG.MODEL_PATH).exists():
+            raise Exception(
+                "No MODEL_PATH specified. Download the Model on your machine and mount the directory.\n"
+                "Re-downloading the file every time causes server load. Exiting"
+            )
+        self.model_path = Path(CFG.MODEL_PATH)
+        self.model = GPT4All(model_path=self.model_path)
+        self.model.open()
+        # TODO: Need to research to add temperature, no obvious flag.
+
+    def instruct(self, prompt):
+        return self.model.prompt(prompt)
