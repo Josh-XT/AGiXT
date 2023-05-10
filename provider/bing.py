0de9b5c72a6f01e3039785138378adf178d2b726
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2023-05-09 20:07:20 -0400

    Adding styles based on temp (#280)

--- provider/bing.py
+++ provider/bing.py
@@ -2,15 +2,21 @@ from EdgeGPT import Chatbot, ConversationStyle
 
 
 class BingProvider:
-    def __init__(self, **kwargs):
+    def __init__(self, AI_TEMPERATURE: float = 0.7, **kwargs):
         self.requirements = ["EdgeGPT"]
+        if AI_TEMPERATURE >= 0.7:
+            self.style = ConversationStyle.creative
+        if AI_TEMPERATURE <= 0.3 and AI_TEMPERATURE >= 0.0:
+            self.style = ConversationStyle.precise
+        if AI_TEMPERATURE >= 0.4 and AI_TEMPERATURE <= 0.6:
+            self.style = ConversationStyle.balanced
 
     def instruct(self, prompt, tokens: int = 0):
         try:
             bot = Chatbot.create(cookie_path="./cookies.json")
             response = bot.ask(
                 prompt,
-                ConversationStyle.creative,
+                self.style,
                 wss_link="wss://sydney.bing.com/sydney/ChatHub",
             )
             bot.close()
