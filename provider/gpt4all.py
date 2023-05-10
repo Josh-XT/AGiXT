24dda4bbd24c935ac2a72078a2e1694f91e0fadb
Josh XT <josh@devxt.com>
2023-05-10 04:17:08 -0400

    Fix typo

--- provider/gpt4all.py
+++ provider/gpt4all.py
@@ -10,7 +10,7 @@ except ImportError:
 CFG = Config()
 
 
-class AIProvider:
+class Gpt4allProvider:
     def __init__(self):
         if not CFG.MODEL_PATH and not Path(CFG.MODEL_PATH).exists():
             raise Exception(
