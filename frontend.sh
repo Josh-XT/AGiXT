45de8285a3fc7e3aa793d79fc7c8b301a444f86f
Josh XT <josh@devxt.com>
2023-05-09 20:40:13 -0400

    Updates

--- frontend.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Get the local IP address
-LOCAL_IP=$(hostname -I | awk '{print $1}')
-
-# Run the Docker command
-docker run -it --pull always -p 3000:3000 -e NEXT_PUBLIC_API_URI=http://$LOCAL_IP:7437 ghcr.io/jamesonrgrieve/agent-llm-frontend:main
