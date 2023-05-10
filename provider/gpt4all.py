bd780b6d3c699fd21724b6ad554c6a42a287976a
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2023-05-10 05:35:57 -0400

    Fix model (#288)

--- provider/gpt4all.py
+++ provider/gpt4all.py
@@ -9,19 +9,13 @@ except ImportError:
 class Gpt4allProvider:
     def __init__(
         self,
-        MODEL_PATH: str = "",
+        MODEL_NAME: str = "gpt4all-lora-quantized",
         MAX_TOKENS: int = 2000,
         AI_MODEL: str = "default",
         AI_TEMPERATURE: float = 0.7,
         **kwargs,
     ):
-        if not MODEL_PATH and not Path(MODEL_PATH).exists():
-            raise Exception(
-                "No MODEL_PATH specified. Download the Model on your machine and mount the directory.\n"
-                "Re-downloading the file every time causes server load. Exiting"
-            )
-        self.model_path = Path(MODEL_PATH)
-        self.model = GPT4All(model_path=self.model_path)
+        self.model = GPT4All(model=MODEL_NAME)
         self.model.open()
         self.MAX_TOKENS = MAX_TOKENS
         self.AI_MODEL = AI_MODEL
