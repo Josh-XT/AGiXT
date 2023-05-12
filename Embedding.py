e0b942a4fb18d16126a784d93b402918f963ec80
Josh XT <josh@devxt.com>
2023-05-12 09:07:26 -0400

    Add Google Palm

--- Embedding.py
+++ Embedding.py
@@ -23,11 +23,47 @@ class Embedding(Agent):
             api_key=self.CFG.AGENT_CONFIG["settings"]["OPENAI_API_KEY"],
         )
 
+    def google_palm(self):
+        self.MAX_EMBEDDING_TOKENS = 3072
+        self.EMBEDDING_AGENT = GooglePalmEmbeddingFunction(
+            api_key=self.CFG.AGENT_CONFIG["settings"]["GOOGLE_API_KEY"],
+        )
+
     def google_vertex(self):
         self.MAX_EMBEDDING_TOKENS = 3072
         self.EMBEDDING_AGENT = GoogleVertexEmbeddingFunction(
             api_key=self.CFG.AGENT_CONFIG["settings"]["GOOGLE_API_KEY"],
         )
+
+
+class GooglePalmEmbeddingFunction(EmbeddingFunction):
+    """To use this EmbeddingFunction, you must have the google.generativeai Python package installed and have a PaLM API key."""
+
+    def __init__(self, api_key: str, model_name: str = "models/embedding-gecko-001"):
+        if not api_key:
+            raise ValueError("Please provide a PaLM API key.")
+
+        if not model_name:
+            raise ValueError("Please provide the model name.")
+
+        try:
+            import google.generativeai as palm
+        except ImportError:
+            raise ValueError(
+                "The Google Generative AI python package is not installed. Please install it with `pip install google-generativeai`"
+            )
+
+        palm.configure(api_key=api_key)
+        self._palm = palm
+        self._model_name = model_name
+
+    def __call__(self, texts: Documents) -> Embeddings:
+        return [
+            self._palm.generate_embeddings(model=self._model_name, text=text)[
+                "embedding"
+            ]
+            for text in texts
+        ]
 
 
 class GoogleVertexEmbeddingFunction(EmbeddingFunction):
