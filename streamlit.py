ca1dc5fb49a792cd782896919fa8ba8ae5d5ee3c
Josh XT <josh@devxt.com>
2023-05-13 17:28:17 -0400

    Updates to Streamlit chat

--- streamlit.py
+++ streamlit.py
@@ -191,7 +191,7 @@ elif main_selection == "Chat":
 
     smart_chat_toggle = st.checkbox("Enable Smart Chat")
 
-    chat_history = st.session_state.get("chat_history", [])
+    st.session_state.setdefault("chat_history", [])
 
     chat_container = st.container()
 
@@ -199,7 +199,7 @@ elif main_selection == "Chat":
         agent = AgentLLM(agent_name)
 
         with chat_container:
-            for chat in chat_history:
+            for chat in st.session_state.chat_history:
                 if chat["sender"] == "User":
                     st.markdown(
                         f'<div style="text-align: left; margin-bottom: 5px;"><strong>User:</strong> {chat["message"]}</div>',
@@ -211,21 +211,24 @@ elif main_selection == "Chat":
                         unsafe_allow_html=True,
                     )
 
-        chat_prompt = st.text_input("Enter your message")
+        chat_prompt = st.text_input("Enter your message", key="chat_prompt")
         send_button = st.button("Send Message")
 
         if send_button:
             if agent_name and chat_prompt:
-                chat_history.append({"sender": "User", "message": chat_prompt})
+                st.session_state.chat_history.append(
+                    {"sender": "User", "message": chat_prompt}
+                )
                 if smart_chat_toggle:
                     response = agent.smart_chat(chat_prompt, shots=3)
                 else:
                     response = agent.run(chat_prompt, prompt="Chat", context_results=6)
-                chat_history.append({"sender": "Agent", "message": response})
-                st.session_state.chat_history = chat_history
+                st.session_state.chat_history.append(
+                    {"sender": "Agent", "message": response}
+                )
                 chat_container.empty()
                 with chat_container:
-                    for chat in chat_history:
+                    for chat in st.session_state.chat_history:
                         if chat["sender"] == "User":
                             st.markdown(
                                 f'<div style="text-align: left; margin-bottom: 5px;"><strong>User:</strong> {chat["message"]}</div>',
