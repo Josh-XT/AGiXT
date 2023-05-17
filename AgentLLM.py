6563671f3bf41e3eb3f291051da7bf35955cfd92
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2023-05-17 06:35:19 -0400

    Modifications to memory system (#358)
    * Modifications to memory system
    
    * Update Memories.py
    
    Signed-off-by: Josh XT <102809327+Josh-XT@users.noreply.github.com>
    
    * Update Memories.py
    
    Signed-off-by: Josh XT <102809327+Josh-XT@users.noreply.github.com>
    
    * Force url format
    
    ---------
    
    Signed-off-by: Josh XT <102809327+Josh-XT@users.noreply.github.com>

--- AgentLLM.py
+++ AgentLLM.py
@@ -25,7 +25,7 @@ def run_asyncio_coroutine(coro):
 
 
 class AgentLLM:
-    def __init__(self, agent_name: str = "AgentLLM", primary_objective=None):
+    def __init__(self, agent_name: str = "Agent-LLM", primary_objective=None):
         self.agent_name = agent_name
         self.CFG = Agent(self.agent_name)
         self.primary_objective = primary_objective
@@ -482,6 +482,7 @@ class AgentLLM:
                     url = link["href"]
                 else:
                     url = link
+                url = re.sub(r"^.*?(http)", r"http", url)
                 print(f"Scraping: {url}")
                 collected_data, link_list = await self.browse_website(url)
                 if collected_data is not None:
