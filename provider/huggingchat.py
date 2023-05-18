4728036c71feef6e785a7f71b6aa2b93ba7527f1
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2023-05-18 07:06:30 -0400

    Switch to using hugchat module (#374)

--- provider/huggingchat.py
+++ provider/huggingchat.py
@@ -1,6 +1,4 @@
-import time
-from requests.sessions import Session
-import json
+from hugchat import hugchat
 
 
 class HuggingchatProvider:
@@ -9,80 +7,27 @@ class HuggingchatProvider:
         AI_TEMPERATURE: float = 0.7,
         MAX_TOKENS: int = 2000,
         AI_MODEL: str = "openassistant",
+        HUGGINGCHAT_COOKIE_PATH: str = "./huggingchat-cookies.json",
         **kwargs,
     ):
         self.requirements = []
         self.AI_TEMPERATURE = AI_TEMPERATURE
         self.MAX_TOKENS = int(MAX_TOKENS)
         self.AI_MODEL = AI_MODEL
-        # get cookie from huggingchat-cookies.json
-        self.cookie = None
+        self.HUGGINGCHAT_COOKIE_PATH = HUGGINGCHAT_COOKIE_PATH
 
     def instruct(self, prompt: str, tokens: int = 0) -> str:
-        self.cookie_path = "./huggingchat-cookies.json"
-        with open(self.cookie_path, "r") as f:
-            self.cookie = json.load(f)
-
-        session = Session()
-        session.get(url="https://huggingface.co/chat/")
-        headers = {"Content-Type": "application/json", "Cookie": self.cookie}
-        res = session.post(
-            url="https://huggingface.co/chat/conversation",
-            json={"model": self._get_model_name(self.AI_MODEL)},
-            headers=headers,
-        )
-        assert res.status_code == 200, "Failed to create new conversation"
-
-        conversation_id = res.json()["conversationId"]
-        url = f"https://huggingface.co/chat/conversation/{conversation_id}"
-        max_new_tokens = int(self.MAX_TOKENS) - tokens - 428
-        res = session.post(
-            url=url,
-            headers=headers,
-            json={
-                "inputs": prompt,
-                "parameters": {
-                    "temperature": float(self.AI_TEMPERATURE),
-                    "top_p": 0.95,
-                    "repetition_penalty": 1.2,
-                    "top_k": 50,
-                    "truncate": 1024,
-                    "watermark": False,
-                    "max_new_tokens": max_new_tokens,
-                    "stop": ["<|endoftext|>"],
-                    "return_full_text": False,
-                },
-                "stream": False,
-                "options": {"use_cache": False},
-            },
-            stream=False,
-        )
         try:
-            data = res.json()
-            data = data[0] if data else {}
-        except ValueError:
-            print("Invalid JSON response")
-            data = {}
-        except:
-            if data.get("error_type", None) == "overloaded":
-                print(
-                    "Provider says that it is overloaded, waiting 3 seconds and trying again"
-                )
-                # @Note: if this is kept in the repo, the delay should be configurable
-                time.sleep(3)
-                return self.instruct(prompt)
-            else:
-                print("Unknown error")
-                print(res.text)
-                data = {}
-
-        return data.get("generated_text", "")
-
-    def _get_model_name(self, ai_model: str):
-        """Returns a model name based on the AI_MODEL"""
-
-        if ai_model == "openassistant":
-            model_name = "OpenAssistant/oasst-sft-6-llama-30b-xor"
-        elif ai_model == "starcoderbase":
-            model_name = "bigcode/starcoderbase"
-        return model_name
+            chatbot = hugchat.ChatBot(cookie_path=self.HUGGINGCHAT_COOKIE_PATH)
+            max_new_tokens = int(self.MAX_TOKENS) - tokens - 428
+            id = chatbot.new_conversation()
+            response = chatbot.chat(
+                text=prompt,
+                temperature=self.AI_TEMPERATURE,
+                max_new_tokens=max_new_tokens,
+                stream=False,
+            )
+            return response
+        except Exception as e:
+            print(e)
+            return f"HuggingChat Provider Failure: {e}."
