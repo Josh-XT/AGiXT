e36e187b0e5644fa7f9d3fa9b0244b0bf51322f3
Josh XT <josh@devxt.com>
2023-05-18 17:00:31 -0400

    Updates

--- setup.py
+++ setup.py
@@ -8,11 +8,13 @@ with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
 # Get requirements from requirements.txt to a list
 with open(os.path.join(this_directory, "requirements.txt"), encoding="utf-8") as f:
     install_requires = f.read().splitlines()
-
+# Get version from version file
+with open(os.path.join(this_directory, "version"), encoding="utf-8") as f:
+    version = f.read().splitlines()[0]
 
 setup(
     name="agent-llm",
-    version="1.1.36-beta",
+    version=version,
     description="An Artificial Intelligence Automation Platform. AI Instruction management from various providers, has an adaptive memory, and a versatile plugin system with many commands including web browsing. Supports many AI providers and models and growing support every day.",
     long_description=long_description,
     long_description_content_type="text/markdown",  # This should match the format of your README
