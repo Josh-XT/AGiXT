cc866aac4d4af79d1d5e5fe6990dd372cd48530b
rm4453 <rmattingly4415@gmail.com>
2023-05-20 10:32:10 -0600

    Add Login Redirect Profile Page
    Signed-off-by: rm4453 <rmattingly4415@gmail.com>

--- src/agixt/pages/Profile.py
+++ src/agixt/pages/Profile.py
@@ -5,6 +5,26 @@ from auth_libs.Cfig import Cfig
 
 CFG = Cfig()
 
+# Check if the user is logged in
+if not st.session_state.get("logged_in"):
+    # Redirect to the login page using JavaScript
+    redirect_code = '''
+        <script>
+            window.location.href = window.location.origin + "/Login"
+        </script>
+    '''
+    st.markdown(redirect_code, unsafe_allow_html=True)
+
+def logout_button():
+    """
+    Renders the logout button.
+    """
+    if st.button("Logout"):
+        # Clear session state and redirect to the login page
+        st.session_state.clear()
+        st.experimental_rerun()  # Redirect to the login page
+
+logout_button()
 
 def logout_button():
     """
