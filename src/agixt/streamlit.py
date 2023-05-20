718ee1233d194e87560140369fadf6b53c0cef0f
Josh XT <josh@devxt.com>
2023-05-20 01:10:27 -0400

    Add docs

--- src/agixt/streamlit.py
+++ src/agixt/streamlit.py
@@ -6,3 +6,11 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
 )
+
+st.markdown(
+    """
+    <iframe src="https://josh-xt.github.io/AGiXT/" width="100%" height="500">
+    </iframe>
+    """,
+    unsafe_allow_html=True,
+)
