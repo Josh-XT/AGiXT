f0bd8e3c5e07e82044d25f0dcc755649de7f6c7a
Josh XT <josh@devxt.com>
2023-05-20 12:27:30 -0400

    Error handling

--- src/agixt/pages/Profile.py
+++ src/agixt/pages/Profile.py
@@ -5,6 +5,7 @@ from auth_libs.Cfig import Cfig
 
 CFG = Cfig()
 
+
 def logout_button():
     """
     Renders the logout button.
@@ -14,7 +15,6 @@ def logout_button():
         st.session_state.clear()
         st.experimental_rerun()  # Redirect to the login page
 
-logout_button()
 
 # Toggle public registrations
 def toggle_value(val, to_toggle="allow_reg"):
@@ -30,6 +30,7 @@ def toggle_value(val, to_toggle="allow_reg"):
 
     st.session_state["regCheck"] = val
 
+
 # Change password form
 def change_password_form():
     st.write("Change Password")
@@ -40,10 +41,15 @@ def change_password_form():
         user_data = load_users()
         for user in user_data["users"]:
             if user["email"] == st.session_state["email"]:
-                if bcrypt.checkpw(current_password.encode('utf-8'), user["password_hash"].encode('utf-8')):
+                if bcrypt.checkpw(
+                    current_password.encode("utf-8"),
+                    user["password_hash"].encode("utf-8"),
+                ):
                     if new_password == confirm_password:
-                        hashed_password = bcrypt.hashpw(new_password.encode('utf-8'), bcrypt.gensalt())
-                        user["password_hash"] = hashed_password.decode('utf-8')
+                        hashed_password = bcrypt.hashpw(
+                            new_password.encode("utf-8"), bcrypt.gensalt()
+                        )
+                        user["password_hash"] = hashed_password.decode("utf-8")
                         save_user_data(user_data)
                         st.success("Password changed successfully!")
                     else:
@@ -51,6 +57,7 @@ def change_password_form():
                 else:
                     st.error("Incorrect current password.")
                 break
+
 
 # Create user form (admin only)
 def create_user_form():
@@ -74,60 +81,91 @@ def create_user_form():
                     st.error("User with the same email already exists.")
                     return
 
-            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
             new_user = {
                 "email": email,
-                "password_hash": hashed_password.decode('utf-8'),
-                "admin": "False"
+                "password_hash": hashed_password.decode("utf-8"),
+                "admin": "False",
             }
             user_data["users"].append(new_user)
             save_user_data(user_data)
             st.success("User created successfully!")
 
-admin_email = CFG.get_admin_email()
-if st.session_state["email"] == admin_email and CFG.auth != "SUL":
-    setup_cfg = CFG.load_cfg_data()
-    for config in setup_cfg["config"]:
-        allow_reg = config["allow_reg"]
-        allow_uac = config["allow_uac"]
-        allow_ucp = config["allow_ucp"]
-        allow_uaa = config["allow_uaa"]
 
-    st.session_state["regCheck"] = allow_reg
+try:
+    admin_email = CFG.get_admin_email()
+    logout_button()
+    if st.session_state["email"] == admin_email and CFG.auth != "SUL":
+        setup_cfg = CFG.load_cfg_data()
+        for config in setup_cfg["config"]:
+            allow_reg = config["allow_reg"]
+            allow_uac = config["allow_uac"]
+            allow_ucp = config["allow_ucp"]
+            allow_uaa = config["allow_uaa"]
 
-    if allow_reg == True:
-        ar_opposite = False
-    else:
-        ar_opposite = True
+        st.session_state["regCheck"] = allow_reg
 
-    if allow_uac == True:
-        uac_opposite = False
-    else:
-        uac_opposite = True
-
-    if allow_ucp == True:
-        ucp_opposite = False
-    else:
-        ucp_opposite = True
-
-    if allow_uaa == True:
-        uaa_opposite = False
-    else:
-        uaa_opposite = True
-
-    col1, col2 = st.columns(2)
-
-    with col1:
-        if CFG.auth == "MPBR":
-            st.checkbox("Enable Public Registration", value=allow_reg, on_change=toggle_value, args=(ar_opposite, "allow_reg"))
+        if allow_reg == True:
+            ar_opposite = False
         else:
-            st.checkbox("Enable Public Registration", value=allow_reg, on_change=toggle_value, args=(ar_opposite, "allow_reg"), disabled=True)
-        st.checkbox("Enable Users to Create Public Agents", value=allow_uac, on_change=toggle_value, args=(uac_opposite, "allow_uac"))
+            ar_opposite = True
 
-    with col2:
-        st.checkbox("Enable Users To Create Private Agents", value=allow_ucp, on_change=toggle_value, args=(ucp_opposite, "allow_ucp"))
-        st.checkbox("Enable Users To Use Admin's Agents", value=allow_uaa, on_change=toggle_value, args=(uaa_opposite, "allow_uaa"))
+        if allow_uac == True:
+            uac_opposite = False
+        else:
+            uac_opposite = True
 
-change_password_form()
-if st.session_state["email"] == admin_email and CFG.auth != "SUL":
-    create_user_form()
+        if allow_ucp == True:
+            ucp_opposite = False
+        else:
+            ucp_opposite = True
+
+        if allow_uaa == True:
+            uaa_opposite = False
+        else:
+            uaa_opposite = True
+
+        col1, col2 = st.columns(2)
+
+        with col1:
+            if CFG.auth == "MPBR":
+                st.checkbox(
+                    "Enable Public Registration",
+                    value=allow_reg,
+                    on_change=toggle_value,
+                    args=(ar_opposite, "allow_reg"),
+                )
+            else:
+                st.checkbox(
+                    "Enable Public Registration",
+                    value=allow_reg,
+                    on_change=toggle_value,
+                    args=(ar_opposite, "allow_reg"),
+                    disabled=True,
+                )
+            st.checkbox(
+                "Enable Users to Create Public Agents",
+                value=allow_uac,
+                on_change=toggle_value,
+                args=(uac_opposite, "allow_uac"),
+            )
+
+        with col2:
+            st.checkbox(
+                "Enable Users To Create Private Agents",
+                value=allow_ucp,
+                on_change=toggle_value,
+                args=(ucp_opposite, "allow_ucp"),
+            )
+            st.checkbox(
+                "Enable Users To Use Admin's Agents",
+                value=allow_uaa,
+                on_change=toggle_value,
+                args=(uaa_opposite, "allow_uaa"),
+            )
+
+    change_password_form()
+    if st.session_state["email"] == admin_email and CFG.auth != "SUL":
+        create_user_form()
+except:
+    st.write("You are not logged in.")
