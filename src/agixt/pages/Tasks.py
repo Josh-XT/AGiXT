fe15b732893b8800d6a46ba08f341a43a8737835
Josh XT <josh@devxt.com>
2023-05-20 12:40:49 -0400

    Task status update

--- src/agixt/pages/Tasks.py
+++ src/agixt/pages/Tasks.py
@@ -10,12 +10,13 @@ st.header("Manage Tasks")
 # Check if the user is logged in
 if not st.session_state.get("logged_in"):
     # Redirect to the login page using JavaScript
-    redirect_code = '''
+    redirect_code = """
         <script>
             window.location.href = window.location.origin + "/Login"
         </script>
-    '''
+    """
     st.markdown(redirect_code, unsafe_allow_html=True)
+
 
 def logout_button():
     """
@@ -25,6 +26,7 @@ def logout_button():
         # Clear session state and redirect to the login page
         st.session_state.clear()
         st.experimental_rerun()  # Redirect to the login page
+
 
 logout_button()
 
@@ -59,33 +61,30 @@ if agent_name:
         agent_status = st.session_state.agent_status.get(agent_name, "Not Running")
 
         if agent_status == "Not Running":
-            agent_status = st.session_state.agent_status.get(agent_name, "Not Running")
-
-        if agent_status == "Not Running":
             if st.button("Start Task"):
-                    if agent_name and task_objective:
-                        if agent_name not in CFG.agent_instances:
-                            CFG.agent_instances[agent_name] = AGiXT(agent_name)
-                        stop_event = threading.Event()
-                        st.session_state.st.session_state.agent_stop_events[agent_name] = stop_event
-                        agent_thread = threading.Thread(
-                            target=CFG.agent_instances[agent_name].run_task,
-                            args=(stop_event, task_objective, True, learn_file_path),
-                        )
-                        agent_thread.start()
-                        st.session_state.st.session_state.agent_status[agent_name][agent_name] = "Running"
-                        columns[0].success(f"Task started for agent '{agent_name}'.")
-                    else:
-                        columns[0].error("Agent name and task objective are required.")
-        else:  # agent_status == "Running"        else:  # agent_status == "Running"
-                if st.button("Stop Task"):
-                    if agent_name in st.session_state.st.session_state.agent_stop_events:
-                        st.session_state.    st.session_state.agent_stop_events[agent_name].set()
-                        del st.session_state.st.session_state.agent_stop_events[agent_name]
-                        st.session_state.    st.session_state.agent_status[agent_name][agent_name] = "Not Running"
-                        columns[0].success(f"Task stopped for agent '{agent_name}'.")
-                    else:
-                        columns[0].error("No task is running for the selected agent.")
+                if agent_name and task_objective:
+                    if agent_name not in CFG.agent_instances:
+                        CFG.agent_instances[agent_name] = AGiXT(agent_name)
+                    stop_event = threading.Event()
+                    st.session_state.agent_stop_events[agent_name] = stop_event
+                    agent_thread = threading.Thread(
+                        target=CFG.agent_instances[agent_name].run_task,
+                        args=(stop_event, task_objective, True, learn_file_path),
+                    )
+                    agent_thread.start()
+                    st.session_state.agent_status[agent_name] = "Running"
+                    columns[0].success(f"Task started for agent '{agent_name}'.")
+                else:
+                    columns[0].error("Agent name and task objective are required.")
+        else:  # agent_status == "Running"
+            if st.button("Stop Task"):
+                if agent_name in st.session_state.agent_stop_events:
+                    st.session_state.agent_stop_events[agent_name].set()
+                    del st.session_state.agent_stop_events[agent_name]
+                    st.session_state.agent_status[agent_name] = "Not Running"
+                    columns[0].success(f"Task stopped for agent '{agent_name}'.")
+                else:
+                    columns[0].error("No task is running for the selected agent.")
 
     with col2:
         st.markdown(f"**Status:** {agent_status}")
