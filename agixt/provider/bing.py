69e44bb4013130061ab76adb793ff3815333d72d
Josh XT <josh@devxt.com>
2023-05-21 09:40:19 -0400

    Bing updates

--- agixt/provider/bing.py
+++ agixt/provider/bing.py
@@ -1,5 +1,7 @@
 from EdgeGPT import Chatbot, ConversationStyle
 import asyncio
+import os
+import json
 
 
 class BingProvider:
@@ -15,8 +17,14 @@ class BingProvider:
             self.style = ConversationStyle.balanced
 
     async def ask(self, prompt, tokens: int = 0):
+        cookie_path = "./cookies.json"
+        # Check if cookies exist
+        if os.path.exists(cookie_path):
+            with open(cookie_path, "r") as f:
+                cookies = f.read()
+            cookies = json.loads(cookies)
         try:
-            bot = await Chatbot.create(cookie_path="./cookies.json")
+            bot = await Chatbot.create(cookies=cookies)
             response = await bot.ask(
                 prompt=prompt,
                 conversation_style=self.style,
@@ -27,12 +35,33 @@ class BingProvider:
             return f"EdgeGPT Error: {e}"
 
     def instruct(self, prompt, tokens: int = 0):
-        loop = asyncio.get_event_loop()
-        future = loop.create_future()
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
 
         async def _ask_and_set_result():
             result = await self.ask(prompt, tokens)
-            future.set_result(result)
+            return result
 
-        loop.create_task(_ask_and_set_result())
-        return future.result()
+        response = loop.run_until_complete(_ask_and_set_result())
+
+        # Closing the loop after use
+        loop.close()
+
+        # Extract the text of the bot's message
+        bot_response = ""
+        for message in response.get("item", {}).get("messages", []):
+            if message.get("author") == "bot":
+                bot_response = message.get("text") or message.get("hiddenText")
+
+                # If the response is in an Adaptive Card
+                if "adaptiveCards" in message:
+                    for card in message["adaptiveCards"]:
+                        for item in card.get("body", []):
+                            if item["type"] == "TextBlock":
+                                bot_response = item.get("text")
+
+                # Stop at the first bot message we find
+                if bot_response:
+                    break
+
+        return bot_response
