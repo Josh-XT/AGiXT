232cbbea2ed093997cf9a76889dec87f4fe17811
Josh XT <josh@devxt.com>
2023-05-26 16:56:53 -0400

    Improve Searx

--- agixt/commands/searxng_commands.py
+++ agixt/commands/searxng_commands.py
@@ -1,18 +1,34 @@
 # pip install searx
+import json
+import random
 import requests
 from typing import List
 from Commands import Commands
 
 
-# SearXNG - List of these at https://searx.space/
 class searxng_commands(Commands):
-    def __init__(self, SEARXNG_INSTANCE_URL: str = "https://searx.work", **kwargs):
+    def __init__(self, SEARXNG_INSTANCE_URL: str = "", **kwargs):
         self.SEARXNG_INSTANCE_URL = SEARXNG_INSTANCE_URL
-        if self.SEARXNG_INSTANCE_URL:
-            self.commands = {"Searx Search": self.search_searx}
+        self.commands = {"Use The Search Engine": self.search_searx}
+
+    def searx_servers():
+        try:  # SearXNG - List of these at https://searx.space/
+            url = "https://searx.space/data/instances.json"
+            response = requests.get(url)
+            data = json.loads(response.text)
+            search_engines = list(data["instances"].keys())
+            return search_engines
+        except:
+            return ["https://searx.work"]
 
     def search_searx(self, query: str, category: str = "general") -> List[str]:
-        searx_url = self.SEARXNG_INSTANCE_URL.rstrip("/") + "/search"
+        if self.SEARXNG_INSTANCE_URL == "":
+            searx = self.searx_servers()
+            # Pick a random searx server to use since one was not defined.
+            random_index = random.randint(0, len(searx) - 1)
+            searx_url = searx[random_index].rstrip("/")
+        else:
+            searx_url = self.SEARXNG_INSTANCE_URL.rstrip("/")
         payload = {
             "q": query,
             "categories": category,
@@ -20,7 +36,7 @@ class searxng_commands(Commands):
             "safesearch": 1,
             "format": "json",
         }
-        response = requests.get(searx_url, params=payload)
+        response = requests.get(f"{searx_url}/search", params=payload)
         results = response.json()
         summaries = [
             result["title"] + " - " + result["url"] for result in results["results"]
