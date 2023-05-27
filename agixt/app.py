6ae3720436718b765ee0ee2ed0d5b9f629a85e0b
localagi <132956819+localagi@users.noreply.github.com>
2023-05-27 01:21:59 +0100

    poetry (#465)
    * Update requirements.txt
    
    importing DDG fail fixed
    
    Signed-off-by: localagi <132956819+localagi@users.noreply.github.com>
    
    * changes projct toml
    
    * move to poetry
    
    * updates in poetry, lockfile
    
    * update workflow
    
    * update README
    
    * update poetry file
    
    * pin duckduck go serach to latest version
    
    * update to fixed gpt4free
    
    * fix for packaging in project toml
    
    * get version info from package metadata
    
    * decoupling API version
    
    * remove unused import
    
    * formatting
    
    * readd docs to docker for pyproject to have consistency
    
    * move back to xtekky gpt4free after merge of https://github.com/xtekky/gpt4free/pull/604
    
    * make publish work with optional git dep gpt4free
    
    * use package version for transformers
    
    * make note in the docs
    
    * finalize - regenerate poetry lock
    
    * add info about keyring, an error could occur on servers
    
    * pin gpt to last working version, see https://github.com/xtekky/gpt4free/commit/e35264d3db5dcc6a0e5ff5a4626e658aa9cb9251#r115184795
    
    ---------
    
    Signed-off-by: localagi <132956819+localagi@users.noreply.github.com>
    Co-authored-by: Josh XT <102809327+Josh-XT@users.noreply.github.com>

--- agixt/app.py
+++ agixt/app.py
@@ -12,17 +12,12 @@ from CustomPrompt import CustomPrompt
 from typing import Optional, Dict, List, Any
 from provider import get_provider_options
 from Embedding import get_embedding_providers
-import os
-
-this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, "version"), encoding="utf-8") as f:
-    version = f.read().strip()
 
 CFG = Config()
 app = FastAPI(
     title="AGiXT",
     description="AGiXT is an Artificial Intelligence Automation platform for creating and managing AI agents. Visit the GitHub repo for more information or to report issues. https://github.com/Josh-XT/AGiXT/",
-    version=version,
+    version="1.0.0",  # API version according to https://restfulapi.net/versioning/
     docs_url="/",
 )
 agent_threads = {}
