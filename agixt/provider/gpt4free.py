530a461bb5d5f57754a246ad3fc275fefd81001e
Heiner Lohaus <heiner.lohaus@netformic.com>
2023-05-28 14:51:37 +0200

    Improve gpt4free provider: (#489)
    Add "theb" and remove "poe" service
    Add required account creation
    Fix reading response in some cases
    Improve error messages
    
    Co-authored-by: Josh XT <102809327+Josh-XT@users.noreply.github.com>

--- agixt/provider/gpt4free.py
+++ agixt/provider/gpt4free.py
@@ -1,7 +1,7 @@
 import gpt4free
 import time
 import logging
-
+import importlib
 
 class Gpt4freeProvider:
     def __init__(
@@ -16,8 +16,8 @@ class Gpt4freeProvider:
         self.AI_TEMPERATURE = AI_TEMPERATURE
         self.MAX_TOKENS = MAX_TOKENS
         self.FAILED_PROVIDERS = []
-        self.providers = ["DeepAI", "You", "Poe", "UseLess", "ForeFront"]
-        self.providers.sort()
+        self.providers = ["DeepAI", "You", "UseLess", "ForeFront", "Theb"]
+        self.account_tokens = {}
 
     def instruct(self, prompt, tokens: int = 0):
         final_response = None
@@ -26,31 +26,54 @@ class Gpt4freeProvider:
                 try:
                     if provider not in self.FAILED_PROVIDERS:
                         logging.info(f"[GPT4Free] Using: {provider}")
+                        if (provider not in self.account_tokens):
+                            try:
+                                if provider == "Poe":
+                                    module_name = "quora"
+                                elif provider == "UseLess":
+                                    module_name = "usesless"
+                                else:
+                                    module_name = provider.lower()
+                                module = importlib.import_module("gpt4free.%s" % module_name)
+                                if (module and hasattr(module, "Account")):
+                                    logging.info(f"Create account for: {provider}")
+                                    self.account_tokens[provider] = module.Account.create()
+                            except ModuleNotFoundError:
+                               self.account_tokens[provider] = None
+                        args = {}
+                        if (provider in self.account_tokens):
+                            if (provider == "ForeFront"):
+                                args["account_data"] = self.account_tokens[provider]
+                            elif (provider == "UseLess"):
+                                args["token"] = self.account_tokens[provider]
+                            elif (provider == "Poe"):
+                                args["token"] = self.account_tokens[provider]
+                                args["model"] = "GPT-4"
+                        
                         response = gpt4free.Completion.create(
                             getattr(gpt4free.Provider, provider),
                             prompt=prompt,
+                            **args
                         )
                         if response:
-                            if "text" in response:
-                                final_response = response["text"]
-                            if "status" in response and response["status"] == "Fail":
+                            if provider == "UseLess":
+                                if "text" in response:
+                                    response = response["text"]
+                                if "status" in response and response["status"] == "Fail":
+                                    self.FAILED_PROVIDERS.append(provider)
+                                    logging.info(f"Failed to use {provider}: {response}")
+                                    response = None
+                            if (response == "Unable to fetch the response, Please try again."):
                                 self.FAILED_PROVIDERS.append(provider)
-                                logging.info(f"Failed to use {provider}")
-                                final_response = None
-                            if (
-                                response
-                                == "Unable to fetch the response, Please try again."
-                            ):
-                                self.FAILED_PROVIDERS.append(provider)
-                                logging.info(f"Failed to use {provider}")
-                                final_response = None
+                                logging.info(f"Failed to use {provider}: {response}")
+                                response = None
                             if final_response == None:
                                 final_response = response
                     if final_response:
                         if len(final_response) > 1:
                             return final_response
-                except:
-                    logging.info(f"Failed to use {provider}")
+                except Exception as e:
+                    logging.info(f"Failed to use {provider}: {e}")
                     self.FAILED_PROVIDERS.append(provider)
                     final_response = None
 
