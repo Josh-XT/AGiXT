1c2a9b11132f8475148aa30cd97c0a017b7b3918
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2023-05-28 12:43:38 -0400

    Fix and test Searxng (#498)

--- agixt/commands/searxng_commands.py
+++ agixt/commands/searxng_commands.py
@@ -7,7 +7,11 @@ from Commands import Commands
 
 class searxng_commands(Commands):
     def __init__(self, SEARXNG_INSTANCE_URL: str = "", **kwargs):
-        self.SEARXNG_INSTANCE_URL = SEARXNG_INSTANCE_URL
+        self.SEARXNG_INSTANCE_URL = (
+            SEARXNG_INSTANCE_URL
+            if SEARXNG_INSTANCE_URL
+            else self.choose_random_searx_server()
+        )
         self.commands = {"Use The Search Engine": self.search_searx}
 
     def searx_servers(self):
@@ -18,14 +22,17 @@ class searxng_commands(Commands):
             search_engines = list(data["instances"].keys())
             return search_engines
         except:
-            return ["https://searx.work"]
+            return ["https://search.us.projectsegfau.lt"]
+
+    def choose_random_searx_server(self):
+        searx = self.searx_servers()
+        # Pick a random searx server to use since one was not defined.
+        random_index = random.randint(0, len(searx) - 1)
+        return searx[random_index].rstrip("/")
 
     def search_searx(self, query: str) -> List[str]:
         if self.SEARXNG_INSTANCE_URL == "":
-            searx = self.searx_servers()
-            # Pick a random searx server to use since one was not defined.
-            random_index = random.randint(0, len(searx) - 1)
-            self.SEARXNG_INSTANCE_URL = searx[random_index].rstrip("/")
+            self.SEARXNG_INSTANCE_URL = self.choose_random_searx_server()
         else:
             self.SEARXNG_INSTANCE_URL = self.SEARXNG_INSTANCE_URL.rstrip("/")
         payload = {
@@ -35,7 +42,11 @@ class searxng_commands(Commands):
             "format": "json",
         }
         response = requests.get(f"{self.SEARXNG_INSTANCE_URL}/search", params=payload)
-        results = response.json()
+        try:
+            results = response.json()
+        except:
+            self.SEARXNG_INSTANCE_URL = "https://search.us.projectsegfau.lt"
+            return self.search_searx(query)
         summaries = [
             result["title"] + " - " + result["url"] for result in results["results"]
         ]
