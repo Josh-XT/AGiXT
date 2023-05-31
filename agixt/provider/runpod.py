e41de678ddf2e60915def387c32f599415a0604e
localAGI <132956819+localagi@users.noreply.github.com>
2023-05-31 21:33:43 +0100

    move name back due to weird issue with loader, add settings for model

--- agixt/provider/runpod.py
+++ agixt/provider/runpod.py
@@ -2,6 +2,7 @@ from threading import Thread
 from concurrent.futures import Future
 import logging
 import time
+import random
 
 
 def call_with_future(fn, future, args, kwargs):
@@ -24,7 +25,7 @@ def threaded(fn):
 import requests
 
 
-class RunpodAPIProvider:
+class RunpodProvider:
     def __init__(
         self,
         AI_PROVIDER_URI: str = "",
@@ -44,7 +45,7 @@ class RunpodAPIProvider:
     @threaded
     def instruct(self, prompt, tokens: int = 0):
         headers = {"Authorization": f"Bearer {self.API_KEY}"}
-        max_tokens = (
+        max_new_tokens = (
             int(self.MAX_TOKENS) - tokens
             if int(self.MAX_TOKENS) > tokens
             else self.MAX_TOKENS
@@ -58,9 +59,27 @@ class RunpodAPIProvider:
             json={
                 "input": {
                     "prompt": prompt,
-                    "max_context_length": max_tokens,
-                    "max_length": 200,
+                    "max_new_tokens": max_new_tokens,
+                    # "max_context_length": max_tokens,
+                    # "max_length": 200,
+                    "do_sample": True,
                     "temperature": float(self.AI_TEMPERATURE),
+                    "top_p": 0.73,
+                    "typical_p": 1,
+                    "repetition_penalty": 1.1,
+                    "top_k": 0,
+                    "min_length": 0,
+                    "no_repeat_ngram_size": 0,
+                    "num_beams": 1,
+                    "penalty_alpha": 0,
+                    "length_penalty": 1,
+                    "early_stopping": False,
+                    "seed": random.randint(1, 1000000000),
+                    "add_bos_token": True,
+                    "truncation_length": 4096,
+                    "ban_eos_token": False,
+                    "skip_special_tokens": True,
+                    "stopping_strings": [],
                 },
             },
         )
