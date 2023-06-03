21995fe4ae51e8eacd205ad16d0274d2d5748b7a
Josh XT <josh@devxt.com>
2023-06-03 08:46:36 -0400

    Clean up

--- agixt/provider/llamacppapi.py
+++ agixt/provider/llamacppapi.py
@@ -6,29 +6,20 @@ class LlamacppapiProvider:
     def __init__(
         self,
         AI_PROVIDER_URI: str = "http://localhost:8000",
-        EMBEDDING_URI: str = "http://localhost:8001",
-        MAX_TOKENS: int = 2000,
+        MAX_TOKENS: int = 2048,
         AI_TEMPERATURE: float = 0.7,
         AI_MODEL: str = "default",
-        BATCH_SIZE: int = 512,
-        THREADS: int = 0,
-        STOP_SEQUENCE: str = "\n",
-        EXCLUDE_STRING: str = "",
+        STOP_SEQUENCE: str = "</s>",
         **kwargs,
     ):
         self.AI_PROVIDER_URI = AI_PROVIDER_URI
         self.AI_TEMPERATURE = AI_TEMPERATURE
         self.MAX_TOKENS = MAX_TOKENS
         self.AI_MODEL = AI_MODEL
-        self.BATCH_SIZE = BATCH_SIZE
-        self.THREADS = THREADS if THREADS != 0 else None
         self.STOP_SEQUENCE = STOP_SEQUENCE
-        self.EXCLUDE_STRING = EXCLUDE_STRING
-        self.EMBEDDING_URI = EMBEDDING_URI
         self.MAX_TOKENS = int(self.MAX_TOKENS)
 
     def instruct(self, prompt, tokens: int = 0):
-        new_tokens = int(self.MAX_TOKENS) - tokens
         params = {
             "prompt": prompt,
             "batch_size": int(self.BATCH_SIZE),
@@ -39,10 +30,3 @@ class LlamacppapiProvider:
         response = requests.post(f"{self.AI_PROVIDER_URI}/v1/completion", json=params)
         data = response.json()
         return data["content"]
-
-    def embeddding(self, prompt):
-        data = requests.post(
-            f"{self.EMBEDDING_URI}/v1/embeddding",
-            json={"input": prompt},
-        )
-        return data["embedding"]
