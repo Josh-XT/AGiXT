00fb8160258b160661930cd75331f0f69e261ee6
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2023-06-03 09:04:50 -0400

    Implicit definitions in app.py (#556)
    * Implicit definitions in app.py
    
    * Implicit definitions in app.py

--- agixt/app.py
+++ agixt/app.py
@@ -116,7 +116,7 @@ async def get_providers():
 
 @app.get("/api/provider/{provider_name}", tags=["Provider"])
 async def get_provider_settings(provider_name: str):
-    settings = get_provider_options(provider_name)
+    settings = get_provider_options(provider_name=provider_name)
     return {"settings": settings}
 
 
@@ -128,13 +128,17 @@ async def get_embed_providers():
 
 @app.post("/api/agent", tags=["Agent"])
 async def add_agent(agent: AgentSettings) -> Dict[str, str]:
-    agent_info = Agent(agent.agent_name).add_agent(agent.agent_name, agent.settings)
+    agent_info = Agent(agent.agent_name).add_agent(
+        agent_name=agent.agent_name, provider_settings=agent.settings
+    )
     return {"message": "Agent added", "agent_file": agent_info["agent_file"]}
 
 
 @app.patch("/api/agent/{agent_name}", tags=["Agent"])
 async def rename_agent(agent_name: str, new_name: AgentNewName) -> ResponseMessage:
-    Agent(agent_name).rename_agent(agent_name, new_name.new_name)
+    Agent(agent_name=agent_name).rename_agent(
+        agent_name=agent_name, new_name=new_name.new_name
+    )
     return ResponseMessage(
         message=f"Agent {agent_name} renamed to {new_name.new_name}."
     )
@@ -144,13 +148,17 @@ async def rename_agent(agent_name: str, new_name: AgentNewName) -> ResponseMessa
 async def update_agent_settings(
     agent_name: str, settings: AgentSettings
 ) -> ResponseMessage:
-    update_config = Agent(agent_name).update_agent_config(settings.settings, "settings")
+    update_config = Agent(agent_name=agent_name).update_agent_config(
+        new_config=settings.settings, config_key="settings"
+    )
     return ResponseMessage(message=update_config)
 
 
 @app.delete("/api/agent/{agent_name}", tags=["Agent"])
 async def delete_agent(agent_name: str) -> ResponseMessage:
-    result, status_code = Agent(agent_name).delete_agent(agent_name)
+    result, status_code = Agent(agent_name=agent_name).delete_agent(
+        agent_name=agent_name
+    )
     if status_code == 200:
         return ResponseMessage(message=result["message"])
     else:
@@ -165,25 +173,25 @@ async def get_agents():
 
 @app.get("/api/agent/{agent_name}", tags=["Agent"])
 async def get_agentconfig(agent_name: str):
-    agent_config = Agent(agent_name).get_agent_config()
+    agent_config = Agent(agent_name=agent_name).get_agent_config()
     return {"agent": agent_config}
 
 
 @app.get("/api/{agent_name}/chat", tags=["Agent"])
 async def get_chat_history(agent_name: str):
-    chat_history = Agent(agent_name).get_chat_history(agent_name)
+    chat_history = Agent(agent_name=agent_name).get_chat_history(agent_name=agent_name)
     return {"chat_history": chat_history}
 
 
 @app.delete("/api/agent/{agent_name}/memory", tags=["Agent"])
 async def wipe_agent_memories(agent_name: str) -> ResponseMessage:
-    Agent(agent_name).wipe_agent_memories(agent_name)
+    Agent(agent_name=agent_name).wipe_agent_memories(agent_name=agent_name)
     return ResponseMessage(message=f"Memories for agent {agent_name} deleted.")
 
 
 @app.post("/api/agent/{agent_name}/instruct", tags=["Agent"])
 async def instruct(agent_name: str, prompt: Prompt):
-    agent = AGiXT(agent_name)
+    agent = AGiXT(agent_name=agent_name)
     response = await agent.run(
         task=prompt.prompt,
         prompt="instruct",
@@ -193,28 +201,28 @@ async def instruct(agent_name: str, prompt: Prompt):
 
 @app.post("/api/agent/{agent_name}/smartinstruct/{shots}", tags=["Agent"])
 async def smartinstruct(agent_name: str, shots: int, prompt: Prompt):
-    agent = AGiXT(agent_name)
+    agent = AGiXT(agent_name=agent_name)
     response = await agent.smart_instruct(task=prompt.prompt, shots=int(shots))
     return {"response": str(response)}
 
 
 @app.post("/api/agent/{agent_name}/chat", tags=["Agent"])
 async def chat(agent_name: str, prompt: Prompt):
-    agent = AGiXT(agent_name)
-    response = await agent.run(prompt.prompt, prompt="Chat", context_results=6)
+    agent = AGiXT(agent_name=agent_name)
+    response = await agent.run(task=prompt.prompt, prompt="Chat", context_results=6)
     return {"response": str(response)}
 
 
 @app.post("/api/agent/{agent_name}/smartchat/{shots}", tags=["Agent"])
 async def smartchat(agent_name: str, shots: int, prompt: Prompt):
-    agent = AGiXT(agent_name)
-    response = await agent.smart_chat(prompt.prompt, shots=shots)
+    agent = AGiXT(agent_name=agent_name)
+    response = await agent.smart_chat(task=prompt.prompt, shots=shots)
     return {"response": str(response)}
 
 
 @app.get("/api/agent/{agent_name}/command", tags=["Agent"])
 async def get_commands(agent_name: str):
-    agent = Agent(agent_name)
+    agent = Agent(agent_name=agent_name)
     return {"commands": agent.agent_config["commands"]}
 
 
@@ -222,20 +230,24 @@ async def get_commands(agent_name: str):
 async def toggle_command(
     agent_name: str, payload: ToggleCommandPayload
 ) -> ResponseMessage:
-    agent = Agent(agent_name)
+    agent = Agent(agent_name=agent_name)
     print(payload)
     try:
         if payload.command_name == "*":
             for each_command_name in agent.agent_config["commands"]:
                 agent.agent_config["commands"][each_command_name] = payload.enable
 
-            agent.update_agent_config(agent.agent_config["commands"], "commands")
+            agent.update_agent_config(
+                new_config=agent.agent_config["commands"], config_key="commands"
+            )
             return ResponseMessage(
                 message=f"All commands enabled for agent '{agent_name}'."
             )
         else:
             agent.agent_config["commands"][payload.command_name] = payload.enable
-            agent.update_agent_config(agent.agent_config["commands"], "commands")
+            agent.update_agent_config(
+                new_config=agent.agent_config["commands"], config_key="commands"
+            )
             return ResponseMessage(
                 message=f"Command '{payload.command_name}' toggled for agent '{agent_name}'."
             )
@@ -249,19 +261,20 @@ async def toggle_command(
 
 @app.post("/api/agent/{agent_name}/task", tags=["Agent"])
 async def start_task_agent(agent_name: str, objective: Objective) -> ResponseMessage:
+    task = Tasks(agent_name=agent_name)
     # If it's running stop it.
-    task_status = Tasks(agent_name).get_status()
+    task_status = task.get_status()
     if task_status != False:
-        Tasks(agent_name).stop_tasks()
+        task.stop_tasks()
         return ResponseMessage(message="Task agent stopped")
     # If it's not running start it.
-    Tasks(agent_name).run_task(objective=objective.objective)
+    task.run_task(objective=objective.objective)
     return ResponseMessage(message="Task agent started")
 
 
 @app.get("/api/agent/{agent_name}/task", tags=["Agent"])
 async def get_task_output(agent_name: str) -> TaskOutput:
-    task_output = Tasks(agent_name).get_task_output()
+    task_output = Tasks(agent_name=agent_name).get_task_output()
     if task_output != False:
         return TaskOutput(
             output=task_output,
@@ -276,7 +289,7 @@ async def get_task_output(agent_name: str) -> TaskOutput:
 
 @app.get("/api/agent/{agent_name}/task/status", tags=["Agent"])
 async def get_task_status(agent_name: str):
-    task_status = Tasks(agent_name).get_status()
+    task_status = Tasks(agent_name=agent_name).get_status()
     return {"status": task_status}
 
 
@@ -288,25 +301,25 @@ async def get_chains():
 
 @app.get("/api/chain/{chain_name}", tags=["Chain"])
 async def get_chain(chain_name: str):
-    chain_data = Chain().get_chain(chain_name)
+    chain_data = Chain().get_chain(chain_name=chain_name)
     return {"chain": chain_data}
 
 
 @app.post("/api/chain/{chain_name}/run", tags=["Chain"])
 async def run_chain(chain_name: str) -> ResponseMessage:
-    await Chain().run_chain(chain_name)
+    await Chain().run_chain(chain_name=chain_name)
     return {"message": f"Chain '{chain_name}' started."}
 
 
 @app.post("/api/chain", tags=["Chain"])
 async def add_chain(chain_name: ChainName) -> ResponseMessage:
-    Chain().add_chain(chain_name.chain_name)
+    Chain().add_chain(chain_name=chain_name.chain_name)
     return ResponseMessage(message=f"Chain '{chain_name.chain_name}' created.")
 
 
 @app.put("/api/chain/{chain_name}", tags=["Chain"])
 async def rename_chain(chain_name: str, new_name: ChainNewName) -> ResponseMessage:
-    Chain().rename_chain(chain_name, new_name.new_name)
+    Chain().rename_chain(chain_name=chain_name, new_name=new_name.new_name)
     return ResponseMessage(
         message=f"Chain '{chain_name}' renamed to '{new_name.new_name}'."
     )
@@ -314,18 +327,18 @@ async def rename_chain(chain_name: str, new_name: ChainNewName) -> ResponseMessa
 
 @app.delete("/api/chain/{chain_name}", tags=["Chain"])
 async def delete_chain(chain_name: str) -> ResponseMessage:
-    Chain().delete_chain(chain_name)
+    Chain().delete_chain(chain_name=chain_name)
     return ResponseMessage(message=f"Chain '{chain_name}' deleted.")
 
 
 @app.post("/api/chain/{chain_name}/step", tags=["Chain"])
 async def add_step(chain_name: str, step_info: StepInfo) -> ResponseMessage:
     Chain().add_chain_step(
-        chain_name,
-        step_info.step_number,
-        step_info.prompt_type,
-        step_info.prompt,
-        step_info.agent_name,
+        chain_name=chain_name,
+        step_number=step_info.step_number,
+        prompt_type=step_info.prompt_type,
+        prompt=step_info.prompt,
+        agent_name=step_info.agent_name,
     )
     return {"message": f"Step {step_info.step_number} added to chain '{chain_name}'."}
 
@@ -335,11 +348,11 @@ async def update_step(
     chain_name: str, step_number: int, chain_step: ChainStep
 ) -> ResponseMessage:
     Chain().update_step(
-        chain_name,
-        chain_step.step_number,
-        chain_step.prompt_type,
-        chain_step.prompt,
-        chain_step.agent_name,
+        chain_name=chain_name,
+        step_number=chain_step.step_number,
+        prompt_type=chain_step.prompt_type,
+        prompt=chain_step.prompt,
+        agent_name=chain_step.agent_name,
     )
     return {
         "message": f"Step {chain_step.step_number} updated for chain '{chain_name}'."
@@ -351,9 +364,9 @@ async def move_step(
     chain_name: str, chain_step_new_info: ChainStepNewInfo
 ) -> ResponseMessage:
     Chain().move_step(
-        chain_name,
-        chain_step_new_info.old_step_number,
-        chain_step_new_info.new_step_number,
+        chain_name=chain_name,
+        current_step_number=chain_step_new_info.old_step_number,
+        new_step_number=chain_step_new_info.new_step_number,
     )
     return {
         "message": f"Step {chain_step_new_info.old_step_number} moved to {chain_step_new_info.new_step_number} in chain '{chain_name}'."
@@ -362,14 +375,14 @@ async def move_step(
 
 @app.delete("/api/chain/{chain_name}/step/{step_number}", tags=["Chain"])
 async def delete_step(chain_name: str, step_number: int) -> ResponseMessage:
-    Chain().delete_step(chain_name, step_number)
+    Chain().delete_step(chain_name=chain_name, step_number=step_number)
     return {"message": f"Step {step_number} deleted from chain '{chain_name}'."}
 
 
 @app.post("/api/prompt", tags=["Prompt"])
 async def add_prompt(prompt: CustomPromptModel) -> ResponseMessage:
     try:
-        Prompts().add_prompt(prompt.prompt_name, prompt.prompt)
+        Prompts().add_prompt(prompt_name=prompt.prompt_name, prompt=prompt.prompt)
         return ResponseMessage(message=f"Prompt '{prompt.prompt_name}' added.")
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
@@ -378,7 +391,7 @@ async def add_prompt(prompt: CustomPromptModel) -> ResponseMessage:
 @app.get("/api/prompt/{prompt_name}", tags=["Prompt"], response_model=CustomPromptModel)
 async def get_prompt(prompt_name: str):
     try:
-        prompt_content = Prompts().get_prompt(prompt_name)
+        prompt_content = Prompts().get_prompt(prompt_name=prompt_name)
         return {"prompt_name": prompt_name, "prompt": prompt_content}
     except Exception as e:
         raise HTTPException(status_code=404, detail=str(e))
@@ -393,7 +406,7 @@ async def get_prompts():
 @app.delete("/api/prompt/{prompt_name}", tags=["Prompt"])
 async def delete_prompt(prompt_name: str) -> ResponseMessage:
     try:
-        Prompts().delete_prompt(prompt_name)
+        Prompts().delete_prompt(prompt_name=prompt_name)
         return ResponseMessage(message=f"Prompt '{prompt_name}' deleted.")
     except Exception as e:
         raise HTTPException(status_code=404, detail=str(e))
@@ -402,7 +415,7 @@ async def delete_prompt(prompt_name: str) -> ResponseMessage:
 @app.put("/api/prompt/{prompt_name}", tags=["Prompt"])
 async def update_prompt(prompt: CustomPromptModel) -> ResponseMessage:
     try:
-        Prompts().update_prompt(prompt.prompt_name, prompt.prompt)
+        Prompts().update_prompt(prompt_name=prompt.prompt_name, prompt=prompt.prompt)
         return ResponseMessage(message=f"Prompt '{prompt.prompt_name}' updated.")
     except Exception as e:
         raise HTTPException(status_code=404, detail=str(e))
