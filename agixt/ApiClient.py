4a774a3e47ab08583be9458819dc68c97bba42b3
Josh XT <josh@devxt.com>
2023-06-04 18:18:49 -0400

    Add top level responses

--- agixt/ApiClient.py
+++ agixt/ApiClient.py
@@ -9,191 +9,185 @@ base_uri = os.getenv("BASE_URI", "http://localhost:7437")
 
 class ApiClient:
     @staticmethod
-    def get_providers() -> Dict[str, List[str]]:
+    def get_providers():
         response = requests.get(f"{base_uri}/api/provider")
-        return response.json()
+        return response.json()["providers"]
 
     @staticmethod
-    def get_provider_settings(provider_name: str) -> Dict[str, Any]:
+    def get_provider_settings(provider_name: str):
         response = requests.get(f"{base_uri}/api/provider/{provider_name}")
-        return response.json()
+        return response.json()["settings"]
 
     @staticmethod
-    def get_embed_providers() -> Dict[str, List[str]]:
+    def get_embed_providers():
         response = requests.get(f"{base_uri}/api/embedding_providers")
-        return response.json()
+        return response.json()["providers"]
 
     @staticmethod
-    def add_agent(agent_name: str, settings: Dict[str, Any]) -> Dict[str, str]:
+    def add_agent(agent_name: str, settings: Dict[str, Any]):
         response = requests.post(
             f"{base_uri}/api/agent",
             json={"agent_name": agent_name, "settings": settings},
         )
-        return response.json()
+        return response.json()["message"]
 
     @staticmethod
-    def rename_agent(agent_name: str, new_name: str) -> Dict[str, str]:
+    def rename_agent(agent_name: str, new_name: str):
         response = requests.patch(
             f"{base_uri}/api/agent/{agent_name}",
             json={"new_name": new_name},
         )
-        return response.json()
+        return response.json()["message"]
 
     @staticmethod
-    def update_agent_settings(
-        agent_name: str, settings: Dict[str, Any]
-    ) -> Dict[str, str]:
+    def update_agent_settings(agent_name: str, settings: Dict[str, Any]):
         response = requests.put(
             f"{base_uri}/api/agent/{agent_name}",
             json={"settings": settings, "agent_name": agent_name},
         )
-        return response.json()
+        return response.json()["message"]
 
     @staticmethod
-    def update_agent_commands(
-        agent_name: str, commands: Dict[str, Any]
-    ) -> Dict[str, str]:
+    def update_agent_commands(agent_name: str, commands: Dict[str, Any]):
         response = requests.put(
             f"{base_uri}/api/agent/{agent_name}/commands",
             json={"commands": commands, "agent_name": agent_name},
         )
-        return response.json()
+        return response.json()["message"]
 
     @staticmethod
-    def delete_agent(agent_name: str) -> Dict[str, str]:
+    def delete_agent(agent_name: str):
         response = requests.delete(f"{base_uri}/api/agent/{agent_name}")
-        return response.json()
+        return response.json()["message"]
 
     @staticmethod
-    def get_agents() -> Dict[str, List[str]]:
+    def get_agents():
         response = requests.get(f"{base_uri}/api/agent")
-        return response.json()
+        return response.json()["agents"]
 
     @staticmethod
     def get_agentconfig(agent_name: str) -> Dict[str, Any]:
         response = requests.get(f"{base_uri}/api/agent/{agent_name}")
-        return response.json()
+        return response.json()["agent"]
 
     @staticmethod
-    def get_chat_history(agent_name: str) -> Dict[str, List[str]]:
+    def get_chat_history(agent_name: str):
         response = requests.get(f"{base_uri}/api/{agent_name}/chat")
-        return response.json()
+        return response.json()["chat_history"]
 
     @staticmethod
-    def wipe_agent_memories(agent_name: str) -> Dict[str, str]:
+    def wipe_agent_memories(agent_name: str):
         response = requests.delete(f"{base_uri}/api/agent/{agent_name}/memory")
-        return response.json()
+        return response.json()["message"]
 
     @staticmethod
-    def instruct(agent_name: str, prompt: str) -> Dict[str, str]:
+    def instruct(agent_name: str, prompt: str):
         response = requests.post(
             f"{base_uri}/api/agent/{agent_name}/instruct",
             json={"prompt": prompt},
         )
-        return response.json()
+        return response.json()["response"]
 
     @staticmethod
-    def smartinstruct(agent_name: str, shots: int, prompt: str) -> Dict[str, str]:
+    def smartinstruct(agent_name: str, shots: int, prompt: str):
         response = requests.post(
             f"{base_uri}/api/agent/{agent_name}/smartinstruct/{shots}",
             json={"prompt": prompt},
         )
-        return response.json()
+        return response.json()["response"]
 
     @staticmethod
-    def chat(agent_name: str, prompt: str) -> Dict[str, str]:
+    def chat(agent_name: str, prompt: str):
         response = requests.post(
             f"{base_uri}/api/agent/{agent_name}/chat",
             json={"prompt": prompt},
         )
-        return response.json()
+        return response.json()["response"]
 
     @staticmethod
-    def smartchat(agent_name: str, shots: int, prompt: str) -> Dict[str, str]:
+    def smartchat(agent_name: str, shots: int, prompt: str):
         response = requests.post(
             f"{base_uri}/api/agent/{agent_name}/smartchat/{shots}",
             json={"prompt": prompt},
         )
-        return response.json()
+        return response.json()["response"]
 
     @staticmethod
     def get_commands(agent_name: str) -> Dict[str, Dict[str, bool]]:
         response = requests.get(f"{base_uri}/api/agent/{agent_name}/command")
-        return response.json()
+        return response.json()["commands"]
 
     @staticmethod
-    def toggle_command(
-        agent_name: str, command_name: str, enable: bool
-    ) -> Dict[str, str]:
+    def toggle_command(agent_name: str, command_name: str, enable: bool):
         response = requests.patch(
             f"{base_uri}/api/agent/{agent_name}/command",
             json={"command_name": command_name, "enable": enable},
         )
-        return response.json()
+        return response.json()["message"]
 
     @staticmethod
-    def start_task_agent(agent_name: str, objective: str) -> Dict[str, str]:
+    def start_task_agent(agent_name: str, objective: str):
         response = requests.post(
             f"{base_uri}/api/agent/{agent_name}/task",
             json={"objective": objective},
         )
-        return response.json()
+        return response.json()["message"]
 
     @staticmethod
-    def get_tasks() -> Dict[str, List[str]]:
+    def get_tasks():
         response = requests.get(f"{base_uri}/api/tasks")
-        return response.json()
+        return response.json()["tasks"]
 
     @staticmethod
     def get_task_output(agent_name: str) -> Dict[str, Union[str, Optional[str]]]:
         response = requests.get(f"{base_uri}/api/agent/{agent_name}/task")
-        return response.json()
+        return response.json()["output"]
 
     @staticmethod
     def get_task_status(agent_name: str) -> Dict[str, bool]:
         response = requests.get(f"{base_uri}/api/agent/{agent_name}/task/status")
-        return response.json()
+        return response.json()["status"]
 
     @staticmethod
-    def get_chains() -> Dict[str, List[str]]:
+    def get_chains():
         response = requests.get(f"{base_uri}/api/chain")
         return response.json()
 
     @staticmethod
     def get_chain(chain_name: str) -> Dict[str, Any]:
         response = requests.get(f"{base_uri}/api/chain/{chain_name}")
-        return response.json()
+        return response.json()["chain"]
 
     @staticmethod
     def get_chain_responses(chain_name: str) -> Dict[str, Any]:
         response = requests.get(f"{base_uri}/api/chain/{chain_name}/responses")
-        return response.json()
+        return response.json()["chain"]
 
     @staticmethod
-    def run_chain(chain_name: str) -> Dict[str, str]:
+    def run_chain(chain_name: str):
         response = requests.post(f"{base_uri}/api/chain/{chain_name}/run")
-        return response.json()
+        return response.json()["message"]
 
     @staticmethod
-    def add_chain(chain_name: str) -> Dict[str, str]:
+    def add_chain(chain_name: str):
         response = requests.post(
             f"{base_uri}/api/chain",
             json={"chain_name": chain_name},
         )
-        return response.json()
+        return response.json()["message"]
 
     @staticmethod
-    def rename_chain(chain_name: str, new_name: str) -> Dict[str, str]:
+    def rename_chain(chain_name: str, new_name: str):
         response = requests.put(
             f"{base_uri}/api/chain/{chain_name}",
             json={"new_name": new_name},
         )
-        return response.json()
+        return response.json()["message"]
 
     @staticmethod
-    def delete_chain(chain_name: str) -> Dict[str, str]:
+    def delete_chain(chain_name: str):
         response = requests.delete(f"{base_uri}/api/chain/{chain_name}")
-        return response.json()
+        return response.json()["message"]
 
     @staticmethod
     def add_step(
@@ -202,7 +196,7 @@ class ApiClient:
         agent_name: str,
         prompt_type: str,
         prompt: dict,
-    ) -> Dict[str, str]:
+    ):
         response = requests.post(
             f"{base_uri}/api/chain/{chain_name}/step",
             json={
@@ -212,7 +206,7 @@ class ApiClient:
                 "prompt": prompt,
             },
         )
-        return response.json()
+        return response.json()["message"]
 
     @staticmethod
     def update_step(
@@ -221,7 +215,7 @@ class ApiClient:
         agent_name: str,
         prompt_type: str,
         prompt: str,
-    ) -> Dict[str, str]:
+    ):
         response = requests.put(
             f"{base_uri}/api/chain/{chain_name}/step/{step_number}",
             json={
@@ -231,14 +225,14 @@ class ApiClient:
                 "prompt": prompt,
             },
         )
-        return response.json()
+        return response.json()["message"]
 
     @staticmethod
     def move_step(
         chain_name: str,
         old_step_number: int,
         new_step_number: int,
-    ) -> Dict[str, str]:
+    ):
         response = requests.patch(
             f"{base_uri}/api/chain/{chain_name}/step/move",
             json={
@@ -246,75 +240,73 @@ class ApiClient:
                 "new_step_number": new_step_number,
             },
         )
-        return response.json()
+        return response.json()["message"]
 
     @staticmethod
-    def delete_step(chain_name: str, step_number: int) -> Dict[str, str]:
+    def delete_step(chain_name: str, step_number: int):
         response = requests.delete(
             f"{base_uri}/api/chain/{chain_name}/step/{step_number}"
         )
-        return response.json()
+        return response.json()["message"]
 
     @staticmethod
-    def add_prompt(prompt_name: str, prompt: str) -> Dict[str, str]:
+    def add_prompt(prompt_name: str, prompt: str):
         response = requests.post(
             f"{base_uri}/api/prompt",
             json={"prompt_name": prompt_name, "prompt": prompt},
         )
-        return response.json()
+        return response.json()["message"]
 
     @staticmethod
-    def get_prompt(prompt_name: str) -> Dict[str, str]:
+    def get_prompt(prompt_name: str):
         response = requests.get(f"{base_uri}/api/prompt/{prompt_name}")
         return response.json()
 
     @staticmethod
-    def get_prompts() -> Dict[str, List[str]]:
+    def get_prompts():
         response = requests.get(f"{base_uri}/api/prompt")
-        return response.json()
+        return response.json()["prompts"]
 
     @staticmethod
     def get_prompt_args(prompt_name: str) -> Dict[str, Any]:
         response = requests.get(f"{base_uri}/api/prompt/{prompt_name}/args")
-        return response.json()
+        return response.json()["prompt_args"]
 
     @staticmethod
-    def delete_prompt(prompt_name: str) -> Dict[str, str]:
+    def delete_prompt(prompt_name: str):
         response = requests.delete(f"{base_uri}/api/prompt/{prompt_name}")
-        return response.json()
+        return response.json()["message"]
 
     @staticmethod
-    def update_prompt(prompt_name: str, prompt: str) -> Dict[str, str]:
+    def update_prompt(prompt_name: str, prompt: str):
         response = requests.put(
             f"{base_uri}/api/prompt/{prompt_name}",
             json={"prompt": prompt, "prompt_name": prompt_name},
         )
-        return response.json()
+        return response.json()["message"]
 
     @staticmethod
     def get_extension_settings() -> Dict[str, Any]:
         response = requests.get(f"{base_uri}/api/extensions/settings")
-        return response.json()
+        return response.json()["extension_settings"]
 
     @staticmethod
     def get_command_args(command_name: str) -> Dict[str, Any]:
         response = requests.get(f"{base_uri}/api/extensions/{command_name}/args")
-        return response.json()
+        return response.json()["command_args"]
 
     @staticmethod
-    def learn_url(agent_name: str, url: str) -> Dict[str, str]:
+    def learn_url(agent_name: str, url: str):
         response = requests.post(
             f"{base_uri}/api/agent/{agent_name}/learn/url",
             json={"url": url},
         )
-        return response.json()
+        return response.json()["message"]
 
     @staticmethod
-    def learn_file(
-        agent_name: str, file_name: str, file_content: str
-    ) -> Dict[str, str]:
+    def learn_file(agent_name: str, file_name: str, file_content: str):
         response = requests.post(
             f"{base_uri}/api/agent/{agent_name}/learn/file",
             json={"file_name": file_name, "file_content": file_content},
         )
-        return response.json()
+        return response.json()["message"]
