0ebc005815747969cd6ba2ea5faa18f686bffbb6
ven-ven-ven <134007790+ven-ven-ven@users.noreply.github.com>
2023-06-05 21:15:34 +0200

    Added Konsole run (#580)
    *should* work with xterm, no promises.
    
    Signed-off-by: ven-ven-ven <134007790+ven-ven-ven@users.noreply.github.com>

---
+++ run-linux-konsole.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Tested with xterm as well, but no guarantees.
+git pull
+poetry install --with gpt4free
+#If you have problems, comment below line and install playwright for your OS, or increase sleep time
+poetry run playwright install
+cd agixt
+konsole -e "poetry run uvicorn app:app --port 7437" &
+uvicorn_pid=$!
+sleep 3
+konsole -e "poetry run streamlit run Main.py" &
+streamlit_pid=$!
+read -p "Press any key to exit..."
+kill $uvicorn_pid $streamlit_pid
+wait $uvicorn_pid $streamlit_pid
