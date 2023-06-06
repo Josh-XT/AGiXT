e1768c7e12fc3e5683abe5460bf6698ad52f332a
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2023-06-06 19:08:51 -0400

    Remove theb from gpt4free (#604)
    Signed-off-by: Josh XT <102809327+Josh-XT@users.noreply.github.com>

--- agixt/provider/gpt4free.py
+++ agixt/provider/gpt4free.py
@@ -18,7 +18,7 @@ class Gpt4freeProvider:
         self.AI_TEMPERATURE = AI_TEMPERATURE
         self.MAX_TOKENS = MAX_TOKENS
         self.FAILED_PROVIDERS = []
-        self.providers = ["DeepAI", "You", "UseLess", "ForeFront", "Theb"]
+        self.providers = ["DeepAI", "You", "UseLess", "ForeFront"]
         self.account_tokens = {}
 
     def create_account(self, provider, module):
