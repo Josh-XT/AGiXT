681a32bbea7d5504edf9b8ca55af69d96abe5dc8
Josh XT <josh@devxt.com>
2023-06-07 08:00:38 -0400

    Improve quickstart script

--- AGiXT.sh
+++ AGiXT.sh
@@ -2,10 +2,12 @@
 git pull
 pip install poetry==1.5.1
 export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
+export POETRY_NO_INTERACTION=1
+export PLAYWRIGHT_BROWSERS_PATH=0
 poetry install --with gpt4free
-poetry run playwright install
+poetry run playwright install --with-deps
 cd agixt
-poetry run uvicorn app:app --host 0.0.0.0 --port 7437 --workers 2 &
+poetry run uvicorn app:app --host 0.0.0.0 --port 7437 --workers 4 &
 uvicorn_pid=$!
 cd ../streamlit
 poetry run streamlit run Main.py &
