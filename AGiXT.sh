c2afceffcc3188fd67980337b866cf437034e1e4
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2023-06-06 21:14:09 -0400

    Move Streamlit files (#606)
    * Move streamlit
    
    * Fix compose
    
    * Fix docs
    
    * Update docs

---
+++ AGiXT.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pip install poetry==1.5.1
+export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
+cd AGiXT
+poetry install --with gpt4free
+poetry run playwright install
+cd streamlit
+poetry run streamlit run Main.py &
+cd ../agixt
+poetry run uvicorn app:app --host 0.0.0.0 --port 7437 --workers 2
