405edbdc675ddba3e9d1dd0fa0d4d1902602c43a
Josh XT <josh@devxt.com>
2023-06-06 22:02:52 -0400

    Add workers, update path

--- run-linux-konsole.sh
+++ run-linux-konsole.sh
@@ -5,9 +5,10 @@ poetry install --with gpt4free
 #If you have problems, comment below line and install playwright for your OS, or increase sleep time
 poetry run playwright install
 cd agixt
-konsole -e "poetry run uvicorn app:app --port 7437" &
+konsole -e "poetry run uvicorn app:app --port 7437 --workders 2" &
 uvicorn_pid=$!
 sleep 3
+cd ../streamlit
 konsole -e "poetry run streamlit run Main.py" &
 streamlit_pid=$!
 read -p "Press any key to exit..."
