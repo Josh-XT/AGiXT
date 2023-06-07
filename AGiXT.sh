15410fa1538770524489036e3f2f49bbc93d6773
Josh XT <josh@devxt.com>
2023-06-06 22:43:29 -0400

    AGiXT Quick Start Shell script

--- AGiXT.sh
+++ AGiXT.sh
@@ -4,7 +4,12 @@ export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
 cd AGiXT
 poetry install --with gpt4free
 poetry run playwright install
-cd streamlit
+cd agixt
+poetry run uvicorn app:app --host 0.0.0.0 --port 7437 --workers 2 &
+uvicorn_pid=$!
+cd ../streamlit
 poetry run streamlit run Main.py &
-cd ../agixt
-poetry run uvicorn app:app --host 0.0.0.0 --port 7437 --workers 2
+streamlit_pid=$!
+read -p "Press any key to exit..."
+kill $streamlit_pid $uvicorn_pid
+wait $streamlit_pid $uvicorn_pid
