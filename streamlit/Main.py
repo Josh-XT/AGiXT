f50bfbd6d4f8eaf54b6aa759f70e2d59535f7153
rm4453 <rmattingly4415@gmail.com>
2023-06-07 21:31:57 -0600

    Auto logo switching (#626)
    Signed-off-by: Josh XT <102809327+Josh-XT@users.noreply.github.com>
    Co-authored-by: Josh XT <102809327+Josh-XT@users.noreply.github.com>

--- streamlit/Main.py
+++ streamlit/Main.py
@@ -6,6 +6,10 @@ from components.verify_backend import verify_backend
 
 verify_backend()
 
+with open('./.streamlit/config.toml') as f:
+    if 'Dark' in f.read():
+        light_theme = False
+
 st.set_page_config(
     page_title="AGiXT",
     page_icon=":robot:",
@@ -13,12 +17,25 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-st.markdown(
-    """
-    <img src="https://josh-xt.github.io/AGiXT/images/AGiXTwhiteborder.svg" width="100%">
-    """,
-    unsafe_allow_html=True,
-)
+
+if light_theme == True:
+    st.markdown(
+        """
+        <div style="text-align: center;">
+        <center><img src="https://josh-xt.github.io/AGiXT/images/AGiXT.svg" width="65%"></center>
+        </div>
+        """,
+        unsafe_allow_html=True,
+    )
+else:
+    st.markdown(
+        """
+        <div style="text-align: center;">
+        <img src="https://josh-xt.github.io/AGiXT/images/AGiXTwhiteborder.svg" width="65%">
+        </div>
+        """,
+        unsafe_allow_html=True,
+    )
 
 st.markdown("## Useful Links")
 st.markdown(
