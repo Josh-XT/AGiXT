0b2b1007f45d938f69ae18180f27b9dbd94d8b78
Josh XT <josh@devxt.com>
2023-06-09 23:40:33 -0400

    Remove Poetry

--- run-linux-konsole.sh
+++ run-linux-konsole.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-#Tested with xterm as well, but no guarantees.
 git pull
-poetry install --with gpt4free
-#If you have problems, comment below line and install playwright for your OS, or increase sleep time
-poetry run playwright install
+playwright install
 cd agixt
-konsole -e "poetry run uvicorn app:app --port 7437 --workders 2" &
-uvicorn_pid=$!
-sleep 3
+pip install --upgrade pip
+pip install -r requirements.txt
 cd ../streamlit
-konsole -e "poetry run streamlit run Main.py" &
+konsole -e "streamlit run Main.py" &
 streamlit_pid=$!
 read -p "Press any key to exit..."
-kill $uvicorn_pid $streamlit_pid
-wait $uvicorn_pid $streamlit_pid
+kill $streamlit_pid
+wait $streamlit_pid
