0bc12b18df866431e4247e38e102a6c32fd9a6f0
Josh XT <josh@devxt.com>
2023-06-10 08:48:10 -0400

    Update docs and lint

--- agixt/extensions/github.py
+++ agixt/extensions/github.py
@@ -29,7 +29,9 @@ class github(Extensions):
         else:
             auth_repo_url = "//".join(split_url)
         try:
-            git.Repo.clone_from(auth_repo_url, os.path.join(self.WORKING_DIRECTORY, clone_path))
+            git.Repo.clone_from(
+                auth_repo_url, os.path.join(self.WORKING_DIRECTORY, clone_path)
+            )
             return f"""Cloned {repo_url} to {os.path.join(self.WORKING_DIRECTORY, clone_path)}"""
         except Exception as e:
             return f"Error: {str(e)}"
