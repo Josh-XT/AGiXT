c9230fd636d30b1a1b2ac3e3f2874943608eb2b3
Josh XT <josh@devxt.com>
2023-06-18 20:26:18 -0400

    Improve ooba provider

--- agixt/provider/oobabooga.py
+++ agixt/provider/oobabooga.py
@@ -1,5 +1,4 @@
 import requests
-import random
 import re
 
 
@@ -7,15 +6,53 @@ class OobaboogaProvider:
     def __init__(
         self,
         AI_PROVIDER_URI: str = "",
-        MAX_TOKENS: int = 2048,
+        MAX_TOKENS: int = 2000,
+        DO_SAMPLE: str = "True",
         AI_TEMPERATURE: float = 0.7,
+        TOP_P: float = 0.9,
+        TYPICAL_P: float = 0.2,
+        EPSILON_CUTOFF: float = 0,
+        ETA_CUTOFF: float = 0,
+        TFS: float = 1,
+        TOP_A: float = 0,
+        REPETITION_PENALTY: float = 1.05,
+        ENCODER_REPETITION_PENALTY: float = 1,
+        TOP_K: int = 200,
+        MIN_LENGTH: float = 0,
+        NO_REPEAT_NGRAM_SIZE: float = 0,
+        NUM_BEAMS: int = 1,
+        PENALTY_ALPHA: float = 0,
+        LENGTH_PENALTY: float = 1,
+        MIROSTAT_MODE: float = 0,
+        MIROSTAT_TAU: float = 5,
+        MIROSTAT_ETA: float = 0.1,
+        TRUNCATION_LENGTH: int = 2048,
         AI_MODEL: str = "default",
         **kwargs,
     ):
         self.AI_PROVIDER_URI = AI_PROVIDER_URI
         self.MAX_TOKENS = MAX_TOKENS
+        self.DO_SAMPLE = DO_SAMPLE
         self.AI_TEMPERATURE = AI_TEMPERATURE
         self.AI_MODEL = AI_MODEL
+        self.TOP_P = TOP_P
+        self.TYPICAL_P = TYPICAL_P
+        self.EPSILON_CUTOFF = EPSILON_CUTOFF
+        self.ETA_CUTOFF = ETA_CUTOFF
+        self.TFS = TFS
+        self.TOP_A = TOP_A
+        self.REPETITION_PENALTY = REPETITION_PENALTY
+        self.ENCODER_REPETITION_PENALTY = ENCODER_REPETITION_PENALTY
+        self.TOP_K = TOP_K
+        self.MIN_LENGTH = MIN_LENGTH
+        self.NO_REPEAT_NGRAM_SIZE = NO_REPEAT_NGRAM_SIZE
+        self.NUM_BEAMS = NUM_BEAMS
+        self.PENALTY_ALPHA = PENALTY_ALPHA
+        self.LENGTH_PENALTY = LENGTH_PENALTY
+        self.MIROSTAT_MODE = MIROSTAT_MODE
+        self.MIROSTAT_TAU = MIROSTAT_TAU
+        self.MIROSTAT_ETA = MIROSTAT_ETA
+        self.TRUNCATION_LENGTH = TRUNCATION_LENGTH
         self.requirements = []
 
     async def instruct(self, prompt, tokens: int = 0):
@@ -25,26 +62,45 @@ class OobaboogaProvider:
             "max_new_tokens": new_tokens,
             "do_sample": True,
             "temperature": float(self.AI_TEMPERATURE),
-            "top_p": 0.73,
-            "typical_p": 1,
-            "repetition_penalty": 1.1,
-            "top_k": 0,
-            "min_length": 0,
-            "no_repeat_ngram_size": 0,
-            "num_beams": 1,
-            "penalty_alpha": 0,
-            "length_penalty": 1,
+            "top_p": float(self.TOP_P),
+            "typical_p": float(self.TYPICAL_P),
+            "epsilon_cutoff": float(self.EPSILON_CUTOFF),
+            "eta_cutoff": float(self.ETA_CUTOFF),
+            "tfs": float(self.TFS),
+            "top_a": float(self.TOP_A),
+            "repetition_penalty": float(self.REPETITION_PENALTY),
+            "encoder_repetition_penalty": float(self.ENCODER_REPETITION_PENALTY),
+            "top_k": int(self.TOP_K),
+            "min_length": float(self.MIN_LENGTH),
+            "no_repeat_ngram_size": float(self.NO_REPEAT_NGRAM_SIZE),
+            "num_beams": int(self.NUM_BEAMS),
+            "penalty_alpha": float(self.PENALTY_ALPHA),
+            "length_penalty": float(self.LENGTH_PENALTY),
             "early_stopping": False,
-            "seed": random.randint(1, 1000000000),
+            "mirostat_mode": float(self.MIROSTAT_MODE),
+            "mirostat_tau": float(self.MIROSTAT_TAU),
+            "mirostat_eta": float(self.MIROSTAT_ETA),
+            "seed": -1,
             "add_bos_token": True,
-            "truncation_length": 4096,
+            "truncation_length": 2048,
             "ban_eos_token": False,
             "skip_special_tokens": True,
+            "custom_stopping_strings": "",  # leave this blank
             "stopping_strings": [],
         }
         response = requests.post(f"{self.AI_PROVIDER_URI}/api/v1/generate", json=params)
         data = None
+        prefixes = ["ASSISTANT: ", f"agent_name: ", "Assistant: ", "AI: ", "Bot: "]
+
+        def strip_prefix(text):
+            for prefix in prefixes:
+                if prefix in text:
+                    return text.split(prefix)[-1]
+            return text
+
         if response.status_code == 200:
             data = response.json()["results"][0]["text"]
             data = re.sub(r"(?<!\\)\\(?!n)", "", data)
+            data = strip_prefix(data)
+
         return data
