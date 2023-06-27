b2204be247f8cff83ba231294f3c6edd30e234a9
ostix360 <55257054+ostix360@users.noreply.github.com>
2023-06-27 14:22:23 +0200

    update gpt4free to V2 (#798)
    * update gpt4free to V2
    
    * reimplementing rotation for gpt4free providers
    
    ---------
    
    Co-authored-by: Josh XT <102809327+Josh-XT@users.noreply.github.com>

--- agixt/provider/gpt4free.py
+++ agixt/provider/gpt4free.py
@@ -1,44 +1,54 @@
-import gpt4free
-import time
 import logging
-import importlib
-import sys
+import time
+
+import g4f
+from g4f.Provider import (
+    Ails,
+    You,
+    Bing,
+    Yqcloud,
+    Theb,
+    Aichat,
+    Bard,
+    Vercel,
+    Forefront,
+    Lockchat,
+    Liaobots,
+    H2o,
+    ChatgptLogin,
+    DeepAi,
+    GetGpt
+)
 
 
 class Gpt4freeProvider:
     def __init__(
-        self,
-        AI_MODEL: str = "gpt-3.5-turbo",
-        AI_TEMPERATURE: float = 0.7,
-        MAX_TOKENS: int = 4000,
-        **kwargs,
+            self,
+            AI_MODEL: str = "gpt-3.5-turbo",
+            AI_TEMPERATURE: float = 0.7,
+            MAX_TOKENS: int = 4000,
+            **kwargs,
     ):
         self.requirements = ["gpt4free"]
-        self.AI_MODEL = AI_MODEL if AI_MODEL else "gpt-3.5-turbo"
-        self.AI_TEMPERATURE = AI_TEMPERATURE if AI_TEMPERATURE else 0.7
-        self.MAX_TOKENS = MAX_TOKENS if MAX_TOKENS else 4000
+        self.AI_MODEL = AI_MODEL
+        self.AI_TEMPERATURE = AI_TEMPERATURE
+        self.MAX_TOKENS = MAX_TOKENS
         self.FAILED_PROVIDERS = []
-        self.providers = [
-            "DeepAI",
-            "You",
-            "UseLess",
-            "ForeFront",
-            "Theb",
-            "AiColors",
+        self.providers = [  # Exclude providers that require auth
+            Forefront,
+            Yqcloud,
+            Aichat,
+            Lockchat,
+            ChatgptLogin,
+            DeepAi,
+            GetGpt,
+            Ails,
+            You,
+            Bing,
+            Theb,
+            Vercel,
+            H2o,
         ]
-        self.account_tokens = {}
-
-    def create_account(self, provider, module):
-        try:
-            # the following call will not terminate the program even if it calls quit()
-            return module.Account.create()
-        except SystemExit:
-            logging.error(f"Account creation for {provider} called quit(), ignoring")
-            return None
-        except Exception as e:
-            # handle other exceptions here
-            logging.error(f"Failed to create account for {provider}: {e}")
-            return None
 
     async def provider_failure(self, provider):
         if provider not in self.FAILED_PROVIDERS:
@@ -52,60 +62,25 @@ class Gpt4freeProvider:
                 time.sleep(10)
 
     async def instruct(self, prompt, tokens: int = 0):
-        while True:
-            for provider in self.providers:
-                try:
-                    if provider not in self.FAILED_PROVIDERS:
-                        logging.info(f"[GPT4Free] Using: {provider}")
-                        if provider not in self.account_tokens:
-                            try:
-                                if provider == "Poe":
-                                    module_name = "quora"
-                                elif provider == "UseLess":
-                                    module_name = "usesless"
-                                else:
-                                    module_name = provider.lower()
-                                module = importlib.import_module(
-                                    "gpt4free.%s" % module_name
-                                )
-                                if module and hasattr(module, "Account"):
-                                    logging.info(f"Create account for: {provider}")
-                                    self.account_tokens[provider] = self.create_account(
-                                        provider, module
-                                    )
-                            except ModuleNotFoundError:
-                                self.account_tokens[provider] = None
-                        args = {}
-                        if provider in self.account_tokens:
-                            if provider == "ForeFront":
-                                args["account_data"] = self.account_tokens[provider]
-                            elif provider == "UseLess":
-                                args["token"] = self.account_tokens[provider]
-                            elif provider == "Poe":
-                                args["token"] = self.account_tokens[provider]
-                                args["model"] = "GPT-4"
-
-                        response = gpt4free.Completion.create(
-                            getattr(gpt4free.Provider, provider), prompt=prompt, **args
-                        )
-                        if response:
-                            if provider == "UseLess":
-                                if "text" in response:
-                                    response = response["text"]
-                                if (
-                                    "status" in response
-                                    and response["status"] == "Fail"
-                                ):
-                                    response = None
-                            if (
-                                response
-                                == "Unable to fetch the response, Please try again."
-                            ):
+        for provider in self.providers:
+            try:
+                if provider not in self.FAILED_PROVIDERS:
+                    response = g4f.ChatCompletion.create(model=g4f.Model.gpt_35_turbo, provider=provider, messages=[
+                        {"role": "user", "content": prompt}])
+                    if response:
+                        if provider == Ails:
+                            if "error" in response and "message" in response:
                                 response = None
-                        if response and len(response) > 1:
-                            return response
-                        else:
-                            await self.provider_failure(provider)
-                except Exception as e:
-                    logging.info(f"[GPT4Free] Exception: {e}")
-                    await self.provider_failure(provider)
+                        elif provider == Vercel:
+                            if response == "Vercel is currently not working.":
+                                response = None
+                        if response == "Unable to fetch the response, Please try again.":
+                            response = None
+                    if response and len(response) > 1:
+                        return response
+                    else:
+                        await self.provider_failure(provider)
+
+            except Exception as e:
+                logging.error(f"[GPT4Free] Exception: {e}")
+                await self.provider_failure(provider)
