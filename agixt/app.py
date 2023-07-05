572a3521b392cfe8f0b7944daddbcfda8102cfc6
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2023-07-05 09:15:47 -0400

    Fix back end launch in docker (#825)
    * Modifying launch settings
    
    * Move db creation to launch script
    
    * Improve back end launcher
    
    * v1.3.12

--- agixt/app.py
+++ agixt/app.py
@@ -8,25 +8,6 @@ import time
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from DBConnection import DBConnection
-
-db = DBConnection()
-try:
-    db.engine.execute("SELECT 1 FROM agent LIMIT 1")
-except Exception as e:
-    # Check if migration.txt exists
-    if os.path.exists("migration.txt"):
-        while os.path.exists("migration.txt"):
-            time.sleep(2)
-    else:
-        # Create migration.txt
-        with open("migration.txt", "w") as f:
-            f.write("1")
-
-from Hub import import_agixt_hub
-
-import_agixt_hub()
-
 from Interactions import Interactions
 from Agent import Agent, add_agent, delete_agent, rename_agent, get_agents
 from Chain import Chain
--- agixt/launch-backend.sh
+++ agixt/launch-backend.sh
@@ -4,13 +4,22 @@ set -e
 
 host="${POSTGRES_SERVER:-db}"
 port="${POSTGRES_PORT:-5432}"
+db="${POSTGRES_DB:-postgres}"
+echo "Waiting for postgres server to start... $host:$port"
+sleep 15
 
 until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -p "$port" -U "$POSTGRES_USER" -c '\q'; do
   >&2 echo "Waiting for database... $host:$port"
   sleep 1
 done
-
->&2 echo "Database started. Starting AGiXT"
-
+sleep 5
+# Check if the database exists, and create it if it doesn't
+PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -p "$port" -U "$POSTGRES_USER" -tc "SELECT 1 FROM pg_database WHERE datname = '$db'" | grep -q 1 || PGPASSWORD=$POSTGRES_PASSWORD createdb -h "$host" -p "$port" -U "$POSTGRES_USER" "$db"
+sleep 5
+python3 DBConnection.py
+sleep 10
+python3 Hub.py
+sleep 5
+echo "Starting AGiXT..."
 # Start the Uvicorn server
 uvicorn app:app --host 0.0.0.0 --port 7437 --workers "$UVICORN_WORKERS" --proxy-headers
