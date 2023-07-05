09e32948c5cf76ad7971e074d03a7dce55339ff7
Josh XT <josh@devxt.com>
2023-07-05 17:21:22 -0400

    Add AGiXT.sh

---
+++ AGiXT.sh
@@ -0,0 +1,231 @@
+#!/bin/bash
+
+# Define colors and formatting
+BOLD=$(tput bold)
+GREEN=$(tput setaf 2)
+CYAN=$(tput setaf 6)
+YELLOW=$(tput setaf 3)
+RED=$(tput setaf 1)
+MAGENTA=$(tput setaf 5)
+BLUE=$(tput setaf 4)
+RESET=$(tput sgr0)
+
+# Function to display a show opening style animation
+display_animation() {
+  # More dynamic smoke animation above ASCII art
+  for i in {1..10}; do
+    clear
+    echo "          (   )"
+    echo "        ( )  ( )"
+    echo "         (  )  ("
+    echo "      _____________"
+    echo "     <_____________> ___"
+    echo "     |             |/ _ \\"
+    echo "     |               | | |"
+    echo "     |               |_| |"
+    echo "  ___|             |\\___/"
+    echo " /    \\___________/    \\"
+    echo " \\_____________________/"
+    sleep 0.2
+
+    clear
+    echo "         (  ) ("
+    echo "        )  (   )"
+    echo "       ( )   ( )"
+    echo "      _____________"
+    echo "     <_____________> ___"
+    echo "     |             |/ _ \\"
+    echo "     |               | | |"
+    echo "     |               |_| |"
+    echo "  ___|             |\\___/"
+    echo " /    \\___________/    \\"
+    echo " \\_____________________/"
+    sleep 0.2
+
+    clear
+    echo "        (  )   ("
+    echo "       )   (    )"
+    echo "      ( )    ( )"
+    echo "      _____________"
+    echo "     <_____________> ___"
+    echo "     |             |/ _ \\"
+    echo "     |               | | |"
+    echo "     |               |_| |"
+    echo "  ___|             |\\___/"
+    echo " /    \\___________/    \\"
+    echo " \\_____________________/"
+    sleep 0.2
+
+    clear
+    echo "         (  ) ("
+    echo "        )  (   )"
+    echo "       ( )   ( )"
+    echo "      _____________"
+    echo "     <_____________> ___"
+    echo "     |             |/ _ \\"
+    echo "     |               | | |"
+    echo "     |               |_| |"
+    echo "  ___|             |\\___/"
+    echo " /    \\___________/    \\"
+    echo " \\_____________________/"
+    sleep 0.2
+  done
+
+
+
+
+
+  # Spinning loading indicator
+  echo "${BOLD}${GREEN}Loading...${RESET}"
+  for i in {1..10}; do
+    for s in / - \\ \|; do
+      printf "\r${BOLD}${GREEN}Loading %s${RESET}" "$s"
+      sleep 0.1
+    done
+  done
+  echo
+}
+
+# Function to display the menu
+display_menu() {
+  clear
+  echo "${BOLD}${CYAN}"
+  echo "    ___   _______ _  ________"
+  echo "   /   | / ____(_) |/ /_  __/"
+  echo "  / /| |/ / __/ /|   / / /   "
+  echo " / ___ / /_/ / //   | / /    "
+  echo "/_/  |_\____/_//_/|_|/_/     "
+  echo "                              "
+  echo "----------------------------------------------------${RESET}"
+  echo "${BOLD}${MAGENTA}Welcome to the AGiXT Installer!${RESET}"
+  echo "${BOLD}${GREEN}Please choose an option:${RESET}"
+  echo "  ${BOLD}${YELLOW}1.${RESET} ${YELLOW}Local Install/Run AGiXT Core${RESET}"
+  echo "  ${BOLD}${YELLOW}2.${RESET} ${YELLOW}Local Install/Run AGiXT Core and Streamlit Web UI${RESET}"
+  echo "  ${BOLD}${YELLOW}3.${RESET} ${YELLOW}Docker Install/Run AGiXT Core and Streamlit Web UI${RESET}"
+  echo "  ${BOLD}${YELLOW}4.${RESET} ${YELLOW}Update pulls latest from repo & pulls latest docker${RESET}"
+  echo "  ${BOLD}${RED}5.${RESET} ${RED}Exit${RESET}"
+  echo ""
+}
+
+# Function to perform the local install
+local_install() {
+    echo "${BOLD}${GREEN}Running local install...${RESET}"
+    display_animation
+
+    echo "${BOLD}${YELLOW}Step 1: Updating the repository...${RESET}"
+    git pull
+    sleep 1
+
+    echo "${BOLD}${YELLOW}Step 2: Upgrading pip...${RESET}"
+    pip install --upgrade pip
+    sleep 1
+
+    echo "${BOLD}${YELLOW}Step 3: Installing requirements...${RESET}"
+    pip install -r static-requirements.txt
+    sleep 1
+    pip install -r requirements.txt
+    sleep 1
+
+    echo "${BOLD}${YELLOW}Step 4: Installing Playwright dependencies...${RESET}"
+    playwright install --with-deps
+    sleep 1
+
+    echo "${BOLD}${YELLOW}Step 5: Set Token Env Var...${RESET}"
+    export TOKENIZERS_PARALLELISM=false &
+    sleep 1
+
+    echo "${BOLD}${YELLOW}Step 6: Running AGiXT Back End...${RESET}"
+    ./agixt/launch-backend.sh &
+    sleep 1
+}
+# Function to perform the local install
+local_install_with_streamlit() {
+    echo "${BOLD}${GREEN}Running local install...${RESET}"
+    display_animation
+
+    echo "${BOLD}${YELLOW}Step 1: Updating the repository...${RESET}"
+    git pull
+    sleep 1
+
+    echo "${BOLD}${YELLOW}Step 2: Upgrading pip...${RESET}"
+    pip install --upgrade pip
+    sleep 1
+
+    echo "${BOLD}${YELLOW}Step 3: Installing requirements...${RESET}"
+    pip install -r static-requirements.txt
+    sleep 1
+    pip install -r requirements.txt
+    sleep 1
+
+    echo "${BOLD}${YELLOW}Step 4: Installing Playwright dependencies...${RESET}"
+    playwright install --with-deps
+    sleep 1
+
+    echo "${BOLD}${YELLOW}Step 5: Set Token Env Var...${RESET}"
+    export TOKENIZERS_PARALLELISM=false &
+    sleep 1
+
+    echo "${BOLD}${YELLOW}Step 6: Running AGiXT Back End...${RESET}"
+    ./agixt/launch-backend.sh &
+    sleep 1
+
+    echo "${BOLD}${YELLOW}Step 7: Running Streamlit Web UI...${RESET}"
+    git clone https://github.com/AGiXT/streamlit
+    cd streamlit
+    pip install -r requirements.txt
+    streamlit run Main.py
+}
+# Function to perform the Docker install
+docker_install() {
+  echo "${BOLD}${GREEN}Running Docker install...${RESET}"
+  display_animation
+
+  echo "${BOLD}${YELLOW}Step 1: Starting Docker Compose...${RESET}"
+  docker-compose up
+}
+
+# Function to perform the Update
+update() {
+  echo "${BOLD}${GREEN}Running Update...${RESET}"
+  display_animation
+
+  echo "${BOLD}${YELLOW}Step 1: Updating the repository...${RESET}"
+  git pull
+ 
+    echo "${BOLD}${YELLOW}Step 2: Pulling latest Docker Images...${RESET}"
+  docker compose pull
+}
+
+# Main loop to display the menu and handle user input
+while true; do
+  display_menu
+  read -p "${BOLD}${CYAN}Enter your choice:${RESET} " choice
+
+  case "$choice" in
+    1)
+      local_install
+      break
+      ;;
+    2)
+      local_install_with_streamlit
+      break
+      ;;
+    3)
+      docker_install
+      break
+      ;;
+    4)
+      update
+      echo "${BOLD}${GREEN}Update complete.${RESET}"
+      sleep 2
+      ;;
+    5)
+      echo "${BOLD}${MAGENTA}Thank you for using AGiXT Installer. Goodbye!${RESET}"
+      break
+      ;;
+    *)
+      echo "${RED}Invalid option. Please try again.${RESET}"
+      sleep 2
+      ;;
+  esac
+done
