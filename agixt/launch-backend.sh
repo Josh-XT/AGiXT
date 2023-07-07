a4c2b01cd3e9dc48ede4cc9f8f08095cec84a81e
Josh XT <josh@devxt.com>
2023-07-07 09:16:59 -0400

    Improved env interactions

--- agixt/launch-backend.sh
+++ agixt/launch-backend.sh
@@ -1,6 +1,31 @@
 #!/bin/sh
 
 set -e
+
+# Check if .env file exists
+if [ -f ".env" ]; then
+  echo "Sourcing .env file..."
+  source .env
+elif [ -f "../.env" ]; then
+  echo "Sourcing ../.env file..."
+  source ../.env
+else
+  # Check if .env.example file exists in the current directory
+  if [ -f ".env.example" ]; then
+    echo "No .env file found, sourcing .env.example from current directory..."
+    source .env.example
+  else
+    # Check if .env.example file exists in the parent directory
+    if [ -f "../.env.example" ]; then
+      echo "No .env or ../.env file found, sourcing .env.example from parent directory..."
+      source ../.env.example
+    else
+      echo "No .env, ../.env, or .env.example file found!"
+      exit 1
+    fi
+  fi
+fi
+
 workers="${UVICORN_WORKERS:-4}"
 
 if [ "$DB_CONNECTED" = "true" ]; then
@@ -23,6 +48,5 @@ if [ "$DB_CONNECTED" = "true" ]; then
 fi
 # Install AGiXT Hub
 python3 Hub.py
-echo "Starting AGiXT..."
-sleep 5
+echo "Starting AGiXT... Please wait until you see 'Applicaton startup complete' before opening Streamlit..."
 uvicorn app:app --host 0.0.0.0 --port 7437 --workers $workers --proxy-headers
