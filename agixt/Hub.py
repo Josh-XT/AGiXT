d9a41ecb88d5fa8aa398de599db5d4acd13a5b1a
Josh XT <josh@devxt.com>
2023-07-26 14:43:07 -0400

    Fix hub over writing agent files

--- agixt/Hub.py
+++ agixt/Hub.py
@@ -42,7 +42,6 @@ def import_agixt_hub():
         response = requests.get(repo_url, auth=(github_user, github_token))
         response.raise_for_status()
 
-        # Check if previous zip exists and compare it with the new one
         new_zip_hash = hashlib.sha256(response.content).hexdigest()
         if os.path.exists(zip_file_name):
             with open(zip_file_name, "rb") as f:
@@ -53,62 +52,61 @@ def import_agixt_hub():
                 )
                 return
 
-        # Save the new zip file
         with open(zip_file_name, "wb") as f:
             f.write(response.content)
 
         zip_ref = zipfile.ZipFile(io.BytesIO(response.content))
         zip_ref.extractall(".")
         zip_ref.close()
-        # Set permissions for the extracted files and directories
+
         for root, dirs, files in os.walk(f"{repo_name}-main"):
             for dir_name in dirs:
                 dir_path = os.path.join(root, dir_name)
-                os.chmod(
-                    dir_path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO
-                )  # Set permissions to read, write, and execute for all
+                os.chmod(dir_path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
             for file_name in files:
                 file_path = os.path.join(root, file_name)
                 os.chmod(
                     file_path, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IROTH
-                )  # Set permissions to read for owner and group, read-only for others
+                )
 
         print(f"Updating AGiXT Hub from {github_repo}")
-        # Move the files and directories from the reponame-main directory to the current directory
+
         for file in os.listdir(f"{repo_name}-main"):
             src_file = os.path.join(f"{repo_name}-main", file)
             dest_file = os.path.join(".", file)
 
             if os.path.isdir(src_file):
-                if os.path.exists(dest_file):
-                    for item in os.listdir(dest_file):
-                        dest_item = os.path.join(dest_file, item)
-                        if os.path.isfile(dest_item) and "config.json" not in dest_item:
-                            os.remove(dest_item)
-                else:
+                if not os.path.exists(dest_file):
                     os.makedirs(dest_file, exist_ok=True)
-
                 for item in os.listdir(src_file):
                     src_item = os.path.join(src_file, item)
                     dest_item = os.path.join(dest_file, item)
                     if os.path.isdir(src_item):
-                        if os.path.exists(dest_item):
-                            shutil.rmtree(dest_item)
-                        shutil.copytree(src_item, dest_item)
+                        if not os.path.exists(dest_item):
+                            os.makedirs(dest_item, exist_ok=True)
+                        for dirpath, dirnames, filenames in os.walk(src_item):
+                            destination = dirpath.replace(src_item, dest_item, 1)
+                            if not os.path.exists(destination):
+                                os.makedirs(destination)
+                            for filename in filenames:
+                                if filename != "config.json" or not os.path.exists(
+                                    os.path.join(destination, filename)
+                                ):
+                                    shutil.copy2(
+                                        os.path.join(dirpath, filename), destination
+                                    )
                     else:
                         if not (
-                            "config.json" not in dest_item and os.path.exists(dest_item)
-                        ):  # Don't overwrite existing config.json
+                            dest_item.endswith("config.json")
+                            and os.path.exists(dest_item)
+                        ):
                             shutil.copy2(src_item, dest_item)
             else:
-                if "config.json" not in dest_file and not os.path.exists(
-                    dest_file
-                ):  # Don't overwrite existing config.json
-                    if os.path.exists(dest_file):
-                        os.remove(dest_file)
+                if not (
+                    dest_file.endswith("config.json") and os.path.exists(dest_file)
+                ):
                     shutil.move(src_file, dest_file)
 
-        # Remove the reponame-main directory
         shutil.rmtree(f"{repo_name}-main")
         print(f"Updated AGiXT Hub from {github_repo}")
     except Exception as e:
