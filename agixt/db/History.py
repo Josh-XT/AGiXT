c2d06b9ce7246784ec368e37f162868c1c3c7507
Josh XT <josh@devxt.com>
2023-08-07 22:55:04 -0400

    Add UUID as default conversation ID

--- agixt/db/History.py
+++ agixt/db/History.py
@@ -1,5 +1,6 @@
 import os
 import yaml
+import uuid
 from datetime import datetime
 from DBConnection import (
     Conversation,
@@ -14,9 +15,7 @@ def export_conversation(agent_name, conversation_name=None):
     if not agent:
         print(f"Agent '{agent_name}' not found in the database.")
         return
-    conversation_name = (
-        f"{agent_name} History" if not conversation_name else conversation_name
-    )
+    conversation_name = uuid.uuid4() if not conversation_name else conversation_name
     conversation = (
         session.query(Conversation)
         .filter(
@@ -74,7 +73,7 @@ def get_conversation(agent_name, conversation_name=None, limit=100, page=1):
         print(f"Agent '{agent_name}' not found in the database.")
         return
     if not conversation_name:
-        conversation_name = f"{agent_name} History"
+        conversation_name = uuid.uuid4()
     conversation = (
         session.query(Conversation)
         .filter(
@@ -173,7 +172,7 @@ def delete_history(agent_name, conversation_name=None):
         print(f"Agent '{agent_name}' not found in the database.")
         return
     if not conversation_name:
-        conversation_name = f"{agent_name} History"
+        conversation_name = uuid.uuid4()
     conversation = (
         session.query(Conversation)
         .filter(
