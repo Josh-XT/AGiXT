c4d51ad3e1f796abd5ab507ac3bb8948bae99550
Josh XT <josh@devxt.com>
2023-08-08 23:45:27 -0400

    Clean up api key verify function

--- agixt/app.py
+++ agixt/app.py
@@ -72,25 +72,23 @@ app.add_middleware(
 )
 
 
-async def get_api_key(authorization: str = Header(None)):
+def verify_api_key(authorization: str = Header(None)):
+    # Check if the API key is set up in the environment
     if AGIXT_API_KEY:
+        # If no authorization header is provided, raise an error
         if authorization is None:
             raise HTTPException(
                 status_code=400, detail="Authorization header is missing"
             )
         scheme, _, api_key = authorization.partition(" ")
+        # If the scheme isn't "Bearer", raise an error
         if scheme.lower() != "bearer":
             raise HTTPException(
                 status_code=400, detail="Authorization scheme is not Bearer"
             )
-        return api_key
-    else:
-        return None
-
-
-def verify_api_key(api_key: str = Depends(get_api_key)):
-    if AGIXT_API_KEY and api_key != AGIXT_API_KEY:
-        raise HTTPException(status_code=403, detail="Invalid API Key")
+        # If the provided API key doesn't match the expected one, raise an error
+        if api_key != AGIXT_API_KEY:
+            raise HTTPException(status_code=403, detail="Invalid API Key")
     return api_key
 
 
