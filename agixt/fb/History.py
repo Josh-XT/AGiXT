b08295415b04b326a3a810d9d9f53c6d3a66f511
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2023-08-09 20:54:05 -0400

    Fix history duplication (#882)

--- agixt/fb/History.py
+++ agixt/fb/History.py
@@ -16,17 +16,15 @@ def export_conversation(conversation_name=None, agent_name=None):
 
 
 def get_conversation(conversation_name=None, limit=100, page=1, agent_name=None):
-    if not conversation_name:
-        conversation_name = uuid.uuid4()
-    history_file = os.path.join("conversations", f"{conversation_name}.yaml")
-    os.makedirs(os.path.dirname(history_file), exist_ok=True)
-    if os.path.exists(history_file):
-        with open(history_file, "r") as file:
-            history = yaml.safe_load(file)
-        if not history:
-            history = {"interactions": []}
-        return history
-    return new_conversation(conversation_name=conversation_name)
+    history = {"interactions": []}
+    try:
+        history_file = os.path.join("conversations", f"{conversation_name}.yaml")
+        if os.path.exists(history_file):
+            with open(history_file, "r") as file:
+                history = yaml.safe_load(file)
+    except Exception as e:
+        print(e)
+    return history
 
 
 def get_conversations(agent_name=None):
@@ -50,7 +48,8 @@ def new_conversation(conversation_name, agent_name=None):
 def log_interaction(role: str, message: str, conversation_name=None, agent_name=None):
     history = get_conversation(conversation_name=conversation_name)
     history_file = os.path.join("conversations", f"{conversation_name}.yaml")
-    os.makedirs(os.path.dirname(history_file), exist_ok=True)
+    if not os.path.exists(history_file):
+        os.makedirs(os.path.dirname(history_file), exist_ok=True)
     if not history:
         history = {"interactions": []}
     if "interactions" not in history:
@@ -67,27 +66,21 @@ def log_interaction(role: str, message: str, conversation_name=None, agent_name=
 
 
 def delete_history(conversation_name=None, agent_name=None):
-    if not conversation_name:
-        conversation_name = uuid.uuid4()
     history_file = os.path.join("conversations", f"{conversation_name}.yaml")
-
     if os.path.exists(history_file):
         os.remove(history_file)
 
 
 def delete_message(message, conversation_name=None, agent_name=None):
-    history = get_conversation(
-        agent_name=agent_name, conversation_name=conversation_name
-    )
-    history["interactions"] = [
-        interaction
-        for interaction in history["interactions"]
-        if interaction["message"] != message
-    ]
-    if not conversation_name:
-        conversation_name = "history"
-    history_file = os.path.join(
-        "conversations", agent_name, f"{conversation_name}.yaml"
-    )
-    with open(history_file, "w") as file:
-        yaml.safe_dump(history, file)
+    if conversation_name:
+        history = get_conversation(
+            agent_name=agent_name, conversation_name=conversation_name
+        )
+        history["interactions"] = [
+            interaction
+            for interaction in history["interactions"]
+            if interaction["message"] != message
+        ]
+        history_file = os.path.join("conversations", f"{conversation_name}.yaml")
+        with open(history_file, "w") as file:
+            yaml.safe_dump(history, file)
