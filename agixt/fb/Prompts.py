f48c0be94258941ec96930943c6e9a9387c9dd3e
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2023-08-11 19:53:17 -0400

    Add prompt category functionality to endpoints (#895)
    * Improve prompt category functionality
    
    * Update db
    
    * Naming consistency
    
    * Add get_prompt_categories
    
    * v1.3.91

--- agixt/fb/Prompts.py
+++ agixt/fb/Prompts.py
@@ -31,10 +31,10 @@ def get_prompt_file_path(prompt_name, prompt_category="Default"):
 
 
 class Prompts:
-    def add_prompt(self, prompt_name, prompt, prompt_category_name="Default"):
+    def add_prompt(self, prompt_name, prompt, prompt_category="Default"):
         # if prompts folder does not exist, create it
         file_path = get_prompt_file_path(
-            prompt_name=prompt_name, prompt_category=prompt_category_name
+            prompt_name=prompt_name, prompt_category=prompt_category
         )
         # if prompt file does not exist, create it
         if not os.path.exists(file_path):
@@ -49,16 +49,32 @@ class Prompts:
             prompt = f.read()
         return prompt
 
-    def get_prompts(self):
+    def get_prompts(self, prompt_category="Default"):
         # Get all files in prompts folder that end in .txt and replace .txt with empty string
         prompts = []
         # For each folder in prompts folder, get all files that end in .txt and replace .txt with empty string
+        base_path = os.path.join("prompts", prompt_category)
+        base_path = os.path.join(os.getcwd(), "prompts")
+        base_model_path = os.path.normpath(
+            os.path.join(os.getcwd(), "prompts", prompt_category)
+        )
+        if not base_model_path.startswith(base_path) or not base_model_path.startswith(
+            base_model_path
+        ):
+            raise ValueError(
+                "Invalid file path. Prompt name cannot contain '/', '\\' or '..' in"
+            )
+        for file in os.listdir(base_model_path):
+            if file.endswith(".txt"):
+                prompts.append(file.replace(".txt", ""))
+        return prompts
+
+    def get_prompt_categories(self):
+        prompt_categories = []
         for folder in os.listdir("prompts"):
             if os.path.isdir(os.path.join("prompts", folder)):
-                for file in os.listdir(os.path.join("prompts", folder)):
-                    if file.endswith(".txt"):
-                        prompts.append(file.replace(".txt", ""))
-        return prompts
+                prompt_categories.append(folder)
+        return prompt_categories
 
     def get_prompt_args(self, prompt_text):
         # Find anything in the file between { and } and add them to a list to return
@@ -68,16 +84,24 @@ class Prompts:
                 prompt_vars.append(word[1:-1])
         return prompt_vars
 
-    def delete_prompt(self, prompt_name):
-        prompt_file = get_prompt_file_path(prompt_name=prompt_name)
+    def delete_prompt(self, prompt_name, prompt_category="Default"):
+        prompt_file = get_prompt_file_path(
+            prompt_name=prompt_name, prompt_category=prompt_category
+        )
         os.remove(prompt_file)
 
-    def update_prompt(self, prompt_name, prompt):
-        prompt_file = get_prompt_file_path(prompt_name=prompt_name)
+    def update_prompt(self, prompt_name, prompt, prompt_category="Default"):
+        prompt_file = get_prompt_file_path(
+            prompt_name=prompt_name, prompt_category=prompt_category
+        )
         with open(prompt_file, "w") as f:
             f.write(prompt)
 
-    def rename_prompt(self, prompt_name, new_prompt_name):
-        prompt_file = get_prompt_file_path(prompt_name=prompt_name)
-        new_prompt_file = get_prompt_file_path(prompt_name=new_prompt_name)
+    def rename_prompt(self, prompt_name, new_prompt_name, prompt_category="Default"):
+        prompt_file = get_prompt_file_path(
+            prompt_name=prompt_name, prompt_category=prompt_category
+        )
+        new_prompt_file = get_prompt_file_path(
+            prompt_name=new_prompt_name, prompt_category=prompt_category
+        )
         os.rename(prompt_file, new_prompt_file)
