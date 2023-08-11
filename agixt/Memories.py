edfd6220519019aed7ce5f46d840877dc7ea77f3
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2023-08-11 16:28:17 -0400

    Add branch to github learning (#892)

--- agixt/Memories.py
+++ agixt/Memories.py
@@ -312,15 +312,31 @@ class Memories:
             return text_content, link_list
 
     async def read_github_repo(
-        self, github_repo="Josh-XT/AGiXT", github_user=None, github_token=None
+        self,
+        github_repo="Josh-XT/AGiXT",
+        github_user=None,
+        github_token=None,
+        github_branch="main",
     ):
         github_repo = github_repo.replace("https://github.com/", "")
         github_repo = github_repo.replace("https://www.github.com/", "")
+        if not github_branch:
+            github_branch = "main"
         user = github_repo.split("/")[0]
         repo = github_repo.split("/")[1]
-        repo_url = f"https://github.com/{user}/{repo}/archive/refs/heads/main.zip"
-        zip_file_name = f"{repo}_main.zip"
-        response = requests.get(repo_url, auth=(github_user, github_token))
+        repo_url = (
+            f"https://github.com/{user}/{repo}/archive/refs/heads/{github_branch}.zip"
+        )
+        try:
+            response = requests.get(repo_url, auth=(github_user, github_token))
+        except:
+            github_branch = "master"
+            repo_url = f"https://github.com/{user}/{repo}/archive/refs/heads/{github_branch}.zip"
+            try:
+                response = requests.get(repo_url, auth=(github_user, github_token))
+            except:
+                return False
+        zip_file_name = f"{repo}_{github_branch}.zip"
         with open(zip_file_name, "wb") as f:
             f.write(response.content)
         await self.read_file(file_path=zip_file_name)
