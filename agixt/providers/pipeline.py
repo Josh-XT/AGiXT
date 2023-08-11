31d632800d1e9c31cd8c636d3b0e8fb5e1ad3f42
Josh XT <josh@devxt.com>
2023-08-11 13:54:14 -0400

    Update from PR on hub

--- agixt/providers/pipeline.py
+++ agixt/providers/pipeline.py
@@ -44,22 +44,22 @@ class PipelineProvider:
         self.pipeline_kwargs = kwargs
         if HUGGINGFACE_API_KEY:
             self.pipeline_kwargs["use_auth_token"] = HUGGINGFACE_API_KEY
-        self.pipeline_kwargs["return_full_text"] = False
 
     async def instruct(self, prompt, tokens: int = 0):
         self.load_pipeline()
         return self.pipeline(
             prompt,
             temperature=self.AI_TEMPERATURE,
+            return_full_text=False,
             max_new_tokens=self.get_max_new_tokens(tokens)
         )[0]["generated_text"]
     
-    def load_cuda(self):
+    def load_args(self):
         if is_cuda_available():
             # Use "half" = bfloat16 or float16
-            if "torch_dtype" not in self.pipeline_kwargs and torch.cuda.is_bf16_supported():
+            if "torch_dtype" not in self.pipeline_kwargs:
                 self.pipeline_kwargs["torch_dtype"] = torch.bfloat16 if torch.cuda.is_bf16_supported() else torch.float16
-            # Use "cuda"
+            # Enable GPU support
             if "device_map" not in self.pipeline_kwargs and "device" not in self.pipeline_kwargs:
                 self.pipeline_kwargs["device_map"] = "auto"
                 # Use "quantization"
@@ -68,7 +68,7 @@ class PipelineProvider:
 
     def load_pipeline(self):
         if not self.pipeline:
-            self.load_cuda()
+            self.load_args()
             self.pipeline = pipeline("text-generation", self.MODEL_PATH, **self.pipeline_kwargs)
         
     def get_max_length(self):
@@ -81,8 +81,7 @@ class PipelineProvider:
         return max_length
     
     def get_max_new_tokens(self, input_length: int = 0) -> int:
-        max_length = self.get_max_length()
-        max_length -= input_length
+        max_length = self.get_max_length() - input_length
         if max_length > 0 and self.MAX_TOKENS > max_length:
             return max_length
         return self.MAX_TOKENS
