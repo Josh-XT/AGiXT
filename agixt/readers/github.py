5f3a84922072e3a543e0308aead6d2acf7466255
Josh XT <josh@devxt.com>
2023-08-14 12:14:21 -0400

    Add Github Reader

--- agixt/readers/github.py
+++ agixt/readers/github.py
@@ -4,8 +4,21 @@ import os
 
 
 class GithubReader(Memories):
-    def __init__(self, agent_name):
-        super().__init__(agent_name=agent_name)
+    def __init__(
+        self, agent_name, agent_config, use_agent_settings: bool = False, **kwargs
+    ):
+        super().__init__(agent_name=agent_name, agent_config=agent_config)
+        self.use_agent_settings = use_agent_settings
+        if (
+            use_agent_settings == True
+            and "GITHUB_USERNAME" in self.agent_config["settings"]
+            and "GITHUB_API_KEY" in self.agent_config["settings"]
+        ):
+            self.github_user = self.agent_config["settings"]["GITHUB_USERNAME"]
+            self.github_token = self.agent_config["settings"]["GITHUB_API_KEY"]
+        else:
+            self.github_user = None
+            self.github_token = None
 
     async def read_github_repo(
         self,
@@ -14,6 +27,9 @@ class GithubReader(Memories):
         github_token=None,
         github_branch="main",
     ):
+        if self.use_agent_settings == True:
+            github_user = self.github_user if self.github_user else github_user
+            github_token = self.github_token if self.github_token else github_token
         github_repo = github_repo.replace("https://github.com/", "")
         github_repo = github_repo.replace("https://www.github.com/", "")
         if not github_branch:
