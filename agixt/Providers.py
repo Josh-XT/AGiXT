f6b9ebaf6dfa49757e49b30e1b76ad2fcd72ac1d
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2023-08-15 15:58:54 -0400

    Add provider import error handling (#925)

--- agixt/Providers.py
+++ agixt/Providers.py
@@ -16,15 +16,28 @@ def get_providers():
 
 def get_provider_options(provider_name):
     provider_name = provider_name.lower()
-    module = importlib.import_module(f"providers.{provider_name}")
-    provider_class = getattr(module, f"{provider_name.capitalize()}Provider")
-    signature = inspect.signature(provider_class.__init__)
-    options = {
-        name: param.default if param.default is not inspect.Parameter.empty else None
-        for name, param in signature.parameters.items()
-        if name != "self" and name != "kwargs"
-    }
-    options["provider"] = provider_name
+    try:
+        module = importlib.import_module(f"providers.{provider_name}")
+        provider_class = getattr(module, f"{provider_name.capitalize()}Provider")
+        signature = inspect.signature(provider_class.__init__)
+        options = {
+            name: param.default
+            if param.default is not inspect.Parameter.empty
+            else None
+            for name, param in signature.parameters.items()
+            if name != "self" and name != "kwargs"
+        }
+        options["provider"] = provider_name
+    except:
+        options = {
+            "provider": provider_name,
+            "MAX_TOKENS": 4096,
+            "AI_MODEL": "gpt-3.5-turbo",
+            "AI_TOP_P": 0.7,
+            "AI_TEMPERATURE": 0.7,
+            "WAIT_BETWEEN_REQUESTS": 1,
+            "WAIT_AFTER_FAILURE": 3,
+        }
     return options
 
 
