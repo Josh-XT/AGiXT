8632dc929575f0590dcb0f18799c4cb82381facc
ostix360 <55257054+ostix360@users.noreply.github.com>
2023-08-29 16:26:02 +0200

    nbox little fix (#959)
    * Add nbox provider
    
    * Standardize with the other providers
    
    Signed-off-by: Josh XT <102809327+Josh-XT@users.noreply.github.com>
    
    * nbox little fix
    
    ---------
    
    Signed-off-by: Josh XT <102809327+Josh-XT@users.noreply.github.com>
    Co-authored-by: Josh XT <102809327+Josh-XT@users.noreply.github.com>

--- agixt/providers/nbox.py
+++ agixt/providers/nbox.py
@@ -4,18 +4,23 @@ import requests
 class NboxProvider:
     def __init__(
         self,
-        AI_TEMPERATURE: float = 0.7,
-        MAX_TOKENS: int = 4096,
         NBOX_TOKEN: str = "",
+        AI_TEMPERATURE: float = 0.7,
+        MAX_TOKENS: int = 4085,
         AI_MODEL: str = "llama-2-chat-70b-4k",
     ):
-        self.AI_TEMPERATURE = AI_TEMPERATURE if AI_TEMPERATURE else 0.7
-        self.MAX_TOKENS = MAX_TOKENS if MAX_TOKENS else 4096
-        self.AI_MODEL = AI_MODEL.lower() if AI_MODEL else "llama-2-chat-70b-4k"
         self.NBOX_TOKEN = NBOX_TOKEN
+        self.AI_TEMPERATURE = AI_TEMPERATURE if AI_TEMPERATURE else 0.7
+        self.MAX_TOKENS = MAX_TOKENS if MAX_TOKENS else 4085
+        self.AI_MODEL = AI_MODEL.lower() if AI_MODEL else "llama-2-chat-70b-4k"
 
     async def instruct(self, prompt, tokens: int = 0):
-        max_new_tokens = int(self.MAX_TOKENS) - tokens
+        max_new_tokens = int(self.MAX_TOKENS - tokens)
+        if max_new_tokens < 0:
+            raise Exception(f"Max tokens exceeded: {max_new_tokens}")
+        elif max_new_tokens > 4085:
+            max_new_tokens = 4085
+        print(max_new_tokens)
         params = {
             "messages": [{"role": "user", "content": prompt}],
             "model": self.AI_MODEL,
