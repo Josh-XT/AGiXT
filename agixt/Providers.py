b8f522762ff10632d8c2efa3a7f10ed27c4e3a40
Josh XT <josh@devxt.com>
2023-09-23 10:24:12 -0400

    Fix import error

--- agixt/Providers.py
+++ agixt/Providers.py
@@ -5,7 +5,23 @@ import glob
 import os
 import inspect
 from dotenv import load_dotenv
-from fb.Agent import DEFAULT_SETTINGS
+
+DEFAULT_SETTINGS = {
+    "provider": "gpt4free",
+    "embedder": "default",
+    "AI_MODEL": "gpt-3.5-turbo",
+    "AI_TEMPERATURE": "0.7",
+    "AI_TOP_P": "1",
+    "MAX_TOKENS": "4096",
+    "helper_agent_name": "gpt4free",
+    "WEBSEARCH_TIMEOUT": 0,
+    "WAIT_BETWEEN_REQUESTS": 1,
+    "WAIT_AFTER_FAILURE": 3,
+    "stream": False,
+    "WORKING_DIRECTORY": "./WORKSPACE",
+    "WORKING_DIRECTORY_RESTRICTED": True,
+    "AUTONOMOUS_EXECUTION": False,
+}
 
 load_dotenv()
 
