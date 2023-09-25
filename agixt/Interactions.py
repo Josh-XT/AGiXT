2f7e2006d831f9b856896044d6271323ca20bc46
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2023-09-25 06:15:39 -0400

    Use "Chat" if no commands are available (#988)
    * Use Chat if no commands are available
    
    * Use empty string instead of None
    
    * Updates
    
    * Enable autonomous execution by default

--- agixt/Interactions.py
+++ agixt/Interactions.py
@@ -241,6 +241,8 @@ class Interactions:
     }
 }
         """
+        if prompt_name == "Chat with Commands" and command_list == "":
+            prompt_name = "Chat"
         formatted_prompt = self.custom_format(
             string=prompt,
             user_input=user_input,
