6fa61fb7aa0a1a5e1ba8b72d46bf9cc57fba1b5b
Heiner Lohaus <heiner@lohaus.eu>
2023-09-25 16:30:57 +0200

    Use new async function (#990)
    Use default RetryProvider
    
    Co-authored-by: Josh XT <102809327+Josh-XT@users.noreply.github.com>

--- agixt/providers/gpt4free.py
+++ agixt/providers/gpt4free.py
@@ -1,81 +1,13 @@
-import asyncio
 import logging
-import time
-import random
-from concurrent.futures import ThreadPoolExecutor
+import asyncio
 
-from g4f import Provider, ChatCompletion
-from g4f.models import ModelUtils
-
-providers = [
-    # Working:
-    Provider.Wewordle,
-    Provider.Ails,
-    Provider.Aivvm,
-    Provider.Bing,
-    Provider.ChatBase,
-    Provider.ChatgptAi,
-    Provider.ChatgptLogin,
-    Provider.DeepAi,
-    Provider.Opchatgpts,
-    Provider.Vitalentum,
-    Provider.Ylokh,
-    # Works sometimes:
-    Provider.Aichat,
-    Provider.AItianhu,
-    Provider.Acytoo,
-    Provider.Vercel,
-    Provider.You,
-    Provider.Yqcloud,
-    Provider.Theb,
-    # Not working today:
-    Provider.AiService,
-    #   Provider.DfeHub, endless loop
-    Provider.EasyChat,
-    Provider.Equing,
-    Provider.FastGpt,
-    Provider.Forefront,
-    Provider.GetGpt,
-    Provider.Liaobots,
-    Provider.Lockchat,
-    Provider.V50,
-    Provider.Wuguokai,
-    Provider.H2o,
-]
-
-safe_providers = [
-    Provider.Wewordle,
-    Provider.Ails,
-    Provider.Aivvm,
-    Provider.Bing,
-    Provider.ChatBase,
-    Provider.ChatgptAi,
-    Provider.ChatgptLogin,
-    Provider.DeepAi,
-    Provider.Opchatgpts,
-    Provider.Vitalentum,
-    Provider.Ylokh,
-]
-
-
-def validate_response(response):
-    if not response:
-        raise RuntimeError("Empty response")
-    elif not isinstance(response, str):
-        raise RuntimeError("Response is not a string")
-    elif response in (
-        "Vercel is currently not working.",
-        "Unable to fetch the response, Please try again.",
-    ) or response.startswith('{"error":{"message":'):
-        raise RuntimeError(f"Response: {response}")
-    else:
-        return response
-
+from g4f.Provider import RetryProvider
+from g4f.models import ModelUtils, gpt_35_turbo, default
 
 class Gpt4freeProvider:
     def __init__(
         self,
-        AI_MODEL: str = "gpt-3.5-turbo",
+        AI_MODEL: str = gpt_35_turbo.name,
         MAX_TOKENS: int = 4096,
         AI_TEMPERATURE: float = 0.7,
         AI_TOP_P: float = 0.7,
@@ -84,7 +16,12 @@ class Gpt4freeProvider:
         **kwargs,
     ):
         self.requirements = ["g4f", "httpx"]
-        self.AI_MODEL = AI_MODEL if AI_MODEL else "gpt-3.5-turbo"
+        if not AI_MODEL:
+            self.AI_MODEL = default
+        elif AI_MODEL in ModelUtils.convert:
+            self.AI_MODEL = ModelUtils.convert[AI_MODEL]
+        else:
+            raise ValueError(f"Model not found: {AI_MODEL}")
         self.AI_TEMPERATURE = AI_TEMPERATURE if AI_TEMPERATURE else 0.7
         self.MAX_TOKENS = MAX_TOKENS if MAX_TOKENS else 4096
         self.AI_TOP_P = AI_TOP_P if AI_TOP_P else 0.7
@@ -97,47 +34,41 @@ class Gpt4freeProvider:
         max_new_tokens = (
             int(self.MAX_TOKENS) - int(tokens) if tokens > 0 else self.MAX_TOKENS
         )
-        provider = random.choice(safe_providers)
-        loop = asyncio.get_event_loop()
-
-        answer = await loop.run_in_executor(
-            ThreadPoolExecutor(),
-            self.instruct_provider,
-            provider,
-            max_new_tokens,
-            prompt,
-        )
-        if answer is not None:
-            return answer
-        for provider in providers:
-            answer = await loop.run_in_executor(
-                ThreadPoolExecutor(),
-                self.instruct_provider,
-                provider,
-                max_new_tokens,
-                prompt,
-            )
-            if answer is not None:
-                return answer
-
-    def instruct_provider(self, provider, max_new_tokens, prompt):
-        if not provider.working:
-            return
-        if int(self.WAIT_BETWEEN_REQUESTS) > 0:
-            time.sleep(int(self.WAIT_BETWEEN_REQUESTS))
+        model = self.AI_MODEL
+        provider = model.best_provider
+        if provider:
+            append_model = f" and model: {model.name}" if model.name else ""
+            logging.info(f"[Gpt4Free] Use provider: {provider.__name__}{append_model}")
         try:
-            logging.info(f"[Gpt4Free] Use provider: {provider.__name__}")
-            response = ChatCompletion.create(
-                model=ModelUtils.convert[self.AI_MODEL],
-                provider=provider,
-                messages=[{"role": "user", "content": prompt}],
-                max_tokens=max_new_tokens,
-                temperature=float(self.AI_TEMPERATURE),
-                top_p=float(self.AI_TOP_P),
-                stream=False,
-            )
-            return validate_response(response=response)
+            return (await asyncio.gather(
+                provider.create_async(
+                    model=model.name,
+                    messages=[{"role": "user", "content": prompt}],
+                    max_tokens=max_new_tokens,
+                    temperature=float(self.AI_TEMPERATURE),
+                    top_p=float(self.AI_TOP_P),
+                ),
+                asyncio.sleep(int(self.WAIT_BETWEEN_REQUESTS))
+            ))[0]
         except Exception as e:
-            logging.error(f"[Gpt4Free] Skip provider: {e}")
             if int(self.WAIT_AFTER_FAILURE) > 0:
-                time.sleep(int(self.WAIT_AFTER_FAILURE))
+                await asyncio.sleep(int(self.WAIT_AFTER_FAILURE))
+            raise e
+        finally:
+            if provider and isinstance(provider, RetryProvider):
+                if hasattr(provider, "exceptions"):
+                    for provider_name in provider.exceptions:
+                        error = provider.exceptions[provider_name]
+                        logging.error(f"[Gpt4Free] {provider_name}: {error}")
+
+if __name__ == "__main__":
+    import asyncio, time
+
+    async def run_test():
+        petal = Gpt4freeProvider()
+        start = time.time()
+        response = await petal.instruct("What is the meaning of life?")
+        print(response)
+        print(f"{round(time.time()-start, 2)} secs")
+
+    asyncio.run(run_test())
