bd070613d785cb0edf87efefc484d0714dba5063
Josh XT <josh@devxt.com>
2023-09-27 12:39:05 -0400

    Add JWT

--- agixt/ApiClient.py
+++ agixt/ApiClient.py
@@ -1,10 +1,12 @@
 import os
+import jwt
 from agixtsdk import AGiXTSDK
 from dotenv import load_dotenv
 from fastapi import Header, HTTPException
 
 load_dotenv()
 AGIXT_API_KEY = os.getenv("AGIXT_API_KEY", None)
+USING_JWT = True if os.getenv("USING_JWT", "false").lower() == "true" else False
 DB_CONNECTED = True if os.getenv("DB_CONNECTED", "false").lower() == "true" else False
 ApiClient = AGiXTSDK(base_uri="http://localhost:7437", api_key=AGIXT_API_KEY)
 # Defining these here to be referenced externally.
@@ -35,22 +37,28 @@ else:
 
 
 def verify_api_key(authorization: str = Header(None)):
-    # Check if the API key is set up in the environment
     if AGIXT_API_KEY:
-        # If no authorization header is provided, raise an error
         if authorization is None:
             raise HTTPException(
-                status_code=400, detail="Authorization header is missing"
+                status_code=401, detail="Authorization header is missing"
             )
-        scheme, _, api_key = authorization.partition(" ")
-        # If the scheme isn't "Bearer", raise an error
-        if scheme.lower() != "bearer":
-            raise HTTPException(
-                status_code=400, detail="Authorization scheme is not Bearer"
-            )
-        # If the provided API key doesn't match the expected one, raise an error
-        if api_key != AGIXT_API_KEY:
-            raise HTTPException(status_code=403, detail="Invalid API Key")
-        return api_key
+        try:
+            scheme, _, api_key = authorization.partition(" ")
+            if scheme.lower() != "bearer":
+                raise HTTPException(
+                    status_code=401, detail="Invalid authentication scheme"
+                )
+            if USING_JWT:
+                return jwt.decode(
+                    jwt=api_key,
+                    key=AGIXT_API_KEY,
+                    algorithms=["HS256"],
+                )
+            else:
+                if api_key != AGIXT_API_KEY:
+                    raise HTTPException(status_code=401, detail="Invalid API Key")
+                return api_key
+        except Exception as e:
+            raise HTTPException(status_code=401, detail="Invalid API Key")
     else:
         return 1
