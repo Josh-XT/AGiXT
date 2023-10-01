3bf8c339491f27d3af36bad9053926e1f8097e6d
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2023-10-01 11:16:01 -0400

    Add voice chat extension (#1022)
    * Add voice chat
    
    * Add voice chat extension
    
    * Use conversation name injection

---
+++ agixt/extensions/voice_chat.py
@@ -0,0 +1,68 @@
+from ApiClient import ApiClient
+from Extensions import Extensions
+
+
+class voice_chat(Extensions):
+    def __init__(self, **kwargs):
+        if "agent_name" in kwargs:
+            self.agent_name = kwargs["agent_name"]
+        else:
+            self.agent_name = "gpt4free"
+        self.voice_prompt = "Voice Chat without Commands"
+        if "enabled_commands" in kwargs:
+            if len(kwargs["enabled_commands"]) > 0:
+                self.voice_prompt = "Voice Chat"
+        self.tts_command = "Speak with TTS with Streamlabs Text to Speech"
+        if "USE_STREAMLABS_TTS" in kwargs:
+            if kwargs["USE_STREAMLABS_TTS"].lower() == "true":
+                self.tts_command = "Speak with TTS with Streamlabs Text to Speech"
+        if "USE_GTTS" in kwargs:
+            if kwargs["USE_GTTS"].lower() == "true":
+                self.tts_command = "Speak with GTTS"
+        if "USE_HUGGINGFACE_TTS" in kwargs:
+            if (
+                kwargs["USE_HUGGINGFACE_TTS"].lower() == "true"
+                and "HUGGINGFACE_API_KEY" in kwargs
+            ):
+                if kwargs["HUGGINGFACE_API_KEY"] != "":
+                    self.tts_command = "Read Audio with Huggingface"
+        if "ELEVENLABS_API_KEY" in kwargs:
+            if kwargs["ELEVENLABS_API_KEY"] != "":
+                self.tts_command = "Speak with TTS Using Elevenlabs"
+        self.commands = {
+            "Chat with Voice": self.chat_with_voice,
+        }
+        self.conversation_name = f"Voice Chat with {self.agent_name}"
+        if "conversation_name" in kwargs:
+            self.conversation_name = kwargs["conversation_name"]
+
+    def chat_with_voice(
+        self,
+        base64_audio,
+        conversation_results=3,
+        context_results=10,
+    ):
+        # Transcribe the audio to text.
+        user_input = ApiClient.execute_command(
+            agent_name=self.agent_name,
+            command_name="Transcribe Base64 Audio",
+            command_args={"base64_audio": base64_audio},
+        )
+        # Send the transcribed text to the agent.
+        text_response = ApiClient.prompt_agent(
+            agent_name=self.agent_name,
+            prompt_name=self.voice_prompt,
+            prompt_args={
+                "user_input": user_input,
+                "conversation_name": self.conversation_name,
+                "conversation_results": conversation_results,
+                "context_results": context_results,
+            },
+        )
+        # Get the audio response from the TTS engine and return it.
+        audio_response = ApiClient.execute_command(
+            agent_name=self.agent_name,
+            command_name=self.tts_command,
+            command_args={"text": text_response},
+        )
+        return audio_response
