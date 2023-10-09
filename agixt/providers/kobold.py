803fee8ecddc3623ccee5f0f2ac1b0d215b4718d
Josh XT <josh@devxt.com>
2023-10-09 11:25:29 -0400

    Add prompt prefix and suffix to kobold

--- agixt/providers/kobold.py
+++ agixt/providers/kobold.py
@@ -8,6 +8,8 @@ class KoboldProvider:
         AI_MODEL: str = "default",
         MAX_TOKENS: int = 2048,
         AI_TEMPERATURE: float = 0.7,
+        PROMPT_PREFIX: str = "",
+        PROMPT_SUFFIX: str = "",
         **kwargs,
     ):
         self.requirements = []
@@ -15,8 +17,11 @@ class KoboldProvider:
         self.MAX_TOKENS = MAX_TOKENS if MAX_TOKENS else 2048
         self.AI_TEMPERATURE = AI_TEMPERATURE if AI_TEMPERATURE else 0.7
         self.AI_MODEL = AI_MODEL
+        self.PROMPT_PREFIX = PROMPT_PREFIX if PROMPT_PREFIX else ""
+        self.PROMPT_SUFFIX = PROMPT_SUFFIX if PROMPT_SUFFIX else ""
 
     async def instruct(self, prompt, tokens: int = 0):
+        prompt = f"{self.PROMPT_PREFIX}{prompt}{self.PROMPT_SUFFIX}"
         try:
             max_tokens = int(self.MAX_TOKENS - tokens)
         except:
