bf0b663d9e871fbf76042d3930cff5ee91f07906
Josh XT <josh@devxt.com>
2023-10-20 12:31:57 -0400

    Add audio to user output on tts

--- agixt/extensions/whisper_stt.py
+++ agixt/extensions/whisper_stt.py
@@ -43,7 +43,7 @@ class whisper_stt(Extensions):
     def __init__(self, WHISPER_MODEL="base.en", **kwargs):
         self.commands = {
             "Transcribe Audio from File": self.transcribe_audio_from_file,
-            "Transcribe Base64 Audio": self.transcribe_base64_audio,
+            "Convert M4A to WAV": self.convert_m4a_to_wav,
         }
         # https://huggingface.co/ggerganov/whisper.cpp
         # Models: tiny, tiny.en, base, base.en, small, small.en, medium, medium.en, large, large-v1
@@ -81,14 +81,13 @@ class whisper_stt(Extensions):
         w.transcribe(file_path)
         return w.output()
 
-    async def transcribe_base64_audio(self, base64_audio: str):
+    async def convert_m4a_to_wav(
+        self, base64_audio: str, filename: str = "recording.wav"
+    ):
         # Convert the base64 audio to a 16k WAV format
         audio_data = base64.b64decode(base64_audio)
         audio_segment = AudioSegment.from_file(io.BytesIO(audio_data), format="m4a")
         audio_segment = audio_segment.set_frame_rate(16000)
-        filename = "recording.wav"
         file_path = os.path.join(os.getcwd(), "WORKSPACE", filename)
         audio_segment.export(file_path, format="wav")
-        output = await self.transcribe_audio_from_file(filename)
-        os.remove(file_path)
-        return output
+        return file_path
