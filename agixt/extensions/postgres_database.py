2d30a115549bb0656cd10a44dbc668a8006070e9
Josh XT <josh@devxt.com>
2023-10-25 14:52:56 -0400

    Reduce SQL functions

--- agixt/extensions/postgres_database.py
+++ agixt/extensions/postgres_database.py
@@ -35,99 +35,24 @@ class postgres_database(Extensions):
         self.POSTGRES_DATABASE_PASSWORD = POSTGRES_DATABASE_PASSWORD
 
         self.commands = {
-            "Create Table in Postgres Database": self.create_table,
-            "Insert Row in Postgres Database": self.insert_row,
-            "Select Rows in Postgres Database": self.select_rows,
-            "Update Rows in Postgres Database": self.update_rows,
-            "Delete Rows in Postgres Database": self.delete_rows,
             "Custom SQL Query in Postgres Database": self.execute_sql,
             "Get Database Schema from Postgres Database": self.get_schema,
-            "Get Postgres Data Preview": self.get_data_preview,
         }
         self.ApiClient = kwargs["ApiClient"] if "ApiClient" in kwargs else None
 
-    async def create_table(self, table_name: str, columns: str):
-        logging.info(f"Creating table '{table_name}'")
-        connection = psycopg2.connect(
-            database=self.POSTGRES_DATABASE_NAME,
-            host=self.POSTGRES_DATABASE_HOST,
-            port=self.POSTGRES_DATABASE_PORT,
-            user=self.POSTGRES_DATABASE_USERNAME,
-            password=self.POSTGRES_DATABASE_PASSWORD,
-        )
-        cursor = connection.cursor()
-        cursor.execute(f"CREATE TABLE {table_name} ({columns});")
-        connection.commit()
-        cursor.close()
-        connection.close()
-        return f"Table '{table_name}' created"
-
-    async def insert_row(self, table_name: str, values: str):
-        logging.info(f"Inserting row into table '{table_name}'")
-        connection = psycopg2.connect(
-            database=self.POSTGRES_DATABASE_NAME,
-            host=self.POSTGRES_DATABASE_HOST,
-            port=self.POSTGRES_DATABASE_PORT,
-            user=self.POSTGRES_DATABASE_USERNAME,
-            password=self.POSTGRES_DATABASE_PASSWORD,
-        )
-        cursor = connection.cursor()
-        cursor.execute(f"INSERT INTO {table_name} VALUES ({values});")
-        connection.commit()
-        cursor.close()
-        connection.close()
-        return f"Row inserted into table '{table_name}'"
-
-    async def select_rows(self, table_name: str, columns: str, where: str):
-        logging.info(f"Selecting rows from table '{table_name}'")
-        connection = psycopg2.connect(
-            database=self.POSTGRES_DATABASE_NAME,
-            host=self.POSTGRES_DATABASE_HOST,
-            port=self.POSTGRES_DATABASE_PORT,
-            user=self.POSTGRES_DATABASE_USERNAME,
-            password=self.POSTGRES_DATABASE_PASSWORD,
-        )
-        cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        cursor.execute(f"SELECT {columns} FROM {table_name} WHERE {where};")
-        rows = cursor.fetchall()
-        cursor.close()
-        connection.close()
-        return rows
-
-    async def update_rows(self, table_name: str, set: str, where: str):
-        logging.info(f"Updating rows in table '{table_name}'")
-        connection = psycopg2.connect(
-            database=self.POSTGRES_DATABASE_NAME,
-            host=self.POSTGRES_DATABASE_HOST,
-            port=self.POSTGRES_DATABASE_PORT,
-            user=self.POSTGRES_DATABASE_USERNAME,
-            password=self.POSTGRES_DATABASE_PASSWORD,
-        )
-        cursor = connection.cursor()
-        cursor.execute(f"UPDATE {table_name} SET {set} WHERE {where};")
-        connection.commit()
-        cursor.close()
-        connection.close()
-        return f"Rows updated in table '{table_name}'"
-
-    async def delete_rows(self, table_name: str, where: str):
-        logging.info(f"Deleting rows from table '{table_name}'")
-        connection = psycopg2.connect(
-            database=self.POSTGRES_DATABASE_NAME,
-            host=self.POSTGRES_DATABASE_HOST,
-            port=self.POSTGRES_DATABASE_PORT,
-            user=self.POSTGRES_DATABASE_USERNAME,
-            password=self.POSTGRES_DATABASE_PASSWORD,
-        )
-        cursor = connection.cursor()
-        cursor.execute(f"DELETE FROM {table_name} WHERE {where};")
-        connection.commit()
-        cursor.close()
-        connection.close()
-        return f"Rows deleted from table '{table_name}'"
-
-    async def get_data_preview(self, text: str):
-        return "\n".join(text.split("\n")[:2])
+    def get_connection(self):
+        try:
+            connection = psycopg2.connect(
+                database=self.POSTGRES_DATABASE_NAME,
+                host=self.POSTGRES_DATABASE_HOST,
+                port=self.POSTGRES_DATABASE_PORT,
+                user=self.POSTGRES_DATABASE_USERNAME,
+                password=self.POSTGRES_DATABASE_PASSWORD,
+            )
+            return connection
+        except Exception as e:
+            logging.error(f"Error connecting to Postgres Database. Error: {str(e)}")
+            return None
 
     async def execute_sql(self, query: str):
         if "```sql" in query:
@@ -135,13 +60,7 @@ class postgres_database(Extensions):
         query = query.replace("\n", " ")
         query = query.strip()
         logging.info(f"Executing SQL Query: {query}")
-        connection = psycopg2.connect(
-            database=self.POSTGRES_DATABASE_NAME,
-            host=self.POSTGRES_DATABASE_HOST,
-            port=self.POSTGRES_DATABASE_PORT,
-            user=self.POSTGRES_DATABASE_USERNAME,
-            password=self.POSTGRES_DATABASE_PASSWORD,
-        )
+        connection = self.get_connection()
         cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
         try:
             cursor.execute(query)
@@ -187,13 +106,7 @@ class postgres_database(Extensions):
 
     async def get_schema(self):
         logging.info(f"Getting schema for database '{self.POSTGRES_DATABASE_NAME}'")
-        connection = psycopg2.connect(
-            database=self.POSTGRES_DATABASE_NAME,
-            host=self.POSTGRES_DATABASE_HOST,
-            port=self.POSTGRES_DATABASE_PORT,
-            user=self.POSTGRES_DATABASE_USERNAME,
-            password=self.POSTGRES_DATABASE_PASSWORD,
-        )
+        connection = self.get_connection()
         cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
         cursor.execute(
             f"SELECT schema_name FROM information_schema.schemata WHERE schema_name NOT IN ('pg_catalog', 'information_schema');"
