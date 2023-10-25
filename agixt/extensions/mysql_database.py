291161c114e947133bf7b1d74139a679ec09f4ee
Josh XT <josh@devxt.com>
2023-10-25 14:52:56 -0400

    Reduce SQL functions

--- agixt/extensions/mysql_database.py
+++ agixt/extensions/mysql_database.py
@@ -1,8 +1,6 @@
 import logging
 import mysql.connector
-from mysql.connector import Error
 from Extensions import Extensions
-import re
 
 
 class mysql_database(Extensions):
@@ -20,19 +18,12 @@ class mysql_database(Extensions):
         self.MYSQL_DATABASE_PORT = MYSQL_DATABASE_PORT
         self.MYSQL_DATABASE_USERNAME = MYSQL_DATABASE_USERNAME
         self.MYSQL_DATABASE_PASSWORD = MYSQL_DATABASE_PASSWORD
-
         self.commands = {
-            "Create Table in MySQL Database": self.create_table,
-            "Insert Row in MySQL Database": self.insert_row,
-            "Select Rows in MySQL Database": self.select_rows,
-            "Update Rows in MySQL Database": self.update_rows,
-            "Delete Rows in MySQL Database": self.delete_rows,
             "Custom SQL Query in MySQL Database": self.execute_sql,
             "Get Database Schema from MySQL Database": self.get_schema,
         }
 
-    async def create_table(self, table_name: str, columns: str):
-        logging.info(f"Creating table '{table_name}'")
+    def get_connection(self):
         try:
             connection = mysql.connector.connect(
                 host=self.MYSQL_DATABASE_HOST,
@@ -41,127 +32,62 @@ class mysql_database(Extensions):
                 user=self.MYSQL_DATABASE_USERNAME,
                 password=self.MYSQL_DATABASE_PASSWORD,
             )
-            cursor = connection.cursor()
-            cursor.execute(f"CREATE TABLE {table_name} ({columns});")
-            connection.commit()
-            cursor.close()
-            connection.close()
-            return f"Table '{table_name}' created"
-        except Error as e:
-            logging.error(f"Error creating table: {e}")
-            return "Error creating table"
-
-    async def insert_row(self, table_name: str, values: str):
-        logging.info(f"Inserting row into table '{table_name}'")
-        try:
-            connection = mysql.connector.connect(
-                host=self.MYSQL_DATABASE_HOST,
-                port=self.MYSQL_DATABASE_PORT,
-                database=self.MYSQL_DATABASE_NAME,
-                user=self.MYSQL_DATABASE_USERNAME,
-                password=self.MYSQL_DATABASE_PASSWORD,
-            )
-            cursor = connection.cursor()
-            cursor.execute(f"INSERT INTO {table_name} VALUES ({values});")
-            connection.commit()
-            cursor.close()
-            connection.close()
-            return f"Row inserted into table '{table_name}'"
-        except Error as e:
-            logging.error(f"Error inserting row: {e}")
-            return "Error inserting row"
-
-    async def select_rows(self, table_name: str, columns: str, where: str):
-        logging.info(f"Selecting rows from table '{table_name}'")
-        try:
-            connection = mysql.connector.connect(
-                host=self.MYSQL_DATABASE_HOST,
-                port=self.MYSQL_DATABASE_PORT,
-                database=self.MYSQL_DATABASE_NAME,
-                user=self.MYSQL_DATABASE_USERNAME,
-                password=self.MYSQL_DATABASE_PASSWORD,
-            )
-            cursor = connection.cursor(dictionary=True)
-            cursor.execute(f"SELECT {columns} FROM {table_name} WHERE {where};")
-            rows = cursor.fetchall()
-            cursor.close()
-            connection.close()
-            return rows
-        except Error as e:
-            logging.error(f"Error selecting rows: {e}")
-            return []
-
-    async def update_rows(self, table_name: str, set_clause: str, where: str):
-        logging.info(f"Updating rows in table '{table_name}'")
-        try:
-            connection = mysql.connector.connect(
-                host=self.MYSQL_DATABASE_HOST,
-                port=self.MYSQL_DATABASE_PORT,
-                database=self.MYSQL_DATABASE_NAME,
-                user=self.MYSQL_DATABASE_USERNAME,
-                password=self.MYSQL_DATABASE_PASSWORD,
-            )
-            cursor = connection.cursor()
-            cursor.execute(f"UPDATE {table_name} SET {set_clause} WHERE {where};")
-            connection.commit()
-            cursor.close()
-            connection.close()
-            return f"Rows updated in table '{table_name}'"
-        except Error as e:
-            logging.error(f"Error updating rows: {e}")
-            return "Error updating rows"
-
-    async def delete_rows(self, table_name: str, where: str):
-        logging.info(f"Deleting rows from table '{table_name}'")
-        try:
-            connection = mysql.connector.connect(
-                host=self.MYSQL_DATABASE_HOST,
-                port=self.MYSQL_DATABASE_PORT,
-                database=self.MYSQL_DATABASE_NAME,
-                user=self.MYSQL_DATABASE_USERNAME,
-                password=self.MYSQL_DATABASE_PASSWORD,
-            )
-            cursor = connection.cursor()
-            cursor.execute(f"DELETE FROM {table_name} WHERE {where};")
-            connection.commit()
-            cursor.close()
-            connection.close()
-            return f"Rows deleted from table '{table_name}'"
-        except Error as e:
-            logging.error(f"Error deleting rows: {e}")
-            return "Error deleting rows"
+            return connection
+        except Exception as e:
+            logging.error(f"Error connecting to MySQL Database. Error: {str(e)}")
+            return None
 
     async def execute_sql(self, query: str):
         if "```sql" in query:
             query = query.split("```sql")[1].split("```")[0]
         logging.info(f"Executing SQL Query: {query}")
+        connection = self.get_connection()
+        cursor = connection.cursor()
         try:
-            connection = mysql.connector.connect(
-                host=self.MYSQL_DATABASE_HOST,
-                port=self.MYSQL_DATABASE_PORT,
-                database=self.MYSQL_DATABASE_NAME,
-                user=self.MYSQL_DATABASE_USERNAME,
-                password=self.MYSQL_DATABASE_PASSWORD,
-            )
-            cursor = connection.cursor(dictionary=True)
             cursor.execute(query)
             rows = cursor.fetchall()
             cursor.close()
             connection.close()
-            return rows
-        except Error as e:
-            logging.error(f"Error executing SQL query: {e}")
-            return []
+            rows_string = ""
+            # If there is only 1 row and 1 column, return the value as a string
+            if len(rows) == 1 and len(rows[0]) == 1:
+                return str(rows[0][0])
+            # If there is more than 1 column and at least 1 row, return it as a CSV format
+            if len(rows) >= 1 and len(rows[0]) > 1:
+                # If there is more than 1 column and at least 1 row, return it as a CSV format, build column heading, and make sure each row value is quoted
+                column_headings = []
+                for column in cursor.description:
+                    column_headings.append(column.name)
+                rows_string += ",".join(column_headings) + "\n"
+                for row in rows:
+                    row_string = []
+                    for value in row:
+                        row_string.append(f'"{value}"')
+                    rows_string += ",".join(row_string) + "\n"
+                return rows_string
+            # If there is only 1 column and more than 1 row, return it as a CSV format
+            if len(rows) > 1 and len(rows[0]) == 1:
+                for row in rows:
+                    rows_string += f'"{row[0]}"\n'
+                return rows_string
+            return rows_string
+        except Exception as e:
+            logging.error(f"Error executing SQL Query: {str(e)}")
+            # Reformat the query if it is invalid.
+            new_query = self.ApiClient.prompt_agent(
+                agent_name=self.agent_name,
+                prompt_name="Validate SQL",
+                prompt_args={
+                    "database_type": "Postgres",
+                    "schema": await self.get_schema(),
+                    "query": query,
+                },
+            )
+            return await self.execute_sql(query=new_query)
 
     async def get_schema(self):
         logging.info(f"Getting schema for database '{self.MYSQL_DATABASE_NAME}'")
-        connection = mysql.connector.connect(
-            host=self.MYSQL_DATABASE_HOST,
-            port=self.MYSQL_DATABASE_PORT,
-            database=self.MYSQL_DATABASE_NAME,
-            user=self.MYSQL_DATABASE_USERNAME,
-            password=self.MYSQL_DATABASE_PASSWORD,
-        )
+        connection = self.get_connection()
         cursor = connection.cursor()
         cursor.execute("SHOW TABLES")
         tables = cursor.fetchall()
@@ -178,4 +104,5 @@ class mysql_database(Extensions):
                 new_columns += f"`{column[0]}` {column_type}, "
             new_columns = new_columns[:-2]
             schema += f"CREATE TABLE {table[0]} ({new_columns});\n"
+        connection.close()
         return schema
