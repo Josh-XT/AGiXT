46bf29d85c5e209f0f6a77fd3be4c1a88623c14d
Josh XT <josh@devxt.com>
2023-11-04 16:03:06 -0400

    Remove set -e

--- agixt/launch-backend.sh
+++ agixt/launch-backend.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-set -e
-workers="${UVICORN_WORKERS:-10}"
 if [ "$DB_CONNECTED" = "true" ]; then
   python3 DBConnection.py
 fi
 echo "Starting AGiXT..."
+workers="${UVICORN_WORKERS:-10}"
 uvicorn app:app --host 0.0.0.0 --port 7437 --workers $workers --proxy-headers
