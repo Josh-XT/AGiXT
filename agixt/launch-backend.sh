b17e0c3383d5b109d4c05ba1dfc9a3d02b7f229c
Josh XT <josh@devxt.com>
2023-11-06 08:37:33 -0500

    Add sleep

--- agixt/launch-backend.sh
+++ agixt/launch-backend.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
-python3 DBConnection.py
 echo "Starting AGiXT..."
+sleep 5
+python3 DBConnection.py
 workers="${UVICORN_WORKERS:-10}"
 uvicorn app:app --host 0.0.0.0 --port 7437 --workers $workers --proxy-headers
