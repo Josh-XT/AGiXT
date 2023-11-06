0beae7fc4adbef060b18b909de5496162f0f1ea0
Josh XT <josh@devxt.com>
2023-11-06 08:34:44 -0500

    Run DBConnection for logic

--- agixt/launch-backend.sh
+++ agixt/launch-backend.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e
-if [ "$DB_CONNECTED" = "true" ]; then
-    echo "Connecting to database..."
-    python3 DBConnection.py
-fi
+python3 DBConnection.py
 echo "Starting AGiXT..."
 workers="${UVICORN_WORKERS:-10}"
 uvicorn app:app --host 0.0.0.0 --port 7437 --workers $workers --proxy-headers
