59b8096e927ee92c9b5d2a8c32f8395f87912a1a
Josh XT <josh@devxt.com>
2023-11-21 11:43:28 -0500

    Fix command injection

--- agixt/Interactions.py
+++ agixt/Interactions.py
@@ -269,7 +269,7 @@ class Interactions:
             command for command in agent_commands if agent_commands[command] == True
         ]
         for command in available_commands:
-            verbose_commands += f'    "{command["friendly_name"]}": {{\n'
+            verbose_commands += f'    "{command}": {{\n'
             for arg in command["args"]:
                 verbose_commands += f'        "{arg}": "Your hallucinated input",\n'
             verbose_commands += "    },\n"
