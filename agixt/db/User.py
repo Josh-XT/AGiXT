84bde21b98797f756d26c4ea3bf90427e9b6724d
Josh XT <josh@devxt.com>
2023-12-05 11:47:32 -0500

    Fix user creation

--- agixt/db/User.py
+++ agixt/db/User.py
@@ -1,10 +1,13 @@
-from DBConnection import User
+from DBConnection import User, get_session
 import os
 
 
-def create_user(api_key: str, email: str, role: str) -> bool:
+def create_user(api_key: str, email: str, role: str = "user") -> bool:
     if api_key != os.environ.get("AGIXT_API_KEY"):
         return {"error": "Invalid API key"}, 401
-    user = User(email=email, role=role)
-    user.save()
+    session = get_session()
+    user = User(email=email, role=role.lower())
+    session.add(user)
+    session.commit()
+    session.close()
     return {"status": "Success"}, 200
