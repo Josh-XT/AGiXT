49aac27bf077e7757f028c9d5ecd19ba3253518e
Josh XT <josh@devxt.com>
2023-12-23 15:18:10 -0500

    Switch to cpu inference on failure

--- agixt/providers/vllm.py
+++ agixt/providers/vllm.py
@@ -2,6 +2,7 @@ import time
 import logging
 import random
 from providers.gpt4free import Gpt4freeProvider
+from providers.local import LocalProvider
 
 try:
     import openai
@@ -74,9 +75,21 @@ class VllmProvider:
             logging.info(f"vLLM API Error: {e}")
             if "," in self.API_URI:
                 self.rotate_uri()
-            if self.failure_count >= 3:
-                logging.info("vLLM failed 3 times, switching to GPT4Free")
-                return await Gpt4freeProvider().inference(prompt=prompt, tokens=tokens)
+            if self.failure_count >= 2:
+                logging.info(
+                    "vLLM failed 2 times, switching to Local-LLM for CPU inference"
+                )
+                try:
+                    model = self.AI_MODEL.split("/")[1]
+                    return await LocalProvider(AI_MODEL=model).inference(
+                        prompt=prompt, tokens=tokens
+                    )
+                except Exception as e:
+                    logging.info(f"Local-LLM API Error: {e}")
+                    logging.info("Local-LLM failed, switching to GPT4Free.")
+                    return await Gpt4freeProvider().inference(
+                        prompt=prompt, tokens=tokens
+                    )
             if int(self.WAIT_AFTER_FAILURE) > 0:
                 time.sleep(int(self.WAIT_AFTER_FAILURE))
                 return await self.inference(prompt=prompt, tokens=tokens)
