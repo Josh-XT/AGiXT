cd0c1662d97e4413ac88ddf74ad0d50ae0178d3a
Josh XT <josh@devxt.com>
2023-12-23 20:43:42 -0500

    Lint

--- agixt/providers/kobold.py
+++ agixt/providers/kobold.py
@@ -1,6 +1,7 @@
 import requests
 from typing import Dict, List, Optional
 
+
 class KoboldProvider:
     def __init__(
         self,
@@ -41,7 +42,9 @@ class KoboldProvider:
         **kwargs,
     ):
         self.requirements = []
-        self.AI_PROVIDER_URI = AI_PROVIDER_URI if AI_PROVIDER_URI else "http://host.docker.internal:5001"
+        self.AI_PROVIDER_URI = (
+            AI_PROVIDER_URI if AI_PROVIDER_URI else "http://host.docker.internal:5001"
+        )
         self.AI_MODEL = AI_MODEL if AI_MODEL else "default"
         self.PROMPT_PREFIX = PROMPT_PREFIX if PROMPT_PREFIX else ""
         self.PROMPT_SUFFIX = PROMPT_SUFFIX if PROMPT_SUFFIX else ""
@@ -53,7 +56,9 @@ class KoboldProvider:
         self.REP_PEN_SLOPE = REP_PEN_SLOPE if REP_PEN_SLOPE else None
         self.SAMPLER_ORDER = SAMPLER_ORDER if SAMPLER_ORDER else None
         self.SAMPLER_SEED = SAMPLER_SEED if SAMPLER_SEED else None
-        self.SAMPLER_FULL_DETERMINISM = SAMPLER_FULL_DETERMINISM if SAMPLER_FULL_DETERMINISM else False
+        self.SAMPLER_FULL_DETERMINISM = (
+            SAMPLER_FULL_DETERMINISM if SAMPLER_FULL_DETERMINISM else False
+        )
         self.STOP_SEQUENCE = STOP_SEQUENCE if STOP_SEQUENCE else None
         self.TEMPERATURE = TEMPERATURE if TEMPERATURE else 0.7
         self.TFS = TFS if TFS else 1
@@ -62,14 +67,22 @@ class KoboldProvider:
         self.TOP_P = TOP_P if TOP_P else 0.92
         self.MIN_P = MIN_P if MIN_P else 0
         self.TYPICAL = TYPICAL if TYPICAL else 1
-        self.USE_DEFAULT_BADWORDSIDS = USE_DEFAULT_BADWORDSIDS if USE_DEFAULT_BADWORDSIDS else False
+        self.USE_DEFAULT_BADWORDSIDS = (
+            USE_DEFAULT_BADWORDSIDS if USE_DEFAULT_BADWORDSIDS else False
+        )
         self.MIROSTAT = MIROSTAT if MIROSTAT else 0
         self.MIROSTAT_TAU = MIROSTAT_TAU if MIROSTAT_TAU else 5.0
         self.MIROSTAT_ETA = MIROSTAT_ETA if MIROSTAT_ETA else 0.1
         self.GRAMMAR = GRAMMAR if GRAMMAR else ""
-        self.GRAMMAR_RETAIN_STATE = GRAMMAR_RETAIN_STATE if GRAMMAR_RETAIN_STATE else False
-        self.DISABLE_INPUT_FORMATTING = DISABLE_INPUT_FORMATTING if DISABLE_INPUT_FORMATTING else True
-        self.DISABLE_OUTPUT_FORMATTING = DISABLE_OUTPUT_FORMATTING if DISABLE_OUTPUT_FORMATTING else True
+        self.GRAMMAR_RETAIN_STATE = (
+            GRAMMAR_RETAIN_STATE if GRAMMAR_RETAIN_STATE else False
+        )
+        self.DISABLE_INPUT_FORMATTING = (
+            DISABLE_INPUT_FORMATTING if DISABLE_INPUT_FORMATTING else True
+        )
+        self.DISABLE_OUTPUT_FORMATTING = (
+            DISABLE_OUTPUT_FORMATTING if DISABLE_OUTPUT_FORMATTING else True
+        )
         self.FRMTADSNSP = FRMTADSNSP if FRMTADSNSP else False
         self.FRMTRMBLLN = FRMTRMBLLN if FRMTRMBLLN else False
         self.FRMTRMSPCH = FRMTRMSPCH if FRMTRMSPCH else False
@@ -77,14 +90,15 @@ class KoboldProvider:
         self.SINGLELINE = SINGLELINE if SINGLELINE else False
 
     async def inference(self, prompt, tokens: int = 0):
-
         # Determine which endpoints to use
         def is_koboldcpp() -> bool:
-            response = requests.get(f"{self.AI_PROVIDER_URI}/api/extra/version").json()["result"]
+            response = requests.get(f"{self.AI_PROVIDER_URI}/api/extra/version").json()[
+                "result"
+            ]
             if response == "KoboldCpp":
                 return True
             return False
-        
+
         koboldai_endpoint: Dict[str, str] = {
             "ctx_length": "/api/v1/config/max_context_length",
         }
@@ -108,7 +122,7 @@ class KoboldProvider:
             if tokens < true_max_ctx_length:
                 return true_max_ctx_length - tokens
             return 0
-            
+
         params = {
             "prompt": prompt,
             "max_context_length": int(true_max_ctx_length),
@@ -126,7 +140,7 @@ class KoboldProvider:
         # Optional parameters
         if self.MAX_CONTEXT_LENGTH:
             params["max_context_length"] = int(self.MAX_CONTEXT_LENGTH)
-        
+
         if self.MAX_LENGTH:
             params["max_length"] = int(self.MAX_LENGTH)
 
@@ -134,6 +148,7 @@ class KoboldProvider:
             params["quiet"] = self.QUIET
 
         if self.SAMPLER_ORDER:
+
             def is_valid_sample_order(value: List[int]) -> bool:
                 """Check if sample order is a list of integers ranging from 0 to (6 or 7)"""
                 if not isinstance(value, list):
@@ -142,7 +157,7 @@ class KoboldProvider:
                     if not isinstance(num, int):
                         return False
                 return set(value) == set(range(6)) or set(value) == set(range(7))
-        
+
             if is_valid_sample_order(self.SAMPLER_ORDER):
                 params["sampler_order"] = self.SAMPLER_ORDER
 
@@ -151,9 +166,9 @@ class KoboldProvider:
 
         if self.USE_DEFAULT_BADWORDSIDS:
             params["use_default_badwordsids"] = self.USE_DEFAULT_BADWORDSIDS
-        
+
         if self.STOP_SEQUENCE:
-                params["stop_sequence"] = self.STOP_SEQUENCE.split(",")
+            params["stop_sequence"] = self.STOP_SEQUENCE.split(",")
         else:
             self.STOP_SEQUENCE = []
             if self.PROMPT_PREFIX:
@@ -187,7 +202,7 @@ class KoboldProvider:
                 params["disable_input_formatting"] = self.DISABLE_INPUT_FORMATTING
                 if self.FRMTADSNSP:
                     params["frmtadsnsp"] = self.FRMTADSNSP
-            
+
             if not self.DISABLE_OUTPUT_FORMATTING:
                 params["disable_output_formatting"] = self.DISABLE_OUTPUT_FORMATTING
                 if self.FRMTRMBLLN:
@@ -196,8 +211,7 @@ class KoboldProvider:
                     params["frmtrmspch"] = self.FRMTRMSPCH
                 if self.FRMTTRIMINC:
                     params["frmttriminc"] = self.FRMTTRIMINC
-                
-        
+
         # Fetch response
         response = requests.post(f"{self.AI_PROVIDER_URI}/api/v1/generate", json=params)
         json_response = response.json()
