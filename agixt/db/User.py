d0babe1b73b7e6177b0f52acd6cbf16fd219606c
Josh XT <josh@devxt.com>
2023-12-29 14:43:42 -0500

    Force lower username

--- agixt/db/User.py
+++ agixt/db/User.py
@@ -6,7 +6,7 @@ def create_user(api_key: str, email: str, role: str = "user") -> bool:
     if api_key != os.environ.get("AGIXT_API_KEY"):
         return {"error": "Invalid API key"}, 401
     session = get_session()
-    user = User(email=email, role=role.lower())
+    user = User(email=email.lower(), role=role.lower())
     session.add(user)
     session.commit()
     session.close()
