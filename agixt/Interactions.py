d0a5290950d2f62f310e780da85ad520d693bd8b
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2024-01-06 11:28:28 -0500

    Add audio transcription by boolean (#1082)
    * Add audio transcription by bool
    
    * Clean up

--- agixt/Interactions.py
+++ agixt/Interactions.py
@@ -389,8 +389,26 @@ class Interactions:
         browse_links: bool = False,
         prompt_category: str = "Default",
         persist_context_in_history: bool = False,
+        is_m4a_audio: bool = False,
+        is_wav_audio: bool = False,
         **kwargs,
     ):
+        if is_m4a_audio or is_wav_audio:
+            # Convert the audio to text
+            ext = Extensions(
+                agent_name=self.agent_name,
+                agent_config=self.agent.AGENT_CONFIG,
+                conversation_name=conversation_name,
+                ApiClient=self.ApiClient,
+                user=self.user,
+            )
+            command_output = await ext.execute_command(
+                command_name="Transcribe M4A Audio"
+                if is_m4a_audio
+                else "Transcribe WAV Audio",
+                command_args={"base64_audio": user_input},
+            )
+            user_input = command_output
         shots = int(shots)
         if "prompt_category" in kwargs:
             prompt_category = kwargs["prompt_category"]
