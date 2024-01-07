0836c7f10c5cdd42c4286bf9081d617e1047c76f
Josh XT <josh@devxt.com>
2024-01-06 20:03:02 -0500

    Update Local-LLM to 0.0.39

--- agixt/providers/local.py
+++ agixt/providers/local.py
@@ -9,27 +9,30 @@ class LocalProvider:
         AI_MODEL: str = "Mistral-7B-OpenOrca",
         AI_TEMPERATURE: float = 0.7,
         AI_TOP_P: float = 0.95,
+        PROMPT_PREFIX: str = "",
+        PROMPT_SUFFIX: str = "",
         **kwargs,
     ):
         self.requirements = ["local-llm"]
         self.AI_MODEL = AI_MODEL if AI_MODEL else "Mistral-7B-OpenOrca"
         self.AI_TEMPERATURE = AI_TEMPERATURE if AI_TEMPERATURE else 0.7
         self.AI_TOP_P = AI_TOP_P if AI_TOP_P else 0.95
+        self.PROMPT_PREFIX = PROMPT_PREFIX if PROMPT_PREFIX else ""
+        self.PROMPT_SUFFIX = PROMPT_SUFFIX if PROMPT_SUFFIX else ""
 
     def models(self):
         return LLM().models()
 
     async def inference(self, prompt, tokens: int = 0):
         models_dir = os.path.join(os.getcwd(), "models")
+        prompt = f"{self.PROMPT_PREFIX}{prompt}{self.PROMPT_SUFFIX}"
         try:
             response = LLM(
                 models_dir=models_dir,
                 model=self.AI_MODEL,
                 temperature=float(self.AI_TEMPERATURE),
                 top_p=float(self.AI_TOP_P),
-            ).completion(
-                prompt=prompt,
-            )
+            ).completion(prompt=prompt, format_prompt=False)
             return response["choices"][0]["text"]
         except Exception as e:
             logging.error(f"Local-LLM Error: {e}")
