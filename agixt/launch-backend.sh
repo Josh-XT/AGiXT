1cbe79e414142f483dcf014dfa51bc88c485ed3c
Josh XT <josh@devxt.com>
2024-02-27 09:04:00 -0500

    Hack away broken providers

--- agixt/launch-backend.sh
+++ agixt/launch-backend.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 echo "Starting AGiXT..."
 sed -i 's/GptGo,//g' /usr/local/lib/python3.10/site-packages/g4f/models.py
+sed -i 's/GptForLove,//g' /usr/local/lib/python3.10/site-packages/g4f/models.py
+sed -i 's/Chatgpt4Online,//g' /usr/local/lib/python3.10/site-packages/g4f/models.py
+sed -i 's/ChatBase,//g' /usr/local/lib/python3.10/site-packages/g4f/models.py
+
 if [ "$DB_CONNECTED" = "true" ]; then
     sleep 5
     echo "Connecting to DB..."
