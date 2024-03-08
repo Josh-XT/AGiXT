da5f4491898c57ac3a88388614a22aa44cc89c29
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2024-03-08 04:32:34 -0500

    Add ngrok (#1136)
    * Add ngrok
    
    * Add error handling
    
    * Add env var to docs
    
    * Clean up

--- agixt/launch-backend.sh
+++ agixt/launch-backend.sh
@@ -5,5 +5,9 @@ if [ "$DB_CONNECTED" = "true" ]; then
     echo "Connecting to DB..."
     python3 DBConnection.py
 fi
+if [ -n "$NGROK_TOKEN" ]; then
+    echo "Starting ngrok..."
+    python3 Tunnel.py
+fi
 workers="${UVICORN_WORKERS:-10}"
 uvicorn app:app --host 0.0.0.0 --port 7437 --workers $workers --proxy-headers
