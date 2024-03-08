686774c6b08f89b98cfc38044772bc637276424b
Josh XT <josh@devxt.com>
2024-03-08 04:09:42 -0500

    Add error handling

--- agixt/Tunnel.py
+++ agixt/Tunnel.py
@@ -5,10 +5,14 @@ NGROK_TOKEN = os.environ.get("NGROK_TOKEN", "")
 if NGROK_TOKEN:
     from pyngrok import ngrok
 
-    ngrok.set_auth_token(NGROK_TOKEN)
-    public_url = ngrok.connect(8091)
-    logging.info(f"[ngrok] Public Tunnel: {public_url.public_url}")
-    ngrok_url = public_url.public_url
+    try:
+        ngrok.set_auth_token(NGROK_TOKEN)
+        public_url = ngrok.connect(7437)
+        logging.info(f"[ngrok] Public Tunnel: {public_url.public_url}")
+        ngrok_url = public_url.public_url
+    except Exception as e:
+        logging.error(f"[ngrok] Error: {e}")
+        ngrok_url = ""
 
     def get_ngrok_url():
         global ngrok_url
@@ -17,7 +21,7 @@ if NGROK_TOKEN:
 else:
 
     def get_ngrok_url():
-        return "http://localhost:8091"
+        return "http://localhost:7437"
 
 
 if __name__ == "__main__":
