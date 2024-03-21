884140c205121d9d724f020e90cbec6b7f1edd09
Josh XT <josh@devxt.com>
2024-03-21 07:59:23 -0400

    Add faster whisper and chat completions pipeline

--- agixt/endpoints/Completions.py
+++ agixt/endpoints/Completions.py
@@ -1,10 +1,17 @@
 import string
 import random
 import time
+import requests
+import base64
+import PIL
+import uuid
 from fastapi import APIRouter, Depends, Header
 from Interactions import Interactions, get_tokens
 from Embedding import Embedding
 from ApiClient import Agent, verify_api_key, get_api_client
+from readers.file import FileReader
+from readers.website import WebsiteReader
+from AudioToText import AudioToText
 from Models import (
     Completions,
     ChatCompletions,
@@ -15,7 +22,7 @@ app = APIRouter()
 
 
 @app.post(
-    "/api/v1/completions",
+    "/v1/completions",
     tags=["OpenAI Style Endpoints"],
     dependencies=[Depends(verify_api_key)],
 )
@@ -67,7 +74,7 @@ async def completion(
 
 
 @app.post(
-    "/api/v1/chat/completions",
+    "/v1/chat/completions",
     tags=["OpenAI Style Endpoints"],
     dependencies=[Depends(verify_api_key)],
 )
@@ -87,11 +94,96 @@ async def chat_completion(
             model = "undefined"
     else:
         model = "undefined"
+    images = []
+    pil_images = []
+    new_prompt = ""
+    for message in prompt.messages:
+        if isinstance(message["content"], str):
+            role = message["role"] if "role" in message else "User"
+            if role.lower() == "user":
+                new_prompt += f"{message['content']}\n\n"
+            if role.lower() == "system":
+                new_prompt = f"System: {message['content']}\n\nUser: {new_prompt}"
+        if isinstance(message["content"], list):
+            for msg in message["content"]:
+                if "text" in msg:
+                    role = message["role"] if "role" in message else "User"
+                    if role.lower() == "user":
+                        new_prompt += f"{msg['text']}\n\n"
+                if "image_url" in msg:
+                    url = (
+                        msg["image_url"]["url"]
+                        if "url" in msg["image_url"]
+                        else msg["image_url"]
+                    )
+                    image_path = f"./WORKSPACE/{uuid.uuid4().hex}.jpg"
+                    if url.startswith("http"):
+                        image = requests.get(url).content
+                    else:
+                        file_type = url.split(",")[0].split("/")[1].split(";")[0]
+                        if file_type == "jpeg":
+                            file_type = "jpg"
+                        image_path = f"./WORKSPACE/{uuid.uuid4().hex}.{file_type}"
+                        image = base64.b64decode(url.split(",")[1])
+                    with open(image_path, "wb") as f:
+                        f.write(image)
+                    images.append(image_path)
+                    pil_img = PIL.Image.open(image_path)
+                    pil_img = pil_img.convert("RGB")
+                    pil_images.append(pil_img)
+                if "audio_url" in message:
+                    audio_url = (
+                        message["audio_url"]["url"]
+                        if "url" in message["audio_url"]
+                        else message["audio_url"]
+                    )
+                    transcribed_audio = AudioToText().transcribe_audio(
+                        file=audio_url, prompt=new_prompt
+                    )
+                    new_prompt += transcribed_audio
+                if "file_url" in message:
+                    file_url = (
+                        message["file_url"]["url"]
+                        if "url" in message["file_url"]
+                        else message["file_url"]
+                    )
+                    if file_url.startswith("http"):
+                        file_data = requests.get(file_url).content
+                    else:
+                        file_type = file_url.split(",")[0].split("/")[1].split(";")[0]
+                        file_data = base64.b64decode(file_url.split(",")[1])
+                    file_path = f"./WORKSPACE/{uuid.uuid4().hex}.{file_type}"
+                    with open(file_path, "wb") as f:
+                        f.write(file_data)
+                    file_reader = FileReader(
+                        agent_name=prompt.model,
+                        agent_config=agent_config,
+                        collection_number=0,
+                        ApiClient=ApiClient,
+                        user=user,
+                    )
+                    await file_reader.write_file_to_memory(file_path)
+                if "url" in message:
+                    url = (
+                        message["url"]["url"]
+                        if "url" in message["url"]
+                        else message["url"]
+                    )
+                    if url.startswith("http"):
+                        website_reader = WebsiteReader(
+                            agent_name=prompt.model,
+                            agent_config=agent_config,
+                            collection_number=0,
+                            ApiClient=ApiClient,
+                            user=user,
+                        )
+                        await website_reader.write_website_to_memory(url)
     response = await agent.run(
-        user_input=prompt.prompt,
+        user_input=new_prompt,
         prompt="Custom Input",
         context_results=3,
         shots=prompt.n,
+        images=images,
     )
     characters = string.ascii_letters + string.digits
     prompt_tokens = get_tokens(prompt.prompt)
@@ -126,7 +218,7 @@ async def chat_completion(
 
 # Use agent name in the model field to use embedding.
 @app.post(
-    "/api/v1/embedding",
+    "/v1/embedding",
     tags=["OpenAI Style Endpoints"],
     dependencies=[Depends(verify_api_key)],
 )
