f7fd0b4c9acbbead46ef3caf6dd7bd3603751786
Josh XT <josh@devxt.com>
2024-03-31 20:23:34 -0400

    Clean up

--- agixt/providers/default.py
+++ agixt/providers/default.py
@@ -2,14 +2,18 @@ from providers.gpt4free import Gpt4freeProvider
 from providers.huggingface import HuggingfaceProvider
 from chromadb.utils.embedding_functions import ONNXMiniLM_L6_V2
 from faster_whisper import WhisperModel
-from pydub import AudioSegment
 import os
-import io
 import logging
 import numpy as np
-import ffmpeg
-import uuid
-import base64
+import requests
+import random
+
+# Default provider uses:
+# llm: gpt4free
+# tts: Streamlabs TTS
+# transcription: faster-whisper
+# translation: faster-whisper
+# image: huggingface (Requires API Key)
 
 
 class DefaultProvider:
@@ -33,6 +37,218 @@ class DefaultProvider:
         self.embedder = ONNXMiniLM_L6_V2()
         self.embedder.DOWNLOAD_PATH = os.getcwd()
         self.chunk_size = 256
+        STREAMLABS_VOICES = [
+            "Filiz",
+            "Astrid",
+            "Tatyana",
+            "Maxim",
+            "Carmen",
+            "Ines",
+            "Cristiano",
+            "Vitoria",
+            "Ricardo",
+            "Maja",
+            "Jan",
+            "Jacek",
+            "Ewa",
+            "Ruben",
+            "Lotte",
+            "Liv",
+            "Seoyeon",
+            "Takumi",
+            "Mizuki",
+            "Giorgio",
+            "Carla",
+            "Bianca",
+            "Karl",
+            "Dora",
+            "Mathieu",
+            "Celine",
+            "Chantal",
+            "Penelope",
+            "Miguel",
+            "Mia",
+            "Enrique",
+            "Conchita",
+            "Geraint",
+            "Salli",
+            "Matthew",
+            "Kimberly",
+            "Kendra",
+            "Justin",
+            "Joey",
+            "Joanna",
+            "Ivy",
+            "Raveena",
+            "Aditi",
+            "Emma",
+            "Brian",
+            "Amy",
+            "Russell",
+            "Nicole",
+            "Vicki",
+            "Marlene",
+            "Hans",
+            "Naja",
+            "Mads",
+            "Gwyneth",
+            "Zhiyu",
+            "es-ES-Standard-A",
+            "it-IT-Standard-A",
+            "it-IT-Wavenet-A",
+            "ja-JP-Standard-A",
+            "ja-JP-Wavenet-A",
+            "ko-KR-Standard-A",
+            "ko-KR-Wavenet-A",
+            "pt-BR-Standard-A",
+            "tr-TR-Standard-A",
+            "sv-SE-Standard-A",
+            "nl-NL-Standard-A",
+            "nl-NL-Wavenet-A",
+            "en-US-Wavenet-A",
+            "en-US-Wavenet-B",
+            "en-US-Wavenet-C",
+            "en-US-Wavenet-D",
+            "en-US-Wavenet-E",
+            "en-US-Wavenet-F",
+            "en-GB-Standard-A",
+            "en-GB-Standard-B",
+            "en-GB-Standard-C",
+            "en-GB-Standard-D",
+            "en-GB-Wavenet-A",
+            "en-GB-Wavenet-B",
+            "en-GB-Wavenet-C",
+            "en-GB-Wavenet-D",
+            "en-US-Standard-B",
+            "en-US-Standard-C",
+            "en-US-Standard-D",
+            "en-US-Standard-E",
+            "de-DE-Standard-A",
+            "de-DE-Standard-B",
+            "de-DE-Wavenet-A",
+            "de-DE-Wavenet-B",
+            "de-DE-Wavenet-C",
+            "de-DE-Wavenet-D",
+            "en-AU-Standard-A",
+            "en-AU-Standard-B",
+            "en-AU-Wavenet-A",
+            "en-AU-Wavenet-B",
+            "en-AU-Wavenet-C",
+            "en-AU-Wavenet-D",
+            "en-AU-Standard-C",
+            "en-AU-Standard-D",
+            "fr-CA-Standard-A",
+            "fr-CA-Standard-B",
+            "fr-CA-Standard-C",
+            "fr-CA-Standard-D",
+            "fr-FR-Standard-C",
+            "fr-FR-Standard-D",
+            "fr-FR-Wavenet-A",
+            "fr-FR-Wavenet-B",
+            "fr-FR-Wavenet-C",
+            "fr-FR-Wavenet-D",
+            "da-DK-Wavenet-A",
+            "pl-PL-Wavenet-A",
+            "pl-PL-Wavenet-B",
+            "pl-PL-Wavenet-C",
+            "pl-PL-Wavenet-D",
+            "pt-PT-Wavenet-A",
+            "pt-PT-Wavenet-B",
+            "pt-PT-Wavenet-C",
+            "pt-PT-Wavenet-D",
+            "ru-RU-Wavenet-A",
+            "ru-RU-Wavenet-B",
+            "ru-RU-Wavenet-C",
+            "ru-RU-Wavenet-D",
+            "sk-SK-Wavenet-A",
+            "tr-TR-Wavenet-A",
+            "tr-TR-Wavenet-B",
+            "tr-TR-Wavenet-C",
+            "tr-TR-Wavenet-D",
+            "tr-TR-Wavenet-E",
+            "uk-UA-Wavenet-A",
+            "ar-XA-Wavenet-A",
+            "ar-XA-Wavenet-B",
+            "ar-XA-Wavenet-C",
+            "cs-CZ-Wavenet-A",
+            "nl-NL-Wavenet-B",
+            "nl-NL-Wavenet-C",
+            "nl-NL-Wavenet-D",
+            "nl-NL-Wavenet-E",
+            "en-IN-Wavenet-A",
+            "en-IN-Wavenet-B",
+            "en-IN-Wavenet-C",
+            "fil-PH-Wavenet-A",
+            "fi-FI-Wavenet-A",
+            "el-GR-Wavenet-A",
+            "hi-IN-Wavenet-A",
+            "hi-IN-Wavenet-B",
+            "hi-IN-Wavenet-C",
+            "hu-HU-Wavenet-A",
+            "id-ID-Wavenet-A",
+            "id-ID-Wavenet-B",
+            "id-ID-Wavenet-C",
+            "it-IT-Wavenet-B",
+            "it-IT-Wavenet-C",
+            "it-IT-Wavenet-D",
+            "ja-JP-Wavenet-B",
+            "ja-JP-Wavenet-C",
+            "ja-JP-Wavenet-D",
+            "cmn-CN-Wavenet-A",
+            "cmn-CN-Wavenet-B",
+            "cmn-CN-Wavenet-C",
+            "cmn-CN-Wavenet-D",
+            "nb-no-Wavenet-E",
+            "nb-no-Wavenet-A",
+            "nb-no-Wavenet-B",
+            "nb-no-Wavenet-C",
+            "nb-no-Wavenet-D",
+            "vi-VN-Wavenet-A",
+            "vi-VN-Wavenet-B",
+            "vi-VN-Wavenet-C",
+            "vi-VN-Wavenet-D",
+            "sr-rs-Standard-A",
+            "lv-lv-Standard-A",
+            "is-is-Standard-A",
+            "bg-bg-Standard-A",
+            "af-ZA-Standard-A",
+            "Tracy",
+            "Danny",
+            "Huihui",
+            "Yaoyao",
+            "Kangkang",
+            "HanHan",
+            "Zhiwei",
+            "Asaf",
+            "An",
+            "Stefanos",
+            "Filip",
+            "Ivan",
+            "Heidi",
+            "Herena",
+            "Kalpana",
+            "Hemant",
+            "Matej",
+            "Andika",
+            "Rizwan",
+            "Lado",
+            "Valluvar",
+            "Linda",
+            "Heather",
+            "Sean",
+            "Michael",
+            "Karsten",
+            "Guillaume",
+            "Pattara",
+            "Jakub",
+            "Szabolcs",
+            "Hoda",
+            "Naayf",
+        ]
+        if VOICE not in STREAMLABS_VOICES:
+            self.STREAMLABS_VOICE = random.choice(STREAMLABS_VOICES)
+        else:
+            self.STREAMLABS_VOICE = VOICE
 
     @staticmethod
     def services():
@@ -52,7 +268,10 @@ class DefaultProvider:
         ).inference(prompt=prompt, tokens=tokens, images=images)
 
     async def text_to_speech(self, text: str):
-        return await Gpt4freeProvider(VOICE=self.VOICE).text_to_speech(text=text)
+        response = requests.get(
+            f"https://api.streamelements.com/kappa/v2/speech?voice={self.STREAMLABS_VOICE}&text={text}"
+        )
+        return response.content
 
     def embeddings(self, input) -> np.ndarray:
         return self.embedder.__call__(input=[input])[0]
@@ -88,5 +307,3 @@ class DefaultProvider:
         return await HuggingfaceProvider(
             HUGGINGFACE_API_KEY=self.HUGGINGFACE_API_KEY
         ).generate_image(prompt=prompt)
-
-    # Would be nice to add a generate_image method here, but I don't have a good default that doesn't require configuration yet.
