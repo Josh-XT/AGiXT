5a57b7f468c83a94f41727fdcde01998b972748c
Josh XT <josh@devxt.com>
2024-04-06 13:08:49 -0400

    Add engine ID

--- agixt/extensions/google.py
+++ agixt/extensions/google.py
@@ -28,6 +28,8 @@ except:
     )
     from googleapiclient.discovery import build
 
+from googleapiclient.errors import HttpError
+
 
 class google(Extensions):
     def __init__(
@@ -36,12 +38,14 @@ class google(Extensions):
         GOOGLE_CLIENT_SECRET: str = "",
         GOOGLE_REFRESH_TOKEN: str = "",
         GOOGLE_API_KEY: str = "",
+        GOOGLE_SEARCH_ENGINE_ID: str = "",
         **kwargs,
     ):
         self.client_id = GOOGLE_CLIENT_ID
         self.client_secret = GOOGLE_CLIENT_SECRET
         self.refresh_token = GOOGLE_REFRESH_TOKEN
         self.GOOGLE_API_KEY = GOOGLE_API_KEY
+        self.custom_search_engine_id = GOOGLE_SEARCH_ENGINE_ID
         self.attachments_dir = "./WORKSPACE/email_attachments/"
         os.makedirs(self.attachments_dir, exist_ok=True)
         self.creds = self.authenticate()
@@ -446,35 +450,17 @@ class google(Extensions):
     async def google_official_search(
         self, query: str, num_results: int = 8
     ) -> Union[str, List[str]]:
-        from googleapiclient.discovery import build
-        from googleapiclient.errors import HttpError
-
         try:
-            # Get the Google API key and Custom Search Engine ID from the config file
-            api_key = self.GOOGLE_API_KEY
-            custom_search_engine_id = self.custom_search_engine_id
-
-            # Initialize the Custom Search API service
-            service = build("customsearch", "v1", developerKey=api_key)
-
-            # Send the search query and retrieve the results
+            service = build("customsearch", "v1", developerKey=self.GOOGLE_API_KEY)
             result = (
                 service.cse()
-                .list(q=query, cx=custom_search_engine_id, num=num_results)
+                .list(q=query, cx=self.custom_search_engine_id, num=num_results)
                 .execute()
             )
-
-            # Extract the search result items from the response
             search_results = result.get("items", [])
-
-            # Create a list of only the URLs from the search results
             search_results_links = [item["link"] for item in search_results]
-
         except HttpError as e:
-            # Handle errors in the API call
             error_details = json.loads(e.content.decode())
-
-            # Check if the error is related to an invalid or missing API key
             if error_details.get("error", {}).get(
                 "code"
             ) == 403 and "invalid API key" in error_details.get("error", {}).get(
@@ -483,6 +469,4 @@ class google(Extensions):
                 return "Error: The provided Google API key is invalid or missing."
             else:
                 return f"Error: {e}"
-
-        # Return the list of search result URLs
         return search_results_links
