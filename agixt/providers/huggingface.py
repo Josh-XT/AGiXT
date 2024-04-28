59d439548d16fd8a1fbed49d5b5a8ef9b6c03617
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2024-04-28 03:04:51 -0400

    Improve image generation endpoints (#1176)
    * Improve image generation endpoints
    
    * Add image service to ezlocalai

--- agixt/providers/huggingface.py
+++ agixt/providers/huggingface.py
@@ -99,7 +99,6 @@ class HuggingfaceProvider:
     async def generate_image(
         self,
         prompt: str,
-        filename: str = "",
         negative_prompt: str = "out of frame,lowres,text,error,cropped,worst quality,low quality,jpeg artifacts,ugly,duplicate,morbid,mutilated,out of frame,extra fingers,mutated hands,poorly drawn hands,poorly drawn face,mutation,deformed,blurry,dehydrated,bad anatomy,bad proportions,extra limbs,cloned face,disfigured,gross proportions,malformed limbs,missing arms,missing legs,extra arms,extra legs,fused fingers,too many fingers,long neck,username,watermark,signature",
         batch_size: int = 1,
         cfg_scale: int = 7,
@@ -126,8 +125,7 @@ class HuggingfaceProvider:
         tiling: bool = False,
         width: int = 768,
     ) -> str:
-        if filename == "":
-            filename = f"{uuid.uuid4()}.png"
+        filename = f"{uuid.uuid4()}.png"
         image_path = f"./WORKSPACE/{filename}"
         headers = {}
         if (
