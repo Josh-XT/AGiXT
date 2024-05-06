1cf5b4b1b5588f90a81bcf9d6f6f144b08309d25
Josh XT <josh@devxt.com>
2024-05-06 09:45:53 -0400

    Attempt to use async

--- agixt/providers/gpt4free.py
+++ agixt/providers/gpt4free.py
@@ -4,11 +4,7 @@ import asyncio
 
 
 class Gpt4freeProvider:
-    def __init__(
-        self,
-        AI_MODEL: str = "gpt-4-turbo",
-        **kwargs,
-    ):
+    def __init__(self, AI_MODEL: str = "gpt-4-turbo", **kwargs):
         self.requirements = ["g4f"]
         self.AI_MODEL = AI_MODEL if AI_MODEL else "gpt-4-turbo"
 
@@ -17,33 +13,27 @@ class Gpt4freeProvider:
         return ["llm"]
 
     async def inference(self, prompt, tokens: int = 0, images: list = []):
-        models = [
-            "gpt-4-turbo",
-            "gpt-4",
-            "mixtral-8x7b",
-            "mistral-7b",
-        ]
-        client = AsyncClient()
+        asyncio.set_event_loop_policy(asyncio.DefaultEventLoopPolicy())
+        self.client = AsyncClient()
+        models = ["gpt-4-turbo", "gpt-4", "mixtral-8x7b", "mistral-7b"]
         try:
-            task = client.chat.completions.create(
-                model=self.AI_MODEL,
-                messages=[{"role": "user", "content": prompt}],
-                stream=False,
-            )
-            response = await asyncio.gather(task)
-            return response.choices[0].message.content
+            async with self.client as client:  # Use a context manager
+                response = await client.chat.completions.create(
+                    model=self.AI_MODEL,
+                    messages=[{"role": "user", "content": prompt}],
+                )
+                return response.choices[0].message.content
         except Exception as e:
             logging.warning(f"gpt4free API Error: {e}")
             for model in models:
                 try:
-                    task = client.chat.completions.create(
-                        model=model,
-                        messages=[{"role": "user", "content": prompt}],
-                        stream=False,
-                    )
-                    response = await asyncio.gather(task)
-                    return response.choices[0].message.content
+                    async with self.client as client:  # Use a context manager
+                        response = await client.chat.completions.create(
+                            model=model,
+                            messages=[{"role": "user", "content": prompt}],
+                        )
+                        return response.choices[0].message.content
                 except Exception as e:
                     logging.warning(f"gpt4free API Error: {e}")
                     continue
-            return "Unable to retrieve a response from the gpt4free provider."
+        return "Unable to retrieve a response from the gpt4free provider."
