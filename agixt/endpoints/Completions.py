736683f563eb266ba5cc9065a9402aace0877834
Josh XT <josh@devxt.com>
2024-05-06 20:36:30 -0400

    await

--- agixt/endpoints/Completions.py
+++ agixt/endpoints/Completions.py
@@ -536,7 +536,7 @@ async def text_to_speech(
     if agent.TTS_PROVIDER != None:
         audio_data = await agent.text_to_speech(text=tts.input)
     else:
-        audio_data = DefaultProvider().text_to_speech(text=tts.input)
+        audio_data = await DefaultProvider().text_to_speech(text=tts.input)
     if audio_data.startswith("http"):
         audio_data = requests.get(audio_data).content
         audio_data = base64.b64encode(audio_data).decode("utf-8")
