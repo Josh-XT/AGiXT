617b51ce26f790ce65737789d6953aade51b9098
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2024-05-23 01:10:50 -0400

    Fix context injection strings (#1191)
    * Fix context injection strings
    
    * fix lint
    
    * clean up refs, update version

--- agixt/Interactions.py
+++ agixt/Interactions.py
@@ -152,33 +152,31 @@ class Interactions:
                         f"The users input makes you to remember some feedback from previous interactions:\n"
                     )
                     if positive_feedback:
-                        context.append(f"Positive Feedback:\n{positive_feedback}\n")
+                        joined_feedback = "\n".join(positive_feedback)
+                        context.append(f"Positive Feedback:\n{joined_feedback}\n")
                     if negative_feedback:
-                        context.append(f"Negative Feedback:\n{negative_feedback}\n")
+                        joined_feedback = "\n".join(negative_feedback)
+                        context.append(f"Negative Feedback:\n{joined_feedback}\n")
                 if websearch:
-                    context.append(
-                        await self.websearch.agent_memory.get_memories(
-                            user_input=user_input,
-                            limit=top_results,
-                            min_relevance_score=min_relevance_score,
-                        )
+                    context += await self.websearch.agent_memory.get_memories(
+                        user_input=user_input,
+                        limit=top_results,
+                        min_relevance_score=min_relevance_score,
                     )
                 if "inject_memories_from_collection_number" in kwargs:
                     if int(kwargs["inject_memories_from_collection_number"]) > 3:
-                        context.append(
-                            await FileReader(
-                                agent_name=self.agent_name,
-                                agent_config=self.agent.AGENT_CONFIG,
-                                collection_number=int(
-                                    kwargs["inject_memories_from_collection_number"]
-                                ),
-                                ApiClient=self.ApiClient,
-                                user=self.user,
-                            ).get_memories(
-                                user_input=user_input,
-                                limit=top_results,
-                                min_relevance_score=min_relevance_score,
-                            )
+                        context += await FileReader(
+                            agent_name=self.agent_name,
+                            agent_config=self.agent.AGENT_CONFIG,
+                            collection_number=int(
+                                kwargs["inject_memories_from_collection_number"]
+                            ),
+                            ApiClient=self.ApiClient,
+                            user=self.user,
+                        ).get_memories(
+                            user_input=user_input,
+                            limit=top_results,
+                            min_relevance_score=min_relevance_score,
                         )
             else:
                 context = []
@@ -778,9 +776,10 @@ class Interactions:
                                     f"Error: {self.agent_name} failed to execute command `{command_name}`. {e}"
                                 )
                                 command_output = f"**Failed to execute command `{command_name}` with args `{command_args}`. Please try again.**"
-                        reformatted_response = reformatted_response.replace(
-                            f"#execute({command_name}, {command_args})",
-                            command_output,
-                        )
+                        if command_output:
+                            reformatted_response = reformatted_response.replace(
+                                f"#execute({command_name}, {command_args})",
+                                command_output,
+                            )
                         if reformatted_response != self.response:
                             self.response = reformatted_response
