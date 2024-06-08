c3ca688237bc1d2f8d7c076345676699bd4845a3
Josh XT <josh@devxt.com>
2024-06-08 10:34:49 -0400

    only allow https

--- agixt/XT.py
+++ agixt/XT.py
@@ -582,7 +582,7 @@ class AGiXT:
             full_path = os.path.normpath(os.path.join(self.agent_workspace, file_name))
             if not full_path.startswith(self.agent_workspace):
                 raise Exception("Path given not allowed")
-            if file_url.startswith("http"):
+            if file_url.startswith("https"):
                 with open(full_path, "wb") as f:
                     f.write(requests.get(file_url).content)
         if conversation_name != "" and conversation_name != None:
