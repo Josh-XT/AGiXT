56e534e21b3a0a36bd0773068a888cd6302fa730
Josh XT <josh@devxt.com>
2024-06-10 12:26:09 -0400

    add activity feedback note

--- agixt/endpoints/Memory.py
+++ agixt/endpoints/Memory.py
@@ -647,7 +647,7 @@ async def rlhf(
     )
     c.log_interaction(
         role=agent_name,
-        message=response_message,
+        message=f"[ACTIVITY][FEEDBACK] {response_message}",
     )
     c.toggle_feedback_received(message=data.message)
     return ResponseMessage(message=response_message)
