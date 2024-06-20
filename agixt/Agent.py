dc1b648174b2f390667a89adbd7d185a9f85d93b
Josh XT <josh@devxt.com>
2024-06-20 09:06:11 -0400

    Closing database sessions

--- agixt/Agent.py
+++ agixt/Agent.py
@@ -16,6 +16,7 @@ from DB import (
 from Providers import Providers
 from Extensions import Extensions
 from Globals import getenv, DEFAULT_SETTINGS, DEFAULT_USER
+from fastapi import HTTPException
 from datetime import datetime, timezone, timedelta
 import logging
 import json
@@ -29,9 +30,9 @@ logging.basicConfig(
 
 
 def add_agent(agent_name, provider_settings=None, commands=None, user=DEFAULT_USER):
-    session = get_session()
     if not agent_name:
         return {"message": "Agent name cannot be empty."}
+    session = get_session()
     # Check if agent already exists
     agent = (
         session.query(AgentModel)
@@ -39,6 +40,7 @@ def add_agent(agent_name, provider_settings=None, commands=None, user=DEFAULT_US
         .first()
     )
     if agent:
+        session.close()
         return {"message": f"Agent {agent_name} already exists."}
     agent = (
         session.query(AgentModel)
@@ -46,6 +48,7 @@ def add_agent(agent_name, provider_settings=None, commands=None, user=DEFAULT_US
         .first()
     )
     if agent:
+        session.close()
         return {"message": f"Agent {agent_name} already exists."}
     user_data = session.query(User).filter(User.email == user).first()
     user_id = user_data.id
@@ -80,7 +83,7 @@ def add_agent(agent_name, provider_settings=None, commands=None, user=DEFAULT_US
                 )
                 session.add(agent_command)
     session.commit()
-
+    session.close()
     return {"message": f"Agent {agent_name} created."}
 
 
@@ -94,6 +97,7 @@ def delete_agent(agent_name, user=DEFAULT_USER):
         .first()
     )
     if not agent:
+        session.close()
         return {"message": f"Agent {agent_name} not found."}, 404
 
     # Delete associated chain steps
@@ -125,7 +129,7 @@ def delete_agent(agent_name, user=DEFAULT_USER):
     # Delete the agent
     session.delete(agent)
     session.commit()
-
+    session.close()
     return {"message": f"Agent {agent_name} deleted."}, 200
 
 
@@ -139,11 +143,11 @@ def rename_agent(agent_name, new_name, user=DEFAULT_USER):
         .first()
     )
     if not agent:
+        session.close()
         return {"message": f"Agent {agent_name} not found."}, 404
-
     agent.name = new_name
     session.commit()
-
+    session.close()
     return {"message": f"Agent {agent_name} renamed to {new_name}."}, 200
 
 
@@ -162,21 +166,31 @@ def get_agents(user=DEFAULT_USER):
         if agent.name in [a["name"] for a in output]:
             continue
         output.append({"name": agent.name, "id": agent.id, "status": False})
+    session.close()
     return output
+
+
+def get_user_id(user=DEFAULT_USER):
+    session = get_session()
+    user_data = session.query(User).filter(User.email == user).first()
+    if user_data is None:
+        session.close()
+        raise HTTPException(status_code=404, detail=f"User {user} not found.")
+    try:
+        user_id = user_data.id
+    except Exception as e:
+        session.close()
+        raise HTTPException(status_code=404, detail=f"User {user} not found.")
+    session.close()
+    return user_id
 
 
 class Agent:
     def __init__(self, agent_name=None, user=DEFAULT_USER, ApiClient=None):
         self.agent_name = agent_name if agent_name is not None else "AGiXT"
-        self.session = get_session()
         user = user if user is not None else DEFAULT_USER
         self.user = user.lower()
-        try:
-            user_data = self.session.query(User).filter(User.email == self.user).first()
-            self.user_id = user_data.id
-        except Exception as e:
-            logging.error(f"User {self.user} not found.")
-            raise
+        self.user_id = get_user_id(user=self.user)
         self.AGENT_CONFIG = self.get_agent_config()
         self.load_config_keys()
         if "settings" not in self.AGENT_CONFIG:
@@ -286,8 +300,9 @@ class Agent:
                 setattr(self, key, self.AGENT_CONFIG[key])
 
     def get_agent_config(self):
+        session = get_session()
         agent = (
-            self.session.query(AgentModel)
+            session.query(AgentModel)
             .filter(
                 AgentModel.name == self.agent_name, AgentModel.user_id == self.user_id
             )
@@ -295,11 +310,9 @@ class Agent:
         )
         if not agent:
             # Check if it is a global agent
-            global_user = (
-                self.session.query(User).filter(User.email == DEFAULT_USER).first()
-            )
+            global_user = session.query(User).filter(User.email == DEFAULT_USER).first()
             agent = (
-                self.session.query(AgentModel)
+                session.query(AgentModel)
                 .filter(
                     AgentModel.name == self.agent_name,
                     AgentModel.user_id == global_user.id,
@@ -309,12 +322,12 @@ class Agent:
 
         config = {"settings": {}, "commands": {}}
         if agent:
-            all_commands = self.session.query(Command).all()
+            all_commands = session.query(Command).all()
             agent_settings = (
-                self.session.query(AgentSettingModel).filter_by(agent_id=agent.id).all()
+                session.query(AgentSettingModel).filter_by(agent_id=agent.id).all()
             )
             agent_commands = (
-                self.session.query(AgentCommand)
+                session.query(AgentCommand)
                 .join(Command)
                 .filter(
                     AgentCommand.agent_id == agent.id,
@@ -331,7 +344,10 @@ class Agent:
                 )
             for setting in agent_settings:
                 config["settings"][setting.name] = setting.value
+            session.commit()
+            session.close()
             return config
+        session.close()
         return {"settings": DEFAULT_SETTINGS, "commands": {}}
 
     async def inference(self, prompt: str, tokens: int = 0, images: list = []):
@@ -387,8 +403,9 @@ class Agent:
         return verbose_commands
 
     def update_agent_config(self, new_config, config_key):
+        session = get_session()
         agent = (
-            self.session.query(AgentModel)
+            session.query(AgentModel)
             .filter(
                 AgentModel.name == self.agent_name, AgentModel.user_id == self.user_id
             )
@@ -396,15 +413,16 @@ class Agent:
         )
         if not agent:
             logging.error(f"Agent '{self.agent_name}' not found in the database.")
-            return
+            session.close()
+            raise HTTPException(
+                status_code=404, detail=f"Agent '{self.agent_name}' not found."
+            )
         if config_key == "commands":
             for command_name, enabled in new_config.items():
-                command = (
-                    self.session.query(Command).filter_by(name=command_name).first()
-                )
+                command = session.query(Command).filter_by(name=command_name).first()
                 if command:
                     agent_command = (
-                        self.session.query(AgentCommand)
+                        session.query(AgentCommand)
                         .filter_by(agent_id=agent.id, command_id=command.id)
                         .first()
                     )
@@ -414,12 +432,12 @@ class Agent:
                         agent_command = AgentCommand(
                             agent_id=agent.id, command_id=command.id, state=enabled
                         )
-                        self.session.add(agent_command)
+                        session.add(agent_command)
         else:
             for setting_name, setting_value in new_config.items():
                 logging.info(f"Setting {setting_name} to {setting_value}.")
                 agent_setting = (
-                    self.session.query(AgentSettingModel)
+                    session.query(AgentSettingModel)
                     .filter_by(agent_id=agent.id, name=setting_name)
                     .first()
                 )
@@ -429,15 +447,18 @@ class Agent:
                     agent_setting = AgentSettingModel(
                         agent_id=agent.id, name=setting_name, value=str(setting_value)
                     )
-                    self.session.add(agent_setting)
+                    session.add(agent_setting)
         try:
-            self.session.commit()
+            session.commit()
+            session.close()
             logging.info(f"Agent {self.agent_name} configuration updated successfully.")
         except Exception as e:
-            self.session.rollback()
+            session.rollback()
+            session.close()
             logging.error(f"Error updating agent configuration: {str(e)}")
-            raise
-
+            raise HTTPException(
+                status_code=500, detail=f"Error updating agent configuration: {str(e)}"
+            )
         return f"Agent {self.agent_name} configuration updated."
 
     def get_browsed_links(self, conversation_id=None):
@@ -447,21 +468,24 @@ class Agent:
         Returns:
             list: The list of URLs that have been browsed by the agent.
         """
+        session = get_session()
         agent = (
-            self.session.query(AgentModel)
+            session.query(AgentModel)
             .filter(
                 AgentModel.name == self.agent_name, AgentModel.user_id == self.user_id
             )
             .first()
         )
         if not agent:
+            session.close()
             return []
         browsed_links = (
-            self.session.query(AgentBrowsedLink)
+            session.query(AgentBrowsedLink)
             .filter_by(agent_id=agent.id, conversation_id=conversation_id)
             .order_by(AgentBrowsedLink.id.desc())
             .all()
         )
+        session.close()
         if not browsed_links:
             return []
         return browsed_links
@@ -495,8 +519,9 @@ class Agent:
         Returns:
             str: The response message.
         """
+        session = get_session()
         agent = (
-            self.session.query(AgentModel)
+            session.query(AgentModel)
             .filter(
                 AgentModel.name == self.agent_name, AgentModel.user_id == self.user_id
             )
@@ -507,8 +532,9 @@ class Agent:
         browsed_link = AgentBrowsedLink(
             agent_id=agent.id, url=url, conversation_id=conversation_id
         )
-        self.session.add(browsed_link)
-        self.session.commit()
+        session.add(browsed_link)
+        session.commit()
+        session.close()
         return f"Link {url} added to browsed links."
 
     def delete_browsed_link(self, url, conversation_id=None):
@@ -521,8 +547,9 @@ class Agent:
         Returns:
             str: The response message.
         """
+        session = get_session()
         agent = (
-            self.session.query(AgentModel)
+            session.query(AgentModel)
             .filter(
                 AgentModel.name == self.agent_name,
                 AgentModel.user_id == self.user_id,
@@ -532,19 +559,21 @@ class Agent:
         if not agent:
             return f"Agent {self.agent_name} not found."
         browsed_link = (
-            self.session.query(AgentBrowsedLink)
+            session.query(AgentBrowsedLink)
             .filter_by(agent_id=agent.id, url=url, conversation_id=conversation_id)
             .first()
         )
         if not browsed_link:
             return f"Link {url} not found."
-        self.session.delete(browsed_link)
-        self.session.commit()
+        session.delete(browsed_link)
+        session.commit()
+        session.close()
         return f"Link {url} deleted from browsed links."
 
     def get_agent_id(self):
+        session = get_session()
         agent = (
-            self.session.query(AgentModel)
+            session.query(AgentModel)
             .filter(
                 AgentModel.name == self.agent_name, AgentModel.user_id == self.user_id
             )
@@ -552,13 +581,15 @@ class Agent:
         )
         if not agent:
             agent = (
-                self.session.query(AgentModel)
+                session.query(AgentModel)
                 .filter(
                     AgentModel.name == self.agent_name,
                     AgentModel.user.has(email=DEFAULT_USER),
                 )
                 .first()
             )
+            session.close()
             if not agent:
                 return None
+        session.close()
         return agent.id
