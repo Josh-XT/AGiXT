4b01fa27d28b67e1231c3aaceb9dfdf7c20a23f0
Josh XT <josh@devxt.com>
2024-06-25 09:25:44 -0400

    improve rotation

--- agixt/providers/gpt4free.py
+++ agixt/providers/gpt4free.py
@@ -1,19 +1,111 @@
 import logging
 import asyncio
 import random
-from g4f.models import ModelUtils
+from g4f.Provider import (
+    ChatgptNext,
+    HuggingChat,
+    ChatgptDemo,
+    GptForLove,
+    ChatgptAi,
+    DeepInfra,
+    ChatBase,
+    Liaobots,
+    FreeGpt,
+    GptGo,
+    Gpt6,
+)
 
 
 class Gpt4freeProvider:
     def __init__(self, AI_MODEL: str = "claude-v2", **kwargs):
         self.requirements = ["g4f"]  # Breaking changes were made after g4f v0.2.6.2
         self.AI_MODEL = AI_MODEL if AI_MODEL else "claude-v2"
+        self.providers = [
+            {
+                "name": "ChatgptNext",
+                "class": ChatgptNext,
+                "models": [
+                    "gpt-3.5-turbo",
+                ],
+            },
+            {
+                "name": "HuggingChat",
+                "class": HuggingChat,
+                "models": [
+                    "mistralai/Mixtral-8x7B-Instruct-v0.1",
+                    "mistralai/Mistral-7B-Instruct-v0.1",
+                    "openchat/openchat_3.5",
+                    "meta-llama/Llama-2-70b-chat-hf",
+                ],
+            },
+            {
+                "name": "ChatgptDemo",
+                "class": ChatgptDemo,
+                "models": [
+                    "gpt-3.5-turbo",
+                ],
+            },
+            {
+                "name": "GptForLove",
+                "class": GptForLove,
+                "models": [
+                    "gpt-3.5-turbo",
+                ],
+            },
+            {
+                "name": "ChatgptAi",
+                "class": ChatgptAi,
+                "models": [
+                    "gpt-3.5-turbo",
+                ],
+            },
+            {
+                "name": "DeepInfra",
+                "class": DeepInfra,
+                "models": [
+                    "meta-llama/Meta-Llama-3-70B-Instruct",
+                    "Qwen/Qwen2-72B-Instruct",
+                ],
+            },
+            {
+                "name": "ChatBase",
+                "class": ChatBase,
+                "models": [
+                    "gpt-3.5-turbo",
+                ],
+            },
+            {
+                "name": "Liaobots",
+                "class": Liaobots,
+                "models": [
+                    "gpt-4",
+                ],
+            },
+            {
+                "name": "FreeGpt",
+                "class": FreeGpt,
+                "models": [
+                    "gpt-3.5-turbo",
+                ],
+            },
+            {
+                "name": "GptGo",
+                "class": GptGo,
+                "models": [
+                    "gpt-3.5-turbo",
+                ],
+            },
+            {
+                "name": "Gpt6",
+                "class": Gpt6,
+                "models": [
+                    "gpt-3.5-turbo",
+                ],
+            },
+        ]
         self.models = [
             "gpt-3.5-turbo",
-            "mistral-7b",
             "claude-v2",
-            "claude-3-sonnet",
-            "claude-3-opus",
             "gemini",
             "gemini-pro",
         ]
@@ -24,35 +116,49 @@ class Gpt4freeProvider:
         return ["llm"]
 
     async def inference(self, prompt, tokens: int = 0, images: list = []):
-        try:
-            model = ModelUtils.convert[self.AI_MODEL]
-        except:
-            model = ModelUtils.convert["claude-v2"]
-        provider = model.best_provider
-        if provider:
-            append_model = f" and model: {model.name}" if model.name else ""
-            logging.info(
-                f"[Gpt4Free] Using provider: {provider.__name__}{append_model}"
-            )
+        # Provider data is a list of providers for the selected model
+        provider = None
+        provider_name = None
+        model = None
+        for p in self.providers:
+            if self.AI_MODEL in p["models"]:
+                provider = p["class"]
+                provider_name = p["name"]
+                model = p
+                break
+        logging.info(f"[Gpt4Free] Using provider: {provider_name} with model: {model}")
         try:
             return (
                 await asyncio.gather(
                     provider.create_async(
-                        model=model.name,
+                        model=model,
                         messages=[{"role": "user", "content": prompt}],
                     )
                 )
             )[0]
         except Exception as e:
             logging.error(f"[Gpt4Free] {e}")
-            self.failures.append(model.name)
-            if len(self.failures) <= len(self.models):
-                model_list = self.models.copy()
-                # Shuffle model list so that we're not always trying the same models
-                random.shuffle(model_list)
-                for model in model_list:
-                    if model not in self.failures:
-                        logging.info(f"[Gpt4Free] Trying model: {model}")
+            self.failures.append({"provider": provider_name, "model": model})
+            provider_count = len(provider_name)
+            if len(self.failures) < provider_count:
+                for provider in self.providers:
+                    if provider_name not in self.failures:
+                        provider_models = provider["models"]
+                        if not isinstance(provider_models, list):
+                            provider_models = [provider_models]
+                        # Remove any models that have failed
+                        for failure in self.failures:
+                            if failure["provider"] == provider["name"]:
+                                if failure["model"] in provider_models:
+                                    # delete the model from the list for the provider
+                                    provider_models.remove(failure["model"])
+                        if len(provider_models) > 0:
+                            # Skip this provider and try another
+                            continue
+                        model = random.choice(provider_models)
+                        logging.info(
+                            f"[Gpt4Free] Switching to provider: {provider['name']} with model: {model}"
+                        )
                         self.AI_MODEL = model
                         break
                 return await self.inference(prompt=prompt, tokens=tokens, images=images)
