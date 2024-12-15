b1c0e0cd0ee7c4c7ee4e0930c4c0731deda2b24b
Josh XT <josh@devxt.com>
2024-12-15 06:34:29 -0500

    fix function

--- agixt/ApiClient.py
+++ agixt/ApiClient.py
@@ -23,3 +23,21 @@ from MagicalAuth import verify_api_key
 def get_api_client(authorization: str = Header(None)):
     authorization = str(authorization).replace("Bearer ", "").replace("bearer ", "")
     return AGiXTSDK(base_uri="http://localhost:7437", api_key=authorization)
+
+
+def is_admin(email: str = "USER", api_key: str = None):
+    return True
+    # Commenting out functionality until testing is complete.
+    AGIXT_API_KEY = getenv("AGIXT_API_KEY")
+    if api_key is None:
+        api_key = ""
+    api_key = api_key.replace("Bearer ", "").replace("bearer ", "")
+    if AGIXT_API_KEY == api_key:
+        return True
+
+    if email == "" or email is None or email == "None":
+        email = getenv("DEFAULT_USER")
+        if email == "" or email is None or email == "None":
+            email = "USER"
+    return is_agixt_admin(email=email, api_key=api_key)
+    return False
