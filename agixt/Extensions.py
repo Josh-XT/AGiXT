753ac637c3d3a7c1288f1f2eba6188b4691f5d44
Josh XT <josh@devxt.com>
2025-06-20 23:41:33 -0400

    improve tesla commands

--- agixt/Extensions.py
+++ agixt/Extensions.py
@@ -68,8 +68,9 @@ class Extensions:
         self.commands = self.load_commands()
         self.available_commands = self.get_available_commands()
 
-    async def execute_chain(self, user_input="", **kwargs):
+    async def execute_chain(self,**kwargs):
         chain_name = kwargs.get("chain_name", "")
+        user_input = kwargs.get("user_input", "")
         if "chain_name" in kwargs:
             del kwargs["chain_name"]
         return self.ApiClient.run_chain(
