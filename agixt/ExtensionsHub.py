89a70d5756f9f7f086172a7ed2d500f6be850517
Josh XT <josh@devxt.com>
2025-08-31 16:40:38 -0400

    clone to proper path

--- agixt/ExtensionsHub.py
+++ agixt/ExtensionsHub.py
@@ -19,7 +19,8 @@ class ExtensionsHub:
     """Manages external extension repositories"""
 
     def __init__(self):
-        self.extensions_dir = "extensions"
+        # Ensure we're cloning to the same directory that Extensions.py looks in
+        self.extensions_dir = "agixt/extensions"
         self.hub_urls = getenv("EXTENSIONS_HUB")
         self.hub_token = getenv("EXTENSIONS_HUB_TOKEN")
 
