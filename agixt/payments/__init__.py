3255826395b31ba39401d274441d694dd7447316
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2025-10-29 12:02:10 -0400

    Add x402 payments (#1581)

--- agixt/payments/__init__.py
+++ agixt/payments/__init__.py
@@ -1,10 +1,13 @@
 from .pricing import PriceService, SUPPORTED_CURRENCIES
 from .crypto import CryptoPaymentService
 from .stripe_service import StripePaymentService
+from .x402 import X402PaymentService, get_x402_service
 
 __all__ = [
     "PriceService",
     "SUPPORTED_CURRENCIES",
     "CryptoPaymentService",
     "StripePaymentService",
+    "X402PaymentService",
+    "get_x402_service",
 ]
